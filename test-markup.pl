7271be3b64e563e3af98beb0d7594443586443f4
Alex Schroeder <alex@gnu.org>
2003-08-21 16:05:14 +0000

    add more journal tests

--- test-markup.pl
+++ test-markup.pl
@@ -238,6 +238,18 @@ test_page(update_page('2003-01-01', "This is my journal -- recursive:\n\n<journa
 push @Test, 'journal';
 test_page(update_page('2003-01-01', "This is my journal -- truly recursive:\n\n<journal>"), @Test);
 
+@Test = split('\n',<<'EOT');
+2003-06-15(.|\n)*2003-06-14
+EOT
+
+test_page(update_page('Summary', "Counting down:\n\n<journal 2>"), @Test);
+
+@Test = split('\n',<<'EOT');
+2003-01-01(.|\n)*2003-06-13(.|\n)*2003-06-14
+EOT
+
+test_page(update_page('Summary', "Counting up:\n\n<journal 3 reverse>"), @Test);
+
 ### SIMPLE MARKUP TESTS
 
 %Test = split('\n',<<'EOT');
