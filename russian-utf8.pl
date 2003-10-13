18ad96c62d9e5a4f2e471008656f44b60b901b14
Alex Schroeder <alex@gnu.org>
2003-10-13 18:46:43 +0000

    new

---
+++ russian-utf8.pl
@@ -0,0 +1,390 @@
+# UTF-8 encoded Russian language file for use with Oddmuse
+#
+# Copyright (c) 2003  Zajcev Evgeny
+#
+# Permission is granted to copy, distribute and/or modify this
+# document under the terms of the GNU Free Documentation License,
+# Version 1.2 or any later version published by the Free Software
+# Foundation; with no Invariant Sections, no Front-Cover Texts, and no
+# Back-Cover Texts.  A copy of the license could be found at:
+# http://www.gnu.org/licenses/fdl.txt .
+#
+# Installation:
+# =============
+# This file should be saved into the same folder where the config file
+# resides.  Load it from the config file:
+#
+# do 'russian-utf8.pl';
+#
+# This script was last checked for Oddmuse version 1.37.
+#
+%Translate = split('\n',<<END_OF_TRANSLATION);
+Reading not allowed: user, ip, or network is blocked.
+
+Could not create %s
+Невозможно создать %s
+Invalid UserName %s: not saved.
+Hекорректная кличка %s: ничего не записано.
+UserName must be 50 characters or less: not saved
+
+http://shop.barnesandnoble.com/bookSearch/isbnInquiry.asp?isbn=%s
+
+http://www.amazon.com/exec/obidos/ISBN=%s
+
+alternate
+альтернативный
+http://www.pricescan.com/books/BookDetail.asp?isbn=%s
+
+search
+поиск
+Invalid action parameter %s
+Некорректный параметр действия %s
+Invalid URL.
+Некорректный URL.
+Revision %s not available
+Версия %s не доступна
+showing current revision instead
+отображение текущей версии вместо
+Showing revision %s
+Показ версии %s
+Updates since %s
+Обновления с %s
+Updates in the last %s day
+Обновления за последний %s день
+Updates in the last %s days
+Обновления за последние %s дней
+Could not open %s log file
+Не возможно открыть файл протокола %s 
+Error was
+Были ошибки 
+Note: This error is normal if no changes have been made.
+Примечание: Эта ошибка - нормально, если не было сделано изменений.
+Could not open old %s log file
+Не возможно открыть старый файл протокола %s 
+for %s only
+только для %s
+%s day
+%s день
+%s days
+%s дней
+List new changes starting from
+Список новых изменений начиная с
+No updates since %s
+Небыло обновлений с %s
+Page generated %s
+Страница сгенерирована %s
+(minor)
+
+(diff)
+(отличия)
+history
+
+History of %s
+История %s
+Compare
+
+ . . . . 
+
+Revision %s
+Версия %s
+Edit
+Правка
+by
+
+from %s
+с %s
+[Home]
+[Домой]
+redirected from %s
+перенаправлено с %s
+Cookie: 
+
+new comment
+
+Username:
+
+Save
+Сохранить
+Comments on this page
+
+Edit revision %s of this page
+Править версию % этой страницы
+Edit text of this page
+Редактировать текст этой страницы
+This page is read-only
+Эта страница предназначена только для чтения
+View other revisions
+Смотреть другие версии
+View current revision
+Смотреть текущую версию
+View original
+
+Last edited
+Последнее редактирование
+Edited
+Правленное
+Warning
+Внимание
+Database is stored in temporary directory %s
+База данных сохранена во временной директории %s
+%s seconds
+
+Search:
+Поиск:
+Replace:
+
+Go!
+
+Validate HTML
+
+Validate CSS
+
+Please go on to %s.
+
+major diff
+основные отличия
+minor diff
+незначительные отличия
+author diff
+авторские отличия
+major
+основной
+minor
+второстепенный
+author
+автор
+(The revisions are identical or unavailable.)
+(Версии идентичны или недоступны.)
+no other diffs
+других отличий нет
+No diff available.
+Отличий нет.
+current revision
+
+revision %s
+
+Difference (from revision %s
+
+ to %s)
+
+No diff available--this is the first %s revision.
+Отличий нет - это первая версия %s.
+Difference (from prior %s revision)
+Отличия (текущей версии от предыдущей - %s)
+Changed:
+Изменилось:
+Removed:
+Удалено:
+Added:
+Добавлено:
+to
+
+Bad page version (or corrupt page).
+Плохая версия страницы (или испорченая).
+cant write %s
+невозможно записать %s
+Page name is too long: %s
+Имя страницы слишком велико: %s
+Page name may not contain space characters: %s
+Имя страницы не может содержать пробельные символы: %s
+Invalid Page %s
+Некорректная страница %s
+Invalid Page %s (must not end with .db)
+Некорректная страница %s (не должна оканчиваться на .db)
+Invalid Page %s (must not end with .lck)
+Некорректная страница %s (Некорректная страница %s lck)
+can not make %s
+не могу сделать %s
+Unlocking
+
+This operation may take several seconds...
+Это может занять несколько секунд, так что не нервничайте
+Forced unlock of %s lock.
+
+No unlock required.
+
+Can not open %s
+Невозможно открыть %s
+Editing Denied
+Редактирование запрещено
+Editing not allowed: user, ip, or network is blocked.
+Редактирование не разрешено: пользователь, IP или сеть заблокированы.
+Contact the wiki administrator for more information.
+Свяжитесь с нашей администрацией чтоб узнать больше.
+Editing not allowed: %s is read-only.
+Редактирование не допустимо: %s только для чтения.
+Editing %s
+Редактирование %s
+Editing revision %s of
+Редактирование версии %s
+Editing old revision %s.
+Редактирование старой версии %s.
+Saving this page will replace the latest revision with this text.
+Сохранение этой страницы заменит последную версию на этот текст.
+Edit Conflict!
+Конфликт во время редактирования!
+(This is a new conflict)
+(Это новый конфликт)
+Someone saved this page after you started editing.
+Кто то сохранил эту страницу после того, как Вы начали её реадктировать.
+The top textbox contains the saved text.
+Верхнее поле с текстом содержит сохранённый текст.
+Only the text in the top textbox will be saved.
+Только текст из верхнего поля будет сохранён.
+Scroll down to see your text with conflict markers.
+
+Scroll down to see your edited text.
+Смотрите ниже, Ваш правленый текст там.
+Last save time:
+Время последнего сохранения:
+Current time is:
+Текущее время:
+Summary:
+Итого:
+This change is a minor edit.
+Это изменение является незначительной правкой.
+Preview
+Предпросмотр
+This is the text with conflict markers:
+
+This is the text you submitted:
+Вот отправленый Вами текст:
+Preview:
+Предпросмотр:
+NOTE: This preview shows the revision of the other author.
+Внимание: Тут отображена версия другого автора.
+Preview only, not yet saved
+Только предпросмотр - пока ничего не записано
+Password
+
+Your password is saved in a cookie, if you have cookies enabled. Cookies may get lost if you connect from another machine, from another account, or using another software.
+
+You are currently an administrator on this site.
+
+You are currently an editor on this site.
+
+You are a normal user on this site.
+
+Your password does not match any of the  administrator or editor passwords.
+
+Password:
+Пароль:
+This site does not use admin or editor passwords.
+
+This operation is restricted to site editors only...
+Эта операция только для редакторов сайта...
+This operation is restricted to administrators only...
+Эта операция только для администаторов сайта...
+Index of all pages
+Проиндексировать все страницы:
+Replaced: %s
+
+Search for: %s
+Искать: %s
+%s pages found:
+%s страницы не найдены:
+last updated
+
+Full Link List
+Полный список ссылок
+Complete Content
+
+The main page is %s.
+
+Editing not allowed for %s.
+Редактирование не разрешено для %s.
+SampleUndefinedPage
+ПримерПроизвольнойСтраницы
+%s cannot be defined.
+%s невозможно определить.
+Sample_Undefined_Page
+Пример_Произвольной_Страницы
+[[%s]] cannot be defined.
+[[%s]] невозможно определить
+Only an administrator can create %s
+
+Cannot find timestamp on the first line.
+
+Could not get main lock
+
+Anonymous
+
+Could not get a lock to merge!
+
+%s log error:
+
+Ping
+
+No response.
+
+Maintenance on all pages
+
+Maintenance not done.
+
+(Maintenance can only be done once every 12 hours.)
+
+Remove the "maintain" file or wait.
+
+Main lock obtained.
+
+Expiring keep files and deleting pages marked for deletion
+
+deleted
+
+Moving part of the %s log file.
+
+Moving %s log entries.
+
+Main lock released.
+
+Converting all files
+
+No conversion required.
+
+converted
+
+no conversion required
+
+has no file
+
+Set or Remove global edit lock
+
+Edit lock created.
+
+Edit lock removed.
+
+Set or Remove page edit lock
+
+Missing page id to lock/unlock...
+
+Lock for %s created.
+
+Lock for %s removed.
+
+Displaying Wiki Version
+
+Too many connections by %s
+
+Recent Visitors
+
+%s hours ago
+
+1 hour ago
+
+%s minutes ago
+
+1 minute ago
+
+%s seconds ago
+
+1 second ago
+
+just now
+
+Referrers
+
+All Referrers
+
+anchor first defined here
+
+END_OF_TRANSLATION
