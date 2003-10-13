e67f981b9fd20e3a2dc491b472f4b6e96e4dd98f
Alex Schroeder <alex@gnu.org>
2003-10-13 18:14:47 +0000

    new

---
+++ german-utf8.pl
@@ -0,0 +1,404 @@
+# UTF-8 encoded German translation file for use with Oddmuse
+#
+# Copyright (c)  2003  Karl Loncarek <karl@loncarek.de>
+# Copyright (c)  2003  Alex Schröder <alex@oddmuse.org>
+#
+# Permission is granted to copy, distribute and/or modify this
+# document under the terms of the GNU Free Documentation License,
+# Version 1.2 or any later version published by the Free Software
+# Foundation; with no Invariant Sections, no Front-Cover Texts, and no
+# Back-Cover Texts.  A copy of the license could be found at:
+# http://www.gnu.org/licenses/fdl.txt .
+#
+# Installation:
+# =============
+# Diese Datei sollte im gleichen Verzeichnis wie die
+# Konfigurationsdatei gespeichert werden.  In der Konfigurationsdatei
+# muss diese Datei dann geladen werden:
+#
+# do 'german-utf8.pl';
+#
+# Diese Übersetzung wurde für Oddmuse 1.185 erstellt.
+#
+%Translate = split('\n',<<END_OF_TRANSLATION);
+Reading not allowed: user, ip, or network is blocked.
+Lesen nicht erlaubt: Benutzer, IP oder Netzwerk ist gesperrt.
+Could not create %s
+Konnte %s nicht erzeugen
+Invalid UserName %s: not saved.
+Ungültiger Benutzername %s: nicht gespeichert.
+UserName must be 50 characters or less: not saved
+Länge des Benutzernamens maximal 50 Zeichen: nicht gespeichert.
+http://shop.barnesandnoble.com/bookSearch/isbnInquiry.asp?isbn=%s
+http://www.kelkoo.de/shopbot/search.jsp?from=shopbot&catPath=de%2Fbook&catId=100801&title=&author=&isbn=%s&maxprice=&x=36&y=9
+http://www.amazon.com/exec/obidos/ISBN=%s
+http://www.amazon.de/exec/obidos/ISBN=%s
+alternate
+Alternativ
+http://www.pricescan.com/books/BookDetail.asp?isbn=%s
+
+search
+Suchen
+Invalid action parameter %s
+Ungültiger action Parameter: %s
+Invalid URL.
+Ungültige URL.
+Revision %s not available
+Version %s nicht verfügbar
+showing current revision instead
+Stattdessen Anzeige der aktuellen Version
+Showing revision %s
+Anzeige Version %s
+Updates since %s
+Änderungen seit %s
+Updates in the last %s day
+Änderungen der letzten %s Tage
+Updates in the last %s days
+Änderungen der letzten %s Tage
+Could not open %s log file
+Kann Logdatei %s nicht öffnen
+Error was
+Fehler war
+Note: This error is normal if no changes have been made.
+HINWEIS: Dieser Fehler ist normal, wenn keine Änderungen durchgeführt wurden.
+Could not open old %s log file
+Konnte alte Logdatei %s nicht öffnen
+for %s only
+nur für %s
+%s day
+%s Tag
+%s days
+%s Tage
+List new changes starting from
+Neue Änderungen anzeigen seit
+No updates since %s
+Keine Änderungen seit %s
+Page generated %s
+Seite erzeugt am %s
+(minor)
+(Klein)
+(diff)
+(Unterschiede)
+history
+alte Versionen
+rollback
+bis hier alles Rückgängig machen
+from %s
+von %s
+History of %s
+Historie von %s
+Compare
+Vergleichen
+Revision %s
+Version %s
+ . . . . 
+ . . . . 
+by
+von
+Rolling back changes
+Änderungen werden rückgängig gemacht
+Missing target for rollback.
+Das Stichdatum fehlt.
+Target for rollback is too far back.
+Das Stichdatum liegt zu weit zurück.
+Rollback to %s
+Rückgängig gemacht auf den Stand vom %s
+%s rolled back
+Änderungen für %s rückgängig gemacht
+[Home]
+[Erste Seite]
+redirected from %s
+Umgeleitet von %s
+Cookie: 
+Cookie: 
+new comment
+Neuer Kommentar
+Username:
+Benutzername:
+Save
+Speichern
+Comments on this page
+Kommentare zu dieser Seite
+Edit revision %s of this page
+Version %s dieser Seite bearbeiten
+Edit text of this page
+Text dieser Seite bearbeiten
+This page is read-only
+Diese Seite ist schreibgeschützt
+View other revisions
+Andere Versionen ansehen
+View current revision
+Aktuelle Version ansehen
+View all changes
+Alle Änderungen ansehen
+View original
+Original ansehen
+Last edited
+Zuletzt geändert
+Edited
+Geändert
+by %s
+von %s
+Warning
+Warnung
+Database is stored in temporary directory %s
+Datenbank wird im temporären Verzeichnis %s gespeichert
+%s seconds
+%s Sekunden
+Search:
+Suchen:
+Replace:
+Ersetzen mit:
+Go!
+Los!
+Validate HTML
+HTML überprüfen
+Validate CSS
+CSS überprüfen
+Please go on to %s.
+Bitte machen Sie weiter bei %s.
+major
+größere
+minor
+kleinere
+(The revisions are identical or unavailable.)
+(Die Versionen sind identisch oder nicht verfügbar.)
+No diff available.
+Keine Unterschiede vorhanden.
+current revision
+Aktuelle Version
+revision %s
+Version %s
+Difference (from revision %s
+Unterschiede (ab Version %s
+ to %s)
+ bis %s)
+No diff available--this is the first %s revision.
+Keine Unterschiede vorhanden -- Das ist die erste %s Version
+Difference (from prior %s revision)
+(%s Änderungen)
+Changed:
+Geändert:
+Removed:
+Gelöscht:
+Added:
+Eingefügt:
+to
+stattdessen:
+Bad page version (or corrupt page).
+Falsche Seitenversion (oder defekte Seite).
+Cannot save an nameless page.
+Eine Seite ohne Namen kann nicht gespeichert werden.
+cannot write %s
+Kann %s nicht schreiben
+Page name is too long: %s
+Seitenname zu lang: %s
+Invalid Page %s
+Ungültige Seite %s
+Invalid Page %s (must not end with .db)
+Ungültige Seite %s (Darf nicht mit .db enden)
+Invalid Page %s (must not end with .lck)
+Ungültige Seite %s (Darf nicht mit .lck enden)
+Page name may not contain space characters: %s
+Seitenname darf keine Leerzeichen enthalten: %s
+Could not get %s lock
+Die %s Sperre konnte nicht gesetzt werden.
+Unlocking
+Aufheben der Sperren
+This operation may take several seconds...
+Das könnte einige Sekunden dauern...
+Forced unlock of %s lock.
+Erzwungene Aufhebung der Sperre von %s.
+No unlock required.
+Aufhebung der Sperre nicht notwendig.
+Can not open %s
+Kann %s nicht öffnen
+%s hours ago
+%s Stunden vorher
+1 hour ago
+Eine Stunde vorher
+%s minutes ago
+%s Minuten vorher
+1 minute ago
+Eine Minute vorher
+%s seconds ago
+%s Sekunden vorher
+1 second ago
+Eine Sekunde vorher
+just now
+Genau jetzt
+Editing Denied
+Bearbeitung verweigert
+Editing not allowed: user, ip, or network is blocked.
+Bearbeitung nicht erlaubt: Benutzer, IP oder Netzwerk ist gesperrt.
+Contact the wiki administrator for more information.
+Für weitere Informationen setzen Sie sich mit dem WIKI Administrator in Verbindung.
+The rule %s matched for you.
+Die Regel %s hat angesprochen.
+See %s for more information.
+Siehe %s für weitere Informationen.
+Editing not allowed: %s is read-only.
+Bearbeitung nicht erlaubt: %s ist schreibgeschützt.
+Only administrators can upload files.
+Nur Administratoren dürfen Dateien hochladen.
+Editing revision %s of
+Bearbeitung Version %s von
+Editing %s
+Bearbeitung %s
+Editing old revision %s.
+Bearbeitung alte Version %s.
+Saving this page will replace the latest revision with this text.
+Das Speichern dieser Seite wird die letzte Version durch diesen Text ersetzen.
+Summary:
+Zusammenfassung:
+This change is a minor edit.
+Dies ist eine kleinere Änderung.
+Preview
+Vorschau
+Replace this file with text.
+Ersetze diese Datei mit Text.
+Replace this text with a file.
+Ersetze diesen Text mit einer Datei.
+Preview:
+Vorschau:
+Preview only, not yet saved
+Vorschau, noch nicht gespeichert
+File to upload: 
+Hochzuladende Datei: 
+Files of type %s are not allowed.
+Der Dateityp %s ist nicht erlaubt.
+Password
+Passwort
+Your password is saved in a cookie, if you have cookies enabled. Cookies may get lost if you connect from another machine, from another account, or using another software.
+Ihr Passwort wird in einem Cookie gespeichert, falls Sie Cookies eingeschaltet haben. Cookies können verloren gehen, wenn Sie die Verbindung von einer anderen Maschine, von einem anderen Account oder mit anderer Software herstellen.
+You are currently an administrator on this site.
+Sie sind momentan ein Administrator auf dieser Webseite.
+You are currently an editor on this site.
+Sie sind momentan ein Bearbeiter auf dieser Webseite.
+You are a normal user on this site.
+Sie sind ein normaler Benutzer auf dieser Webseite.
+Your password does not match any of the  administrator or editor passwords.
+Ihr Passwort stimmt nicht mit einem Administrator- oder Bearbeiter-Passwort überein.
+Password:
+Passwort:
+This site does not use admin or editor passwords.
+Diese Webseite verwendet keine Administrator- oder Bearbeiter-Passwörter.
+This operation is restricted to site editors only...
+Diese Aktion darf nur von Bearbeitern durchgeführt werden...
+This operation is restricted to administrators only...
+Diese Aktion darf nur von Administratoren durchgeführt werden...
+Index of all pages
+Index aller Seiten
+Replaced: %s
+Ersetzt: %s
+Search for: %s
+Suche nach: %s
+%s pages found:
+%s Seiten gefunden:
+last updated
+Zuletzt geändert
+Full Link List
+Volle Link-Liste
+Complete Content
+Vollständiger Inhalt
+The main page is %s.
+Die Hauptseite ist %s.
+Editing not allowed for %s.
+Bearbeitung von %s nicht erlaubt.
+SampleUndefinedPage
+BeispielEinerUndefiniertenSeite
+%s cannot be defined.
+%s kann nicht definiert werden.
+Sample_Undefined_Page
+Beispiel_Einer_Undefinierten_Seite
+[[%s]] cannot be defined.
+[[%s]] kann nicht definiert werden.
+Only an administrator can create %s
+Nur ein Administrator kann %s erstellen
+Transfer Error: %s
+Transferfehler: %s
+Browser reports no file info.
+Der Browser hat keine Dateiinformationen geliefert.
+Browser reports no file type.
+Der Browser hat keinen Dateityp geliefert.
+Anonymous
+Unbekannt
+This page was changed by somebody else %s.
+Diese Seite wurde von einer anderen Person %s verändert
+The changes conflict.  Please check the page again.
+Die Änderungen beeinträchtigen sich gegenseitig.  Bitte überprüfen Sie die Seite nochmals.
+Please check whether you overwrote those changes.
+Bitte überprüfen Sie, ob diese Änderungen überschrieben worden sind.
+Could not get a lock to merge!
+Kann Sperre nicht verbinden!
+you
+Sie
+ancestor
+Vorgänger
+other
+Andere Person
+%s log error:
+%s Log Fehler:
+Ping
+Ping
+No response.
+Keine Antwort.
+Maintenance on all pages
+Wartungsarbeiten auf allen Seiten
+Maintenance not done.
+Wartungsarbeiten nicht erfolgt.
+(Maintenance can only be done once every 12 hours.)
+(Wartungsarbeiten können frühestens alle 12 Stunden durchgeführt werden.)
+Remove the "maintain" file or wait.
+Löschen Sie die Datei "maintain" oder warten Sie.
+Main lock obtained.
+Hauptsperre aktiviert.
+Expiring keep files and deleting pages marked for deletion
+Bringe Keep-Dateien auf neuesten Stand und lösche markierte Seiten
+and refreshing HTML cache
+und HTML Cache wird aufgefrischt
+deleted
+Datei gelöscht
+Moving part of the %s log file.
+Verschiebe einen Teil der Logdatei %s.
+Moving %s log entries.
+Verschiebe %s Log-Einträge.
+Main lock released.
+Hauptsperre aufgehoben.
+Converting all files
+Alle Dateien werden konvertiert
+No conversion required.
+Konversion unnötig.
+converted
+konvertiert
+no conversion required
+muss nicht konvertiert werden
+has no file
+hat keine Datei
+Set or Remove global edit lock
+Erzeugen oder Aufheben der globalen Bearbeitungssperre
+Edit lock created.
+Bearbeitungssperre erzeugt.
+Edit lock removed.
+Bearbeitungssperre aufgehoben.
+Set or Remove page edit lock
+Erzeugen oder aufheben der Seiten-Bearbeitungssperre
+Missing page id to lock/unlock...
+Fehlende Seiten-ID für die Erzeugung/Aufhebung der Sperre...
+Lock for %s created.
+Sperre von %s erzeugt.
+Lock for %s removed.
+Sperre von %s aufheben.
+Displaying Wiki Version
+Anzeige der Wiki Version
+Too many connections by %s
+Zu viele Verbindungen durch %s
+Recent Visitors
+Kürzliche Besucher
+Referrers
+Links auf diese Seite
+All Referrers
+Alle Links auf diesen Wiki
+anchor first defined here: %s
+Der Anker wurde hier zuerst definiert: %s
+END_OF_TRANSLATION
