4b193f89cb888e84eeff4139886ec5c2bc45b574
Alex Schroeder <alex@gnu.org>
2003-10-13 18:23:34 +0000

    new

---
+++ french-utf8.pl
@@ -0,0 +1,390 @@
+# UTF-8 encoded French language file for use with Oddmuse
+#
+# Copyright (c)  2003 Free Software Foundation
+#
+# Permission is granted to copy, distribute and/or modify this
+# document under the terms of the GNU Free Documentation License,
+# Version 1.2 or any later version published by the Free Software
+# Foundation; with no Invariant Sections, no Front-Cover Texts, and no
+# Back-Cover Texts.  A copy of the license could be found at:
+# http://www.gnu.org/licenses/fdl.txt.
+#
+# Installation:
+# =============
+# This file should be saved into the same folder where the config file
+# resides.  Load it from the config file:
+#
+# do 'french-utf8.pl;
+#
+# This translation was last checked for Oddmuse version 1.105.
+#
+%Translate = split('\n',<<END_OF_TRANSLATION);
+Reading not allowed: user, ip, or network is blocked.
+Accès interdit: votre serveur, adresse IP ou nom d'utilisateur a été bloqué.
+Could not create %s
+Création impossible de %s
+Invalid UserName %s: not saved.
+Nom d'utilisateur non valide %s: pas sauvegardé
+UserName must be 50 characters or less: not saved
+Le nom d'utilisateur ne doit pas dépasser 50 caractères: pas sauvegardé
+http://shop.barnesandnoble.com/bookSearch/isbnInquiry.asp?isbn=%s
+
+http://www.amazon.com/exec/obidos/ISBN=%s
+
+alternate
+Alternative
+http://www.pricescan.com/books/BookDetail.asp?isbn=%s
+
+search
+Recherche
+Invalid action parameter %s
+Parmètre d'action invalide %s
+Invalid URL.
+URL non valide
+Revision %s not available
+La version %s n'est pas disponible
+showing current revision instead
+Présentation de la version en cours à sa place.
+Showing revision %s
+Présentation de la version %s
+Updates since %s
+Mises à hour depuis %s
+Updates in the last %s day
+Mises à jour durant le dernier %s jour
+Updates in the last %s days
+Mises à jour durant les derniers %s jours
+Could not open %s log file
+Impossible d'ouvrir le fichier de log %
+Error was
+L'erreur était
+Note: This error is normal if no changes have been made.
+Remarque: Cette erreur est normale si aucune modification n'a été effectuée.
+Could not open old %s log file
+Impossible d'ouvrir l'ancien fichier de log %
+for %s only
+pour %s seulement
+%s day
+%s jour
+%s days
+%s jours
+List new changes starting from
+Liste des nouveaux changements depuis le
+No updates since %s
+Aucune mise à jour depuis le %s
+Page generated %s
+Page générée le %s
+(minor)
+(mineur)
+(diff)
+(diff)
+history
+historique
+History of %s
+Historique de %s
+Compare
+Comparaison
+ . . . . 
+ . . . . 
+Revision %s
+Version %s
+Edit
+Modification
+by
+par
+from %s
+depuis %s
+[Home]
+[Accueil]
+redirected from %s
+Redirection depuis %s
+Cookie: 
+Cookie: 
+new comment
+nouveau commentaire
+Username:
+Nom d'utilisateur:
+Save
+Sauvegarder
+Comments on this page
+Commentaires sur cette page
+Edit revision %s of this page
+Modifier la version %s de cette page
+Edit text of this page
+Modifier le texte de cette page
+This page is read-only
+Cette page est en lecture seule
+View other revisions
+Voir les autres versions
+View current revision
+Voir la version actuelle
+View original
+Voir l'original
+Last edited
+Dernière modification
+Edited
+Modifié
+Warning
+ATTENTION
+Database is stored in temporary directory %s
+Base de données stockée dans le repertoire temporaire 
+%s seconds
+%s secondes
+Search:
+Rechercher:
+Replace:
+Remplacer:
+Go!
+Aller !
+Validate HTML
+Validation du HTML
+Validate CSS
+Validation des CSS
+Please go on to %s.
+Svp aller a %s
+major diff
+diff majeure
+minor diff
+diff mineure
+author diff
+diff de l'auteur
+major
+majeure
+minor
+mineure
+author
+auteur
+(The revisions are identical or unavailable.)
+(La version est identique ou n'est pas disponible)
+no other diffs
+pas d'autres diffs
+No diff available.
+Pas de diff disponible
+current revision
+version actuelle
+revision %s
+Version %s
+Difference (from revision %s
+Difference (entre les versions %s
+ to %s)
+ et %s)
+No diff available--this is the first %s revision.
+Aucune diff disponible--Ceci est la première version %s
+Difference (from prior %s revision)
+Différences (depuis les dernières versions %s)
+Changed:
+Modifiée:
+Removed:
+Suprimée:
+Added:
+Ajoutée:
+to
+à
+Bad page version (or corrupt page).
+Mauvaise version (ou page corompue) 
+cant write %s
+Impossible d'écrire %s
+Page name is too long: %s
+Nom de page trop long: %s
+Page name may not contain space characters: %s
+Le nom de la page ne peut pas contenir d'espaces: %s
+Invalid Page %s
+Page non valide %s
+Invalid Page %s (must not end with .db)
+Page non valide %s (elle ne doit pas se terminer par .db)
+Invalid Page %s (must not end with .lck)
+Page non valide %s (elle ne doit pas se terminer par .lck)
+can not make %s
+impossible de faire %s
+Unlocking
+Supression du verrou
+This operation may take several seconds...
+Cette opération peut prendre quelques secondes...
+Forced unlock of %s lock.
+Supression forcée du verrou %s.
+No unlock required.
+La suppression du verrour n'est pas necessaire.
+Can not open %s
+Ne peut pas ouvrir %s
+Editing Denied
+Modification interdite
+Editing not allowed: user, ip, or network is blocked.
+Modification interdite: l'utilisateur, l'adresse ip, ou le reseau est bloqué
+Contact the wiki administrator for more information.
+Contactez l'administrateur du wiki pour plus d'information.
+Editing not allowed: %s is read-only.
+Modification interdite: %s est en lecture seule
+Editing %s
+Modification de %s
+Editing revision %s of
+Modification de la version %s de
+Editing old revision %s.
+Modification de l'ancienne version %s.
+Saving this page will replace the latest revision with this text.
+Sauvegarder cette page remplacera la dernière version par ce texte.
+Edit Conflict!
+Conflit lors de la Modification!
+(This is a new conflict)
+(Il s'agit d'un nouveau conflit)
+Someone saved this page after you started editing.
+Quelqu'un a sauvegardé cette page après que vous ayez commencé sa modification
+The top textbox contains the saved text.
+La boîte de texte située en debut de page contient le texte sauvegardé.
+Only the text in the top textbox will be saved.
+Seul le texte dans la boîte de texte située en debut de page sera sauvegardé.
+Scroll down to see your text with conflict markers.
+Faites défiler vers le bas pour voir votre texte avec des marqueurs indiquants les conflits.
+Scroll down to see your edited text.
+Faites défiler vers le bas pour voir votre texte modifié.
+Last save time:
+Dernière date de sauvegarde:
+Current time is:
+Heure actuelle:
+Summary:
+Résumé:
+This change is a minor edit.
+Ce changement est mineur.
+Preview
+Prévisualisation
+This is the text with conflict markers:
+Voici le texte avec des marqueurs indiquant les conflits:
+This is the text you submitted:
+Voici le texte que vous avez soumis:
+Preview:
+Prévisualisation
+NOTE: This preview shows the revision of the other author.
+NOTE: Cette prévisualisation montre la version de l'autre auteur.
+Preview only, not yet saved
+Prévisualisation seulement, pas encore sauvegardée
+Password
+Mot de passe
+Your password is saved in a cookie, if you have cookies enabled. Cookies may get lost if you connect from another machine, from another account, or using another software.
+Votre mot de passe est conservé dans un cookie, si cette fonctionnalité est activée dans votre navigateur. Les cookie peuvent être perdus si vous vous reconnectez d'une autre machine, d'un autre compte, ou en utilisant un autre logiciel. 
+You are currently an administrator on this site.
+Vous êtes actuellement administrateur de ce site.
+You are currently an editor on this site.
+Vous êtes actuellement éditeur de ce site.
+You are a normal user on this site.
+Vous êtes un utilisateur normal sur ce site.
+Your password does not match any of the  administrator or editor passwords.
+Vote mot de passe ne corespond à aucun de ceux des administrateurs ou éditeurs.
+Password:
+Mot de passe:
+This site does not use admin or editor passwords.
+Ce site n'utilise pas de mot de passe pour les administrateurs ou les éditeurs.
+This operation is restricted to site editors only...
+Cette opération est réservée aux éditeurs du site seulement...
+This operation is restricted to administrators only...
+Cette opération est réservée aux administrateurs seulement...
+Index of all pages
+Index de toutes les pages
+Replaced: %s
+Remplacé: %s
+Search for: %s
+Rechercher: %s
+%s pages found:
+%s pages trouvées:
+last updated
+Dernière mise à jour
+Full Link List
+Liste complète des liens
+Complete Content
+Contenu complet
+The main page is %s.
+La page principale est %s.
+Editing not allowed for %s.
+Edition non autorisée pour %s.
+SampleUndefinedPage
+ExempleDePageNonDefinie
+%s cannot be defined.
+%s ne peut être défini.
+Sample_Undefined_Page
+Exemple_De_PageNon_Definie
+[[%s]] cannot be defined.
+[[%s]] ne peut être défini.
+Only an administrator can create %s
+Seul un administrateur peut créer %s
+Cannot find timestamp on the first line.
+Timestamp introuvable sur la première ligne.
+Could not get main lock
+Ne peut pas obtenir le verrou principal
+Anonymous
+Anonyme
+Could not get a lock to merge!
+Ne peut pas obtenir un verrou pour la fusion !
+%s log error:
+%s Erreur lors de l´écriture du journal:
+Ping
+Ping
+No response.
+Pas de réponse.
+Maintenance on all pages
+Maintenance sur toutes les pages
+Maintenance not done.
+Maintenance non effectuée.
+(Maintenance can only be done once every 12 hours.)
+(La maintenance ne peut être effectuée qu'une fois toutes les 12 heures.)
+Remove the "maintain" file or wait.
+Enlevez le fichier "maintain" ou patientez.
+Main lock obtained.
+Verrou principal obtenu
+Expiring keep files and deleting pages marked for deletion
+Expiration des fichiers de cache et suppression des pages marquées pour la suppression
+deleted
+Supprimé
+Moving part of the %s log file.
+Deplace une partie du fichier de log %s.
+Moving %s log entries.
+Déplace %s entrées du log.
+Main lock released.
+Verrou principal rendu.
+Converting all files
+Convertit tous les fichiers
+No conversion required.
+Pas de conversion requise.
+converted
+Convertie
+no conversion required
+pas de conversion requise
+has no file
+n'as pas de fichier
+Set or Remove global edit lock
+Positionne ou enlève le verrou global d'édition
+Edit lock created.
+Verrou d'édition créé.
+Edit lock removed.
+Verrou d'édition enlevé.
+Set or Remove page edit lock
+Positionne ou enlève le verrou d'édition de la page
+Missing page id to lock/unlock...
+Id de page manquant pour verrouiller/déverrouiller...
+Lock for %s created.
+Verrou pour %s créé.
+Lock for %s removed.
+Verrou pour %s enlevé.
+Displaying Wiki Version
+Affiche la version du wiki
+Too many connections by %s
+Trop de connections par %s
+Recent Visitors
+Derniers visiteurs
+%s hours ago
+il y a %s heures
+1 hour ago
+il y a une heure
+%s minutes ago
+il y a %s minutes
+1 minute ago
+il y a une minute
+%s seconds ago
+il y a %s secondes
+1 second ago
+il y a 1 seconde
+just now
+a l'instant
+Referrers
+Referrers
+All Referrers
+Tous les referrers
+anchor first defined here
+ancre d'abord définie ici
+END_OF_TRANSLATION
