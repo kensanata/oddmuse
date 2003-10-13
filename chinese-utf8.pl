def4ace80fbd87907ddf0ec84c71a3ab2fdea7af
Alex Schroeder <alex@gnu.org>
2003-10-13 18:20:51 +0000

    *** empty log message ***

---
+++ chinese-utf8.pl
@@ -0,0 +1,390 @@
+# UTF-8 encoded Chinese translation file for use with Oddmuse
+#
+# Copyright (c) 2003  wctang <wctang@csie.nctu.edu.tw>.
+#
+# Permission is granted to copy, distribute and/or modify this
+# document under the terms of the GNU Free Documentation License,
+# Version 1.2 or any later version published by the Free Software
+# Foundation; with no Invariant Sections, no Front-Cover Texts, and no
+# Back-Cover Texts.  A copy of the license could be found here:
+# http://www.gnu.org/licenses/fdl.txt
+#
+# Installation:
+# =============
+# This file should be saved into the same folder where the config file
+# resides.  Load it from the config file:
+#
+# do 'chinese-utf8.pl';
+#
+# This translation was last checked for an unknown version of Oddmuse.
+#
+%Translate = split('\n',<<END_OF_TRANSLATION);
+Reading not allowed: user, ip, or network is blocked.
+無法讀取：使用者，ip，或是網路已被阻擋。
+Could not create %s
+無法建立 %s
+Invalid UserName %s: not saved.
+無效的使用者名稱 %s：沒有儲存。
+UserName must be 50 characters or less: not saved
+使用者名稱必需小於 50 個字元：沒有儲存
+http://shop.barnesandnoble.com/bookSearch/isbnInquiry.asp?isbn=%s
+
+http://www.amazon.com/exec/obidos/ISBN=%s
+
+alternate
+輪流
+http://www.pricescan.com/books/BookDetail.asp?isbn=%s
+
+search
+搜尋
+Invalid action parameter %s
+無效的動作參數 %s
+Invalid URL.
+無效的 URL。
+Revision %s not available
+%s 的版本不存在
+showing current revision instead
+顯示目前的版本
+Showing revision %s
+顯示 %s 的版本
+Updates since %s
+自從 %s 的更新
+Updates in the last %s day
+在 %s 天內有修改的
+Updates in the last %s days
+在 %s 天內有修改的
+Could not open %s log file
+無法開啟 %s 記錄檔
+Error was
+錯誤是
+Note: This error is normal if no changes have been made.
+注意：如果沒有做任何改變的話，出現這個錯誤是正常的。
+Could not open old %s log file
+無法開啟舊的 %s 記錄檔
+for %s only
+只有 %s
+%s day
+%s 天
+%s days
+%s 天
+List new changes starting from
+列出新的改變，自
+No updates since %s
+自 %s 沒有修改
+Page generated %s
+頁面產生：%s
+(minor)
+（次要的）
+(diff)
+（差異）
+history
+歷史記錄
+History of %s
+%s 的歷史記錄
+Compare
+比較
+ . . . . 
+……
+Revision %s
+版本記錄 %s
+Edit
+編輯
+by
+由
+from %s
+從 %s
+[Home]
+首頁
+redirected from %s
+自 %s 重導向
+Cookie: 
+Cookie: 
+new comment
+新的評論
+Username:
+使用者名稱：
+Save
+儲存
+Comments on this page
+對本頁的評論
+Edit revision %s of this page
+編輯這個頁面的 %s 版本
+Edit text of this page
+編輯本頁的文字
+This page is read-only
+本頁是唯讀的
+View other revisions
+觀看其他的版本
+View current revision
+觀看目前的版本
+View original
+觀看原本的版本
+Last edited
+最後修改
+Edited
+已編輯
+Warning
+警告
+Database is stored in temporary directory %s
+資料庫已儲存在暫存記錄 %s
+%s seconds
+%s 秒
+Search:
+搜尋：
+Replace:
+取代：
+Go!
+前往！
+Validate HTML
+驗證本頁的 HTML
+Validate CSS
+驗證本頁的 CSS
+Please go on to %s.
+請前往 %s。
+major diff
+主要的修改
+minor diff
+次要的修改
+author diff
+作者修改
+major
+主要的
+minor
+次要的
+author
+作者
+(The revisions are identical or unavailable.)
+（這個版本是完全相同的或是不存在的。）
+no other diffs
+沒有其他的修改了
+No diff available.
+沒有其他的修改了。
+current revision
+目前的版本
+revision %s
+%s 版本
+Difference (from revision %s
+差異（從版本 %s
+ to %s)
+到 %s）
+No diff available--this is the first %s revision.
+沒有差異－這是第一個 %s 版本。
+Difference (from prior %s revision)
+差異（相對於先前 %s 的版本）
+Changed:
+改變：
+Removed:
+去除：
+Added:
+增加：
+to
+到
+Bad page version (or corrupt page).
+錯誤頁面版本（或是損壞的頁面）。
+cant write %s
+無法寫入 %s
+Page name is too long: %s
+頁面 %s 的名稱太長
+Page name may not contain space characters: %s
+這個頁面名稱可能沒有包含空白字元：%s
+Invalid Page %s
+無效的頁面 %s
+Invalid Page %s (must not end with .db)
+無效的頁面 %s（不允許 ".db" 結尾）
+Invalid Page %s (must not end with .lck)
+無效的頁面 %s（不允許 ".lck" 結尾）
+can not make %s
+無法產生 %s
+Unlocking
+正在開鎖
+This operation may take several seconds...
+這個操作需要花費幾秒鐘…
+Forced unlock of %s lock.
+強制解開 % 鎖定。
+No unlock required.
+不需要解除鎖定。
+Can not open %s
+無法開啟 %s
+Editing Denied
+禁止修改。
+Editing not allowed: user, ip, or network is blocked.
+不允許編輯：使用者，ip，或是網路已被攔阻了。
+Contact the wiki administrator for more information.
+通知 wiki 管理者以獲得更多的資訊。
+Editing not allowed: %s is read-only.
+不允許編輯：%s 是唯讀的。
+Editing %s
+正在編輯 %s
+Editing revision %s of
+編輯 %s 版本：
+Editing old revision %s.
+編輯先前的 %s 版本。
+Saving this page will replace the latest revision with this text.
+儲存此頁將會取代這份文件的最新版本。
+Edit Conflict!
+編輯衝突！
+(This is a new conflict)
+（這是新的衝突）
+Someone saved this page after you started editing.
+有人在你開始編輯後儲存了這個頁面。
+The top textbox contains the saved text.
+上面的編輯區有這份已儲存的文件。
+Only the text in the top textbox will be saved.
+只有上面的編輯區內的文件會被儲存。
+Scroll down to see your text with conflict markers.
+向下捲動可以看到你所編輯的文件和衝突標記。
+Scroll down to see your edited text.
+向下捲動可以看到你所編輯的文件。
+Last save time:
+最後儲存的時間：
+Current time is:
+目前的時間：
+Summary:
+摘要
+This change is a minor edit.
+這次的修改是次要的修改。
+Preview
+預覽
+This is the text with conflict markers:
+這是含有衝突標記的文件：
+This is the text you submitted:
+這是你提交的文件：
+Preview:
+預覽：
+NOTE: This preview shows the revision of the other author.
+注意：預覽中出現另一個作者的版本。
+Preview only, not yet saved
+只是預覽，還沒有儲存
+Password
+密碼
+Your password is saved in a cookie, if you have cookies enabled. Cookies may get lost if you connect from another machine, from another account, or using another software.
+如果您的 cookies 是打開的話，則您的密碼已儲存在 cookie 中。
+You are currently an administrator on this site.
+您現在是站上的管理者。
+You are currently an editor on this site.
+您現在是站上的編輯者。
+You are a normal user on this site.
+您現在是站上的使用者。
+Your password does not match any of the  administrator or editor passwords.
+您的密碼不符合任何管理者或使用者的密碼。
+Password:
+密碼
+This site does not use admin or editor passwords.
+這個站沒有使用管理者或編輯者密碼的功能。
+This operation is restricted to site editors only...
+只有站上的編輯者才能進行這個被限制的操作…
+This operation is restricted to administrators only...
+只有站上的管理者才能進行這個被限制的操作…
+Index of all pages
+所有頁面的索引
+Replaced: %s
+取代：%s
+Search for: %s
+搜尋：%s
+%s pages found:
+發現　%s 個頁面：
+last updated
+最後更新
+Full Link List
+完整的連結列表
+Complete Content
+完成內容
+The main page is %s.
+主要的頁面是 %s 。
+Editing not allowed for %s.
+不允許對 %s 的編輯。
+SampleUndefinedPage
+未定義頁面範例
+%s cannot be defined.
+%s 無法定義。
+Sample_Undefined_Page
+未定義頁面範例
+[[%s]] cannot be defined.
+[[%s]] 無法定義。
+Only an administrator can create %s
+只有管理者能建立 %s
+Cannot find timestamp on the first line.
+無法發現第一行的 timestamp。
+Could not get main lock
+無法取得主要的鎖定。
+Anonymous
+匿名者
+Could not get a lock to merge!
+無法取得合併的鎖定！
+%s log error:
+%s 記錄錯誤：
+Ping
+探測(Ping)
+No response.
+沒有回應。
+Maintenance on all pages
+所有頁面的維護
+Maintenance not done.
+維護未完成
+(Maintenance can only be done once every 12 hours.)
+（維護每 12 小時只能做一次）
+Remove the "maintain" file or wait.
+（清除 "maintain" 檔或是等待吧）
+Main lock obtained.
+已獲得主要的鎖定。
+Expiring keep files and deleting pages marked for deletion
+清除已過期和標記刪除的檔案
+deleted
+刪除
+Moving part of the %s log file.
+搬移一部分的 %s 記錄檔。
+Moving %s log entries.
+搬移 %s 記錄項目。
+Main lock released.
+已釋放主要的鎖定。
+Converting all files
+轉換所有的檔案
+No conversion required.
+不需要轉換。
+converted
+已轉換
+no conversion required
+不需要轉換
+has no file
+沒有檔案
+Set or Remove global edit lock
+設定或清除全域的編輯鎖定
+Edit lock created.
+建立編輯鎖定。
+Edit lock removed.
+清除編輯鎖定。
+Set or Remove page edit lock
+設定或清除頁面的編輯鎖定
+Missing page id to lock/unlock...
+尋找要鎖定╱解鎖的頁面 id 
+Lock for %s created.
+%s 的鎖定已建立。
+Lock for %s removed.
+%s 的鎖定已清除。
+Displaying Wiki Version
+顯非 Wiki 的版本
+Too many connections by %s
+太多來自 %s 的連線
+Recent Visitors
+最近的參觀者
+%s hours ago
+%s 小時前
+1 hour ago
+1 小時前
+%s minutes ago
+%s 分鐘前
+1 minute ago
+1 分鐘前
+%s seconds ago
+%s 秒前
+1 second ago
+1 秒前
+just now
+就是現在
+Referrers
+引述
+All Referrers
+所有的引述
+anchor first defined here
+
+END_OF_TRANSLATION
