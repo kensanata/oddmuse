f968ea634be06b87802aff4c1db6eeed99c5642c
Alex Schroeder <alex@gnu.org>
2003-10-16 23:03:57 +0000

    Added redirection tests

--- test-markup.pl
+++ test-markup.pl
@@ -130,6 +130,26 @@ close(F);
 
 # --------------------
 
+print '[redirection]';
+
+update_page('Miles_Davis', 'Featuring [[John Coltrane]]'); # plain link
+update_page('John_Coltrane', '#REDIRECT Coltrane'); # no redirect
+update_page('Sonny_Stitt', '#REDIRECT [[Stitt]]'); # redirect
+update_page('Keith_Jarret', 'Plays with [[Gary Peacock]]'); # link to perm. anchor
+update_page('Jack_DeJohnette', 'A friend of [::Gary Peacock]'); # define perm. anchor
+
+test_page(get_page('Miles_Davis'), ('Featuring', 'John Coltrane'));
+test_page(get_page('John_Coltrane'), ('<li>REDIRECT Coltrane'));
+test_page(get_page('Sonny_Stitt'),
+	  ('Status: 302', 'Location: .*wiki.pl\?action=browse;oldid=Sonny_Stitt;id=Stitt'));
+test_page(get_page('Keith_Jarret'),
+	  ('Plays with', 'wiki.pl/Gary_Peacock', 'Keith Jarret', 'Gary Peacock'));
+test_page(get_page('Gary_Peacock'),
+	  ('Status: 302', 'Location: .*wiki.pl\?Jack_DeJohnette#Gary_Peacock'));
+test_page(get_page('Jack_DeJohnette'),
+	  ('A friend of', 'Gary Peacock', 'name="Gary_Peacock"', 'class="definition"',
+	   'title="Click to search for references to this permanent anchor"'));
+
 print '[recent changes]';
 
 $host1 = 'tisch';
