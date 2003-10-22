f0148c47fc7862f9b0824e2f83e99db1673c6ee8
Alex Schroeder <alex@gnu.org>
2003-10-22 11:32:35 +0000

    new empty file

---
+++ japanese-utf8.pl
@@ -0,0 +1,422 @@
+# UTF-8 encoded Japanese translation file for use with Oddmuse
+#
+# Copyright (c) 2003  ...
+#
+# Permission is granted to copy, distribute and/or modify this
+# document under the terms of the GNU Free Documentation License,
+# Version 1.2 or any later version published by the Free Software
+# Foundation; with no Invariant Sections, no Front-Cover Texts, and no
+# Back-Cover Texts.  A copy of the license could be found at:
+# http://www.gnu.org/licenses/fdl.txt .
+#
+# Installation:
+# =============
+# This file should be saved into the same folder where the config file
+# resides.  Load it from the config file:
+#
+# do 'japanese-utf8.pl;
+#
+# This translation was last checked for Oddmuse version 1.215.
+#
+%Translate = split('\n',<<END_OF_TRANSLATION);
+Reading not allowed: user, ip, or network is blocked.
+
+Could not create %s
+
+Invalid UserName %s: not saved.
+
+UserName must be 50 characters or less: not saved
+
+http://shop.barnesandnoble.com/bookSearch/isbnInquiry.asp?isbn=%s
+
+http://www.amazon.com/exec/obidos/ISBN=%s
+
+alternate
+
+http://www.pricescan.com/books/BookDetail.asp?isbn=%s
+
+search
+
+Invalid action parameter %s
+
+Invalid URL.
+
+Page name is too long: %s
+
+Invalid Page %s
+
+Invalid Page %s (must not end with .db)
+
+Invalid Page %s (must not end with .lck)
+
+Page name may not contain space characters: %s
+
+Revision %s not available
+
+showing current revision instead
+
+Showing revision %s
+
+Could not open %s log file
+
+Error was
+
+Note: This error is normal if no changes have been made.
+
+Could not open old %s log file
+
+No updates since %s
+
+Page generated %s
+
+Updates since %s
+
+Updates in the last %s days
+
+Updates in the last %s day
+
+for %s only
+
+List latest change per page only
+
+List only major changes
+
+Include minor changes
+
+List all changes
+
+%s days
+
+List new changes starting from
+
+Related changes
+
+(minor)
+
+(diff)
+
+history
+
+rollback
+
+Cluster:
+
+from %s
+
+History of %s
+
+Compare
+
+Revision %s
+
+ . . . . 
+
+by
+
+Rolling back changes
+
+Missing target for rollback.
+
+Target for rollback is too far back.
+
+Rollback to %s
+
+%s rolled back
+
+[Home]
+
+redirected from %s
+
+Click to search for references to this page
+
+Cookie: 
+
+new comment
+
+Username:
+
+Save
+
+Comments on this page
+
+Edit revision %s of this page
+
+Edit text of this page
+
+This page is read-only
+
+View other revisions
+
+View current revision
+
+View all changes
+
+View original
+
+Last edited
+
+Edited
+
+by %s
+
+Warning
+
+Database is stored in temporary directory %s
+
+%s seconds
+
+Search:
+
+Replace:
+
+Go!
+
+Validate HTML
+
+Validate CSS
+
+Please go on to %s.
+
+major
+
+minor
+
+(The revisions are identical or unavailable.)
+
+No diff available.
+
+current revision
+
+revision %s
+
+Difference (from revision %s
+
+ to %s)
+
+No diff available--this is the first %s revision.
+
+Difference (from prior %s revision)
+
+Changed:
+
+Removed:
+
+Added:
+
+to
+
+Bad page version (or corrupt page).
+
+Cannot save an nameless page.
+
+cannot write %s
+
+Could not get %s lock
+
+Unlocking
+
+This operation may take several seconds...
+
+Forced unlock of %s lock.
+
+No unlock required.
+
+Can not open %s
+
+%s hours ago
+
+1 hour ago
+
+%s minutes ago
+
+1 minute ago
+
+%s seconds ago
+
+1 second ago
+
+just now
+
+Editing Denied
+
+Editing not allowed: user, ip, or network is blocked.
+
+Contact the wiki administrator for more information.
+
+The rule %s matched for you.
+
+See %s for more information.
+
+Editing not allowed: %s is read-only.
+
+Only administrators can upload files.
+
+Editing revision %s of
+
+Editing %s
+
+Editing old revision %s.
+
+Saving this page will replace the latest revision with this text.
+
+Summary:
+
+This change is a minor edit.
+
+Preview
+
+Replace this file with text.
+
+Replace this text with a file.
+
+Preview:
+
+Preview only, not yet saved
+
+File to upload: 
+
+Files of type %s are not allowed.
+
+Password
+
+Your password is saved in a cookie, if you have cookies enabled. Cookies may get lost if you connect from another machine, from another account, or using another software.
+
+You are currently an administrator on this site.
+
+You are currently an editor on this site.
+
+You are a normal user on this site.
+
+Your password does not match any of the  administrator or editor passwords.
+
+Password:
+
+This site does not use admin or editor passwords.
+
+This operation is restricted to site editors only...
+
+This operation is restricted to administrators only...
+
+Index of all pages
+
+Replaced: %s
+
+Search for: %s
+
+View changes for these pages
+
+and
+
+or
+
+%s pages found:
+
+last updated
+
+Full Link List
+
+Complete Content
+
+The main page is %s.
+
+Editing not allowed for %s.
+
+SampleUndefinedPage
+
+%s cannot be defined.
+
+Sample_Undefined_Page
+
+[[%s]] cannot be defined.
+
+Only an administrator can create %s
+
+Transfer Error: %s
+
+Browser reports no file info.
+
+Browser reports no file type.
+
+Anonymous
+
+This page was changed by somebody else %s.
+
+The changes conflict.  Please check the page again.
+
+Please check whether you overwrote those changes.
+
+Could not get a lock to merge!
+
+you
+
+ancestor
+
+other
+
+%s log error:
+
+Ping
+
+No response.
+
+Maintenance on all pages
+
+Maintenance not done.
+
+(Maintenance can only be done once every 12 hours.)
+
+Remove the "maintain" file or wait.
+
+Main lock obtained.
+
+Expiring keep files and deleting pages marked for deletion
+
+and refreshing HTML cache
+
+deleted
+
+Moving part of the %s log file.
+
+Moving %s log entries.
+
+Main lock released.
+
+Converting all files
+
+No conversion required.
+
+converted
+
+no conversion required
+
+has no file
+
+Set or Remove global edit lock
+
+Edit lock created.
+
+Edit lock removed.
+
+Set or Remove page edit lock
+
+Missing page id to lock/unlock...
+
+Lock for %s created.
+
+Lock for %s removed.
+
+Displaying Wiki Version
+
+Too many connections by %s
+
+Recent Visitors
+
+Referrers
+
+All Referrers
+
+anchor first defined here: %s
+
+Click to search for references to this permanent anchor
+
+END_OF_TRANSLATION
