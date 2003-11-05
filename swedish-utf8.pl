58edb3729910149aeab2c07f65f89d89a59f4b6e
Alex Schroeder <alex@gnu.org>
2003-11-05 16:25:22 +0000

    Update by Björn Lindström and Björn X Öqvist.

--- swedish-utf8.pl
+++ swedish-utf8.pl
@@ -1,6 +1,9 @@
+# -*-mode: fundamental; coding: utf-8;-*-
 # UTF-8 encoded Swedish language file for use with Oddmuse
 #
-# Copyright (c) 2003  Erik S-O Johansson and others
+# Copyright (c) 2003 Erik S-O Johansson and others
+# Copyright (c) 2003 Björn Lindström <bkhl@elektrubadur.se> and
+#                    Björn X Öqvist <zrajm@klingonska.org
 #
 # Permission is granted to copy, distribute and/or modify this
 # document under the terms of the GNU Free Documentation License,
@@ -16,17 +19,21 @@
 #
 # do 'swedish-utf8.pl;
 #
-# This translation was last checked for Oddmuse version 1.195.
+# This translation was last checked for OddMuse version 1.226.
 #
 %Translate = split('\n',<<END_OF_TRANSLATION);
+List latest change per page only
+Visa bara den senaste ändringen för varje sida
+Include minor changes
+Visa även små ändringar
 Reading not allowed: user, ip, or network is blocked.
-
+Läsning inte tillåten: användare, ip eller nätverk är blockerat.
 Could not create %s
-Det gick inte att skapa %s
+Kunde inte skapa %s
 Invalid UserName %s: not saved.
-Användarnamnet %s är ogiltigt, det sparas inte.
+Ogiltigt användarnamn %s: Ej sparad.
 UserName must be 50 characters or less: not saved
-Användarnamnet får högst vara 50 tecken: sparades inte
+Användarnamn får bestå av högst 50 tecken: Ej sparad.
 http://shop.barnesandnoble.com/bookSearch/isbnInquiry.asp?isbn=%s
 
 http://www.amazon.com/exec/obidos/ISBN=%s
@@ -42,7 +49,7 @@ Ogiltig parameter %s
 Invalid URL.
 Ogiltig URL.
 Page name is too long: %s
-Sidans namn är för långt: %s
+Sidnamn är för långt: %s
 Invalid Page %s
 Ogiltig sida %s
 Invalid Page %s (must not end with .db)
@@ -52,7 +59,7 @@ Ogiltig sida %s (får inte sluta med .lck)
 Page name may not contain space characters: %s
 Sidans namn får inte innehålla mellanslag: %s
 Revision %s not available
-Version %s är inte tillgänglig
+Version %s inte tillgänglig
 showing current revision instead
 visar nuvarande version istället
 Showing revision %s
@@ -64,13 +71,13 @@ Updates in the last %s day
 Updates in the last %s days
 Ändringar de %s senaste dagarna
 Could not open %s log file
-Det gick inte att öppna %s loggfilen
+Kunde inte öppna %s-loggen
 Error was
 Felet var
 Note: This error is normal if no changes have been made.
-Observera: Det här felet är normalt, om inga ändringar är gjorda.
+Observera: Det här felet är normalt om inga ändringar gjorts.
 Could not open old %s log file
-Det gick inte att öppna den gamla %s loggfilen
+Kunde inte öppna gammal %s-loggfil
 for %s only
 endast för %s
 %s day
@@ -84,17 +91,17 @@ Inga ändringar sedan %s
 Page generated %s
 Sidan skapad %s
 Related changes
-
+Relaterade ändringar
 (minor)
 (mindre)
 (diff)
-(visa ändringar)
+(ändringar)
 history
 historik
 rollback
-
+återställning
 Cluster:
-
+Kluster:
 from %s
 från %s
 History of %s
@@ -108,19 +115,19 @@ Version %s
 by
 av
 Rolling back changes
-
+Återställer
 Missing target for rollback.
-
+Mål för återställning saknas
 Target for rollback is too far back.
-
+Mål för återstållning för gammalt.
 Rollback to %s
-
+Återställning till %s
 %s rolled back
-
+%s återställd
 [Home]
 [Startsida]
 redirected from %s
-automatiskt hitskickad från %s
+omdirigerad från %s
 Cookie: 
 
 new comment
@@ -130,39 +137,39 @@ Användarnamn:
 Save
 Spara
 Comments on this page
-Kommentarer på denna sida
+Kommentarer till den här sidan
 Edit revision %s of this page
-Ändra version %s av den här sidan
+Redigera version %s av den här sidan
 Edit text of this page
-Ändra texten på den här sidan
+Redigera den här sidan
 This page is read-only
-Den här sidan går inte att ändra
+Den här sidan är skrivskyddad
 View other revisions
-Se andra versioner
+Visa andra versioner
 View current revision
-Se aktuell version
+Visa nuvarande version
 View all changes
-
+Visa alla ändringar
 View original
 Visa original
 Last edited
-Senast ändrad
+Senast redigerad
 Edited
-Ändrad
+Redigerad
 by %s
-
+av %s
 Warning
 Varning
 Database is stored in temporary directory %s
-Databasen är sparad i den tillfälliga katalogen %s
+Databas sparad i tillfällig katalog %s
 %s seconds
 %s sekunder
 Search:
-Sök:
+Sökning:
 Replace:
 Ersätt:
 Go!
-Kör!
+Sök
 Validate HTML
 Validera HTML
 Validate CSS
@@ -174,121 +181,121 @@ stor
 minor
 liten
 (The revisions are identical or unavailable.)
-(Versionerna är likadana eller inte tillgängliga)
+(Versionerna är identiska eller otillgängliga)
 No diff available.
-Det finns ingen ändring.
+Ingen ändring tillgänglig.
 current revision
 nuvarande revision
 revision %s
-
+version %s
 Difference (from revision %s
 Skillnad (mellan revision %s
  to %s)
  och %s)
 No diff available--this is the first %s revision.
-Det finns inga andra versioner tillgängliga -- det här är första versionen av %s.
+Det finns inga tidigare ändringar -- det här är den första versionen (%s ändring).
 Difference (from prior %s revision)
-Ändringar (från tidigare versionen %s)
+Ändringar (sedan tidigare version %s)
 Changed:
 Ändrad:
 Removed:
 Borttagen:
 Added:
-Adderad:
+Tillagd:
 to
 till
 Bad page version (or corrupt page).
 Felaktig version av sidan (eller trasig sida)
 Cannot save an nameless page.
-
+Kan inte spara en namnlös sida.
 cannot write %s
-
+kan inte skriva %s
 Could not get %s lock
-
+Kunde inte låsa %s
 Unlocking
-Upplåsning
+Låser upp
 This operation may take several seconds...
-Den här operationen kan ta flera sekunder...
+Den här funktionen kan ta flera sekunder...
 Forced unlock of %s lock.
-Tvingad upplåsning av låset på %s.
+Forcerad upplåsning av %s.
 No unlock required.
-Upplåsning behövs ej.
+Ingen upplåsning behövs.
 Can not open %s
 Kan inte öppna %s
 %s hours ago
 för %s timmar sedan
 1 hour ago
-för en timme sedan
+för 1 timme sedan
 %s minutes ago
 %s minuter sedan
 1 minute ago
-en minut sedan
+1 minut sedan
 %s seconds ago
 %s sekunder sedan
 1 second ago
-en sekund sedan
+1 sekund sedan
 just now
-precis nu
+just nu
 Editing Denied
-Ändringen är inte tillåten
+Redigering nekas
 Editing not allowed: user, ip, or network is blocked.
-Ändringen är inte tillåten: användare, ip-adress eller nätverk är blockerat.
+Redigering inte tillåten: användare, ip-adress eller nätverk är blockerat.
 Contact the wiki administrator for more information.
-För mer information, kontakta wiki-administratören.
+Kontakta wiki-administratören för mer information.
 The rule %s matched for you.
-
+Regeln %s passar in på dig.
 See %s for more information.
-
+Se %s för mer information.
 Editing not allowed: %s is read-only.
-Ändring ej tillåten: %s är bara till för läsning och går inte att ändra
+Redigering är inte tillåten: %s är skrivskyddad.
 Only administrators can upload files.
-
+Bara administratörer kan ladda upp filer.
 Editing revision %s of
-Ändrar version %s av
+Redigerar version %s av
 Editing %s
-Ändrar %s
+Redigerar %s
 Editing old revision %s.
-Ändrar den gamla versionen %s.
+Redigerar gammal version %s.
 Saving this page will replace the latest revision with this text.
-Sparande av den här sidan kommer att ersätta den senaste versionen med den här texten.
+Att spara den här sidan kommer att ersätta den senaste versionen med den här texten.
 Summary:
 Sammanfattning:
 This change is a minor edit.
-Detta är en mindre ändring.
+Den här förändringen är en mindre ändring.
 Preview
 Förhandsgranska
 Replace this file with text.
-
+Ersätt den här filen med text.
 Replace this text with a file.
-
+Ersätt den här texten med en fil.
 Preview:
 Förhandsgranskning:
 Preview only, not yet saved
-Det här är bara en förhandsgranskning, det är inte sparat ännu
+Endast förhandsgranskning, ännu inte sparad
 File to upload: 
-
+Fil att ladda upp:
 Files of type %s are not allowed.
-
+Filer av typen %s är inte tillåtna.
 Password
 Lösenord
 Your password is saved in a cookie, if you have cookies enabled. Cookies may get lost if you connect from another machine, from another account, or using another software.
-
+Ditt lösenord sparas i en cookie, om du har cookies tillgängliga. Cookies kan försvinna om du ansluter från en annan dator, från ett annat konto, eller med annan programvara.
 You are currently an administrator on this site.
-Du är just nu administratör på denna sajt.
+Du är för närvarande administratör av den här webbplatsen.
 You are currently an editor on this site.
-Du är just nu editor på denna sajt.
+Du är för närvarande redaktör på den här webbplatsen.
 You are a normal user on this site.
-Du är en normal användare på denna sajt.
+Du är en normal användare på den här webbplatsen.
 Your password does not match any of the  administrator or editor passwords.
-Ditt lösenord matchar inget av administratör- eller redaktörlösenorden.
+Ditt lösenord motsvarar inget av admininistratörs- eller redaktörslösenorden.
 Password:
 Lösenord:
 This site does not use admin or editor passwords.
-Denna sajt använder inte administratör- eller redaktörlösenord.
+Den här webbplatsen använder inte administratörs- eller redaktörslösenord.
 This operation is restricted to site editors only...
-Den här operationen får bara redaktörerna utföra.
+Den här funktionen kan bara utföras av redaktörer...
 This operation is restricted to administrators only...
-Den här operationen får bara administratörerna utföra.
+Den här funktionen kan bara utföras av adminstratörer...
 Index of all pages
 Index över alla sidor
 Replaced: %s
@@ -296,85 +303,85 @@ Ersatt: %s
 Search for: %s
 Sök efter: %s
 and
-
+och
 or
-
+eller
 %s pages found:
-%s sidor hittade:
+%s sidor funna:
 last updated
-senast uppdaterad
+senast reviderad
 Full Link List
-Lista över alla länkar
+Fullständing länklista
 Complete Content
-Hela innehållet
+Fullständigt innehåll
 The main page is %s.
 Huvudsidan är %s.
 Editing not allowed for %s.
-Ändringar av %s är inte tillåtna.
+%s kan inte redigeras.
 SampleUndefinedPage
-ExempelNamnlösSida
+ProvSida
 %s cannot be defined.
 %s kan inte definieras.
 Sample_Undefined_Page
-Exempel_Namnlös_Sida
+Prov_Sida
 [[%s]] cannot be defined.
 [[%s]] kan inte definieras.
 Only an administrator can create %s
-Bara en administratör can skala %s
+Bara en administratör can skapa %s
 Transfer Error: %s
-
+Överföringsfel: %s
 Browser reports no file info.
-
+Webbläsare rapporterar ingen filinformation.
 Browser reports no file type.
-
+Webbläsare rapporterar ingen filtyp.
 Anonymous
 Anonym
 This page was changed by somebody else %s.
-
+Den här sidan ändrades av någon annan %s.
 The changes conflict.  Please check the page again.
-
+Ändringarna står i konflikt. Kontrollera sidan igen.
 Please check whether you overwrote those changes.
-
+Kontrollera om du skrev över de ändringarna.
 Could not get a lock to merge!
-Kunde inte få tag i ett lås för att merga!
+Kunde inte låsa för att slå samman!
 you
-
+du
 ancestor
-
+förfader
 other
-
+annan
 %s log error:
 %s logg-fel:
 Ping
-
+Ping
 No response.
 Inget svar.
 Maintenance on all pages
 Underhåll av alla sidor
 Maintenance not done.
-Underhåll inte utfört.
+Underhåll är inte färdigt.
 (Maintenance can only be done once every 12 hours.)
-(Underhåll kan bara göras en gång var tolfte timme.)
+(Underhåll kan bara utföras en gång var 12:e timme.)
 Remove the "maintain" file or wait.
 Ta bort filen "maintain" eller vänta
 Main lock obtained.
-Fick tag på huvudlås.
+Huvudlås påslaget.
 Expiring keep files and deleting pages marked for deletion
 
 and refreshing HTML cache
-
+och förnyar HTML-cache
 deleted
 borttagen
 Moving part of the %s log file.
-Flyttar del av %s logfil
+Flyttar del av %s-loggen
 Moving %s log entries.
-
+Flyttar %s loggnotering.
 Main lock released.
-Huvudlås släppt.
+Huvudlås avslaget.
 Converting all files
-Konverterar all filer
+Konverterar alla filer
 No conversion required.
-Ingen konvertering nödvändig
+Ingen konvertering nödvändig.
 converted
 konverterad
 no conversion required
@@ -382,29 +389,33 @@ ingen konvertering nödvändig
 has no file
 har ingen fil
 Set or Remove global edit lock
-Ställ eller ta bort global ändringsspärr
+Slå på eller av globalt redigeringslås
 Edit lock created.
-En ändringsspärr är skapad.
+Redigeringslås påslaget.
 Edit lock removed.
-Ändringsspärren är borttagen.
+Redigeringslås avslaget.
 Set or Remove page edit lock
-Ställ eller ta bort sidändringsspärr.
+Slå på eller av sidredigeringslås
 Missing page id to lock/unlock...
-Sid-id för att spärra/låsa upp saknas.
+Saknar sid-id, kan inte slå på eller av redigeringslås...
 Lock for %s created.
-En spärr för %s är skapad.
+Slog på redigeringslås för %s.
 Lock for %s removed.
-Spärr för %s har blivit borttagen.
+Slog av redigeringslås för för %s.
 Displaying Wiki Version
 Visar Wikiversion
 Too many connections by %s
-För många uppkopplingar från %s
+För många anslutningar ifrån %s
 Recent Visitors
 Senaste besökare
 Referrers
-
+Sidor som länkat hit
 All Referrers
-
+Alla som länkat hit
 anchor first defined here: %s
-
+ankare definierades här först: %s
+List all changes
+Visa alla ändringar
+List only major changes
+Visa bara stora ändringar
 END_OF_TRANSLATION
