81a593fa817ea2a63251b95abdb239dac9a50bef
Alex Schroeder <alex@gnu.org>
2003-11-15 13:29:11 +0000

    Update by Chusslove Illich.

--- serbian-utf8.pl
+++ serbian-utf8.pl
@@ -1,6 +1,7 @@
 # UTF-8 encoded Serbian translation file for use with Oddmuse
 #
 # Copyright (c) 2003  Unknown
+# Copyright (c) 2003  Chusslove Illich
 #
 # Permission is granted to copy, distribute and/or modify this document
 # under the terms of the GNU Free Documentation License, Version 1.2
@@ -19,13 +20,13 @@
 #
 %Translate = split('\n',<<END_OF_TRANSLATION);
 Reading not allowed: user, ip, or network is blocked.
-
+Читање није дозвољено: приступ за корисника, IP број или мрежу је блокиран.
 Could not create %s
-Није могуе креирати %s
+Не могу да направим %s
 Invalid UserName %s: not saved.
-Неисправно КорисничкоИме %s: није снимљено.
+Неисправно корисничко име %s: није снимљено.
 UserName must be 50 characters or less: not saved
-
+Корисничко име мора имати 50 или мање знакова: није снимљено.
 http://shop.barnesandnoble.com/bookSearch/isbnInquiry.asp?isbn=%s
 
 http://www.amazon.com/exec/obidos/ISBN=%s
@@ -37,7 +38,7 @@ http://www.pricescan.com/books/BookDetail.asp?isbn=%s
 search
 претрага
 Invalid action parameter %s
-Неисправан параметар за акцију %s
+Неисправан параметар акције %s
 Invalid URL.
 Неисправан URL.
 Page name is too long: %s
@@ -45,11 +46,11 @@ Page name is too long: %s
 Invalid Page %s
 Неисправна страница %s
 Invalid Page %s (must not end with .db)
-Неисправна страница %s (не сме имати екстензију .db)
+Неисправна страница %s (не сме имати наставак .db)
 Invalid Page %s (must not end with .lck)
-Неисправна страница %s (Неисправна страница .lck)
+Неисправна страница %s (не сме имати наставак .lck)
 Page name may not contain space characters: %s
-Име странице не може садржати знаке за размак: %s
+Име странице не може садржати размаке: %s
 Revision %s not available
 Верзија %s није доступна
 showing current revision instead
@@ -63,13 +64,13 @@ Updates in the last %s day
 Updates in the last %s days
 Измене у последњих %s дана
 Could not open %s log file
-Не може се отворити дневник %s 
+Нисам могао да отворим дневничку датотеку %s
 Error was
 Грешка
 Note: This error is normal if no changes have been made.
 Пажња: Ова грешка је уобичајена уколико ништа нисте изменили.
 Could not open old %s log file
-Не може се отворити стари дневник %s 
+Нисам могао да отворим стару дневничку датотеку %s
 for %s only
 само за %s
 %s day
@@ -83,215 +84,215 @@ No updates since %s
 Page generated %s
 Страница генерисана %s
 Related changes
-
+Повезане измене
 (minor)
-
+(ситна)
 (diff)
 (разлике)
 history
-
+историјат
 rollback
-
+повратак
 Cluster:
-
+Јато:
 from %s
 од %s
 History of %s
-Историја %s
+Историјат за %s
 Compare
-
+Упореди
 Revision %s
 Верзија %s
  . . . . 
 
 by
-
+—
 Rolling back changes
-
+Враћам измене
 Missing target for rollback.
-
+Недостаје циљ за повратак.
 Target for rollback is too far back.
-
+Циљ за повратак је предалеко уназад.
 Rollback to %s
-
+Повратак на %s
 %s rolled back
-
+%s је враћен
 [Home]
 [Кућа]
 redirected from %s
 преусмерено са %s
 Cookie: 
-
+Колачић: 
 new comment
-
+нови коментар
 Username:
-
+Корисничко име:
 Save
-Сними
+Сачувај
 Comments on this page
 Коментари на овој страни
 Edit revision %s of this page
-Измени ревизију %s ове странице
+Уреди верзију %s ове странице
 Edit text of this page
-Измени текст ове странице
+Уреди текст ове странице
 This page is read-only
-Ова страница се може само читати
+Ова страница је само за читање
 View other revisions
-Погледај остале верзије
+Прикажи остале верзије
 View current revision
-Погледај тренутну верзију
+Прикажи тренутну верзију
 View all changes
-
+Прикажи све измене
 View original
-
+Прикажи првобитно
 Last edited
-Последња измена
+Последње уређивање
 Edited
-Измењено
+Уређено
 by %s
-
+— %s
 Warning
-Пажња
+Упозорење
 Database is stored in temporary directory %s
 База је снимљена у привремени директоријум %s
 %s seconds
-
+%s секунди
 Search:
-Претрага:
+Пронађи:
 Replace:
-Замена:
+Замени:
 Go!
-
+Иди!
 Validate HTML
-
+Провери HTML
 Validate CSS
-
+Провери CSS
 Please go on to %s.
-
+Настави до %s.
 major
-главни
+крупна
 minor
-споредно
+ситна
 (The revisions are identical or unavailable.)
 (Верзије су или идентичне или недоступне.)
 No diff available.
-нема података о верзијама
+Разлика није доступна.
 current revision
-
+текућа верзија
 revision %s
-
+верзија %s
 Difference (from revision %s
-
+Разлика (од верзије %s
  to %s)
-
+ до %s)
 No diff available--this is the first %s revision.
-Нема верзија -- ово је прва ревизија %s.
+Разлика није доступна — ово је прва верзија %s.
 Difference (from prior %s revision)
 Разлика (текућа верзија у односу на претходну - %s)
 Changed:
-Промењено:
+Измењено:
 Removed:
-Обрисано:
+Уклоњено:
 Added:
 Додато:
 to
-
+у
 Bad page version (or corrupt page).
-Плохая версия страницы (или испорченая).
+Лоша верзија стране (или је страна искварена).
 Cannot save an nameless page.
-
+Не могу да снимим безимену страну.
 cannot write %s
-
+не могу да запишем %s
 Could not get %s lock
-
+Нисам могао да добавим браву %s
 Unlocking
 Откључавам
 This operation may take several seconds...
 Ова операција може потрајати неколико секунди...
 Forced unlock of %s lock.
-
+Форсирано откључавање браве %s.
 No unlock required.
-
+Откључавање није неопходно.
 Can not open %s
-Не може се отворити %s
+Не могу да отворим %s
 %s hours ago
-
+пре %s часова
 1 hour ago
-
+пре 1 часа
 %s minutes ago
-
+пре %s минута
 1 minute ago
-
+пре 1 минута
 %s seconds ago
-
+пре %s секунди
 1 second ago
-
+пре 1 секунде
 just now
-
+управо сада
 Editing Denied
-Измене нису дозвољене
+Уређивање је одбијено
 Editing not allowed: user, ip, or network is blocked.
-Измене нису дозвољене: приступ за корисника, IP број или мрежу није одобрен.
+Уређивање није дозвољено: приступ за корисника, IP број или мрежу је блокиран.
 Contact the wiki administrator for more information.
-Обратите се администратору за више информација
+Обратите се администратору за више информација.
 The rule %s matched for you.
 
 See %s for more information.
-
+Погледајте %s за више информација.
 Editing not allowed: %s is read-only.
-Измене нису дозвољене: %s се може само читати.
+Уређивање није дозвољено: %s је само за читање.
 Only administrators can upload files.
-
+Само администратори могу да каче датотеке.
 Editing revision %s of
-Измена верзије %s
+Уређује се верзија %s
 Editing %s
-Измена %s
+Уређивање %s
 Editing old revision %s.
-Измена старе верзије %s.
+Уређивање старе верзије %s.
 Saving this page will replace the latest revision with this text.
 Снимање ове странице ће преписати последњу верзију новим текстом.
 Summary:
-Сиже:
+Сажетак:
 This change is a minor edit.
-Ова измена је секундарна.
+Ово је ситна измена.
 Preview
-Предглед
+Преглед
 Replace this file with text.
-
+Замени ову датотеку текстом.
 Replace this text with a file.
-
+Замени овај текст датотеком.
 Preview:
-Предглед:
+Преглед:
 Preview only, not yet saved
-Само предглед, ништа није записано
+Ово је само преглед, измена још увек није записана
 File to upload: 
-
+Датотека коју треба окачити: 
 Files of type %s are not allowed.
-
+Датотеке типа %s нису дозвољене.
 Password
-
+Лозинка
 Your password is saved in a cookie, if you have cookies enabled. Cookies may get lost if you connect from another machine, from another account, or using another software.
-
+Ваша лозинка је снимљена у колачићу, ако су укључени. Колачићи могу бити изгубљени ако се повежете са друге машине, са другог налога, или користећи други програм.
 You are currently an administrator on this site.
-
+Тренутно сте администратор на овом сајту.
 You are currently an editor on this site.
-
+Тренутно сте уредник на овом сајту.
 You are a normal user on this site.
-
+Ви сте нормални корисник на овом сајту.
 Your password does not match any of the  administrator or editor passwords.
-
+Ваша лозинка се не поклапа ни са једном од аминистраторских или уредничких лозинки.
 Password:
 Лозинка:
 This site does not use admin or editor passwords.
-
+Овај сајт не користи администраторске или уредничке лозинке.
 This operation is restricted to site editors only...
-Ово смеју да раде само аутори...
+Ово смеју да раде само уредници сајта...
 This operation is restricted to administrators only...
 Ово сме да ради само администратор...
 Index of all pages
 Списак свих страна:
 Replaced: %s
-
+Замењено: %s
 Search for: %s
 Тражи: %s
 and
@@ -301,109 +302,109 @@ or
 %s pages found:
 %s стране пронађене:
 last updated
-
+последњи пут ажурирана
 Full Link List
-Потпуни списак
+Потпуни списак веза
 Complete Content
 Потпуни садржај
 The main page is %s.
-
+Главна страна је %s.
 Editing not allowed for %s.
-Измене нису дозвољене за %s.
+Уређивање није дозвољено за %s.
 SampleUndefinedPage
 НенаписанаСтраницаЗаПример
 %s cannot be defined.
-%s се не може дефинисато.
+%s се не може дефинисати.
 Sample_Undefined_Page
 Ненаписана_Страница_За_Пример
 [[%s]] cannot be defined.
-[[%s]] се не може дефинисати
+[[%s]] се не може дефинисати.
 Only an administrator can create %s
-
+Само администратор може да направи %s
 Transfer Error: %s
-
+Грешка у преносу: %s
 Browser reports no file info.
-
+Прегледач не пријављује информације о датотеци.
 Browser reports no file type.
-
+Прегледач не пријављује тип датотеке.
 Anonymous
-
+Анонимони
 This page was changed by somebody else %s.
-
+Ову страницу је изменио неко други %s.
 The changes conflict.  Please check the page again.
-
+Измене су у сукобу. Проверите страну поново.
 Please check whether you overwrote those changes.
-
+Проверите да нисте пребрисали ове те измене.
 Could not get a lock to merge!
-
+Нисам могао да добавим браву за стапање!
 you
-
+ви
 ancestor
-
+предак
 other
-
+други
 %s log error:
-
+%s дневничка грешка:
 Ping
-
+Пинг
 No response.
-
+Нема одзива
 Maintenance on all pages
-
+Одржавање на свим странама
 Maintenance not done.
-
+Одржавање није урађено.
 (Maintenance can only be done once every 12 hours.)
-
+(Одржавање може да се уради једном сваких 13 часова.)
 Remove the "maintain" file or wait.
-
+Уклони датотеку одржавања или чекај.
 Main lock obtained.
-
+Главна брава је добављена.
 Expiring keep files and deleting pages marked for deletion
-
+Истичем датотеке чувања и бришем странице означене за брисање
 and refreshing HTML cache
-
+и освежавам HTML кеш
 deleted
-
+обрисано
 Moving part of the %s log file.
-
+Премештам део дневничке датотеке %s.
 Moving %s log entries.
-
+Премештам %s дневничких ставки.
 Main lock released.
-
+Главна брава је отпуштена.
 Converting all files
-
+Конвертујем све датотеке
 No conversion required.
-
+Конверзија није неопходна.
 converted
-
+конвертовано
 no conversion required
-
+конверзија није неопходна
 has no file
-
+нема датотеке
 Set or Remove global edit lock
-
+Постави или уклони глобалну браву за уређивање
 Edit lock created.
-
+Брава за уређивање је направљена.
 Edit lock removed.
-
+Брава за уређивање је уклоњена.
 Set or Remove page edit lock
-
+Постави или уклони браву за уређивање стране
 Missing page id to lock/unlock...
-
+Недостаје ид. стране коју треба откључати/закључати
 Lock for %s created.
-
+Брава за %s је направљена.
 Lock for %s removed.
-
+Брава за %s је уклоњена.
 Displaying Wiki Version
-
+Приказујем верзију Викија
 Too many connections by %s
-
+Превише веза од %s
 Recent Visitors
-
+Скори посетиоци
 Referrers
-
+Референце
 All Referrers
-
+Све референце
 anchor first defined here: %s
-
+сидро је прво дефинисано овде: %s
 END_OF_TRANSLATION
