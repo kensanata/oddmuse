9c514670ee400d1237a49d722a64194dc37db5b6
Alex Schroeder <alex@gnu.org>
2004-01-28 01:00:36 +0000

    *** empty log message ***

---
+++ weblog-1.pl
@@ -0,0 +1,27 @@
+use vars qw($WeblogTextLogo $WeblogXmlLogo);
+
+$WeblogXmlLogo = '/images/rss.png';
+$WeblogTextLogo = '/images/txt.png';
+
+$ModulesDescription .= '<p>$Id: weblog-1.pl,v 1.1 2004/01/28 01:00:36 as Exp $</p>';
+
+*OldWeblog1InitRequest = *InitRequest;
+*InitRequest = *NewWeblog1InitRequest;
+
+sub NewWeblog1InitRequest {
+  OldWeblog1InitRequest();
+  if (GetParam('blog', 1)) { # language independent!
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday) = gmtime(time);
+    $today = sprintf("%d-%02d-%02d", $year + 1900, $mon + 1, $mday);
+    ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday) = gmtime(time - 60*60*24);
+    $yesterday = sprintf("%d-%02d-%02d", $year + 1900, $mon + 1, $mday);
+    push(@UserGotoBarPages, 'Blog', $today, $yesterday);
+  }
+}
+
+$UserGotoBar =
+  ScriptLink('action=rss',
+	     "<img src=\"$WeblogXmlLogo\" alt=\"XML\" class=\"XML\" />")
+  . ' | '
+  . ScriptLink('action=rc&amp;raw=1',
+	       "<img src=\"$WeblogTextLogo\" alt=\"TXT\" class=\"XML\" />");
