af38c76372c1b53041dd29c8dcf1b2b8587539e5
Alex Schroeder <alex@gnu.org>
2004-01-28 01:22:53 +0000

    use scriptlink

--- weblog-1.pl
+++ weblog-1.pl
@@ -3,13 +3,13 @@ use vars qw($WeblogTextLogo $WeblogXmlLogo);
 $WeblogXmlLogo = '/images/rss.png';
 $WeblogTextLogo = '/images/txt.png';
 
-$ModulesDescription .= '<p>$Id: weblog-1.pl,v 1.5 2004/01/28 01:17:23 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: weblog-1.pl,v 1.6 2004/01/28 01:22:53 as Exp $</p>';
 
-*OldWeblog1InitRequest = *InitRequest;
-*InitRequest = *NewWeblog1InitRequest;
+*OldWeblog1InitVariables = *InitVariables;
+*InitVariables = *NewWeblog1InitVariables;
 
-sub NewWeblog1InitRequest {
-  OldWeblog1InitRequest();
+sub NewWeblog1InitVariables {
+  OldWeblog1InitVariables();
   if (GetParam('blog', 1)) { # language independent!
     my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday) = gmtime(time);
     $today = sprintf("%d-%02d-%02d", $year + 1900, $mon + 1, $mday);
@@ -17,10 +17,11 @@ sub NewWeblog1InitRequest {
     $yesterday = sprintf("%d-%02d-%02d", $year + 1900, $mon + 1, $mday);
     # this modification is not mod_perl safe!
     push(@UserGotoBarPages, T('Blog'), $today, $yesterday);
-    $UserGotoBar .= "<a href=\"$ScriptName?action=rss\">"
-      . "<img src=\"$WeblogXmlLogo\" alt=\"XML\" class=\"XML\" /></a>"
+    $UserGotoBar .=
+      ScriptLink('action=rss',
+		 "<img src=\"$WeblogXmlLogo\" alt=\"XML\" class=\"XML\" />")
       . ' | '
-      . "<a href=\"$ScriptName?action=rc&amp;raw=1\">"
-      . "<img src=\"$WeblogTextLogo\" alt=\"TXT\" class=\"XML\" /></a>";
+      . ScriptLink('action=rc;raw=1',
+		   "<img src=\"$WeblogTextLogo\" alt=\"TXT\" class=\"XML\" />");
   }
 }
