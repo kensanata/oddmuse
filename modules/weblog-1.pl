3295f8328ad29ba289be499123de5da744b2bb99
Alex Schroeder <alex@gnu.org>
2004-01-30 21:35:44 +0000

    added copyright and license

--- modules/weblog-1.pl
+++ modules/weblog-1.pl
@@ -1,9 +1,27 @@
+# Copyright (C) 2004  Alex Schroeder <alex@emacswiki.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
+
 use vars qw($WeblogTextLogo $WeblogXmlLogo);
 
 $WeblogXmlLogo = '/images/rss.png';
 $WeblogTextLogo = '/images/txt.png';
 
-$ModulesDescription .= '<p>$Id: weblog-1.pl,v 1.1 2004/01/30 11:53:18 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: weblog-1.pl,v 1.2 2004/01/30 21:35:44 as Exp $</p>';
 
 $RefererTracking = 1;
 $CommentsPrefix = 'Comments_on_';
