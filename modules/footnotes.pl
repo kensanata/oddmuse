abe9489519353eb102bb7c0507780688db2ece08
Alex Schroeder <alex@gnu.org>
2004-01-30 11:53:18 +0000

    moved from top directory

---
+++ modules/footnotes.pl
@@ -0,0 +1,37 @@
+$ModulesDescription .= '<p>$Id: footnotes.pl,v 1.1 2004/01/30 11:53:18 as Exp $</p>';
+
+my $MyFootnoteCounter = 0;
+my @MyFootnotes = ();
+
+push(@MyRules, \&FootnoteRule);
+
+sub FootnoteRule {
+  if (m/\G(\[\[\[(.*?)\]\]\])/gcs) {
+    Dirty($1);
+    push(@MyFootnotes,$2);
+    $MyFootnoteCounter++;
+    print $q->a({-href=>'#'.$MyFootnoteCounter,
+		 -name=>'f'.$MyFootnoteCounter,
+		 -title=>$2,
+		 -class=>'footnote'},
+		$MyFootnoteCounter);
+  }
+  return '';
+}
+
+*OldFootnotePrintFooter = *PrintFooter;
+*PrintFooter = *NewFootnotePrintFooter;
+
+sub NewFootnotePrintFooter {
+  my @params = @_;
+  if ($MyFootnoteCounter) {
+    print '<div class="footnotes">' . $q->hr(), $q->p(T('Footnotes:')) . '<p>';
+    for (my $i = 1; $i <= $MyFootnoteCounter; $i++) {
+      print "<br />" if $i > 1;
+      print $q->a({-name=>$i, -href=>'#f'.$i}, $i . '.' ) . ' ';
+      ApplyRules(shift(@MyFootnotes), 1);
+    }
+    print '</p></div>';
+  }
+  OldFootnotePrintFooter(@params);
+}
---
+++ modules/weblog-1.pl
@@ -0,0 +1,34 @@
+use vars qw($WeblogTextLogo $WeblogXmlLogo);
+
+$WeblogXmlLogo = '/images/rss.png';
+$WeblogTextLogo = '/images/txt.png';
+
+$ModulesDescription .= '<p>$Id: weblog-1.pl,v 1.1 2004/01/30 11:53:18 as Exp $</p>';
+
+$RefererTracking = 1;
+$CommentsPrefix = 'Comments_on_';
+$EditAllowed = 2;
+
+*OldWeblog1InitVariables = *InitVariables;
+*InitVariables = *NewWeblog1InitVariables;
+
+sub NewWeblog1InitVariables {
+  OldWeblog1InitVariables();
+  if (GetParam('blog', 1)) { # language independent!
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday) = gmtime(time);
+    $today = sprintf("%d-%02d-%02d", $year + 1900, $mon + 1, $mday);
+    ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday) = gmtime(time - 60*60*24);
+    $yesterday = sprintf("%d-%02d-%02d", $year + 1900, $mon + 1, $mday);
+    # this modification is not mod_perl safe!
+    my $blog = T('Blog');
+    push(@UserGotoBarPages, $blog) unless grep (/^$blog$/, @UserGotoBarPages);
+    push(@UserGotoBarPages, $today) unless grep (/^$today$/, @UserGotoBarPages);
+    push(@UserGotoBarPages, $yesterday) unless grep (/^$yesterday$/, @UserGotoBarPages);
+    $UserGotoBar .=
+      ScriptLink('action=rss',
+		 "<img src=\"$WeblogXmlLogo\" alt=\"XML\" class=\"XML\" />")
+      . ' | '
+      . ScriptLink('action=rc;raw=1',
+		   "<img src=\"$WeblogTextLogo\" alt=\"TXT\" class=\"XML\" />");
+  }
+}
