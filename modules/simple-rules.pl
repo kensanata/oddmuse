13a51ce1937871539030812df3f85b480e51b32c
Alex Schroeder <alex@gnu.org>
2004-01-30 13:10:46 +0000

    *** empty log message ***

--- modules/simple-rules.pl
+++ modules/simple-rules.pl
@@ -2,7 +2,7 @@ use vars qw($StrictSeTextRules);
 
 $StrictSeTextRules = 0;
 
-$ModulesDescription .= '<p>$Id: simple-rules.pl,v 1.4 2004/01/30 13:09:25 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: simple-rules.pl,v 1.5 2004/01/30 13:10:46 as Exp $</p>';
 
 *ApplyRules = *NewSimpleRulesApplyRules;
 
@@ -36,9 +36,9 @@ sub NewSimpleRulesApplyRules {
     if (!$StrictSeTextRules) {
       $block =~ s/\/\/(.+?)\/\//$q->em($1)/seg;
       $block =~ s/__(.+?)__/$q->u($1)/seg;
-      $block =~ s/\*([^* \t]+)\*/$q->b($1)/seg;
-      $block =~ s/\/([^\/ \t]+)\//$q->i($1)/seg;
-      $block =~ s/_([^_ \t]+)_/$q->u($1)/seg;
+      $block =~ s/\*([^<>\* \t]+)\*/$q->b($1)/seg;
+      $block =~ s/\/([^<>\/ \t]+)\//$q->i($1)/seg;
+      $block =~  s/_([^<>\_ \t]+)_/$q->u($1)/seg;
     }
     if ($locallinks) {
       ($block =~ s/(\[\[$FreeLinkPattern\]\])/
