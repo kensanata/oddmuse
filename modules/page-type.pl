562a0d44d465ac37cb3a7869348da284728eae9b
Alex Schroeder <alex@gnu.org>
2004-01-30 21:35:44 +0000

    added copyright and license

--- modules/page-type.pl
+++ modules/page-type.pl
@@ -1,3 +1,21 @@
+# Copyright (C) 2004  Alex Schroeder <alex@emacswiki.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
+
 use vars qw($PageTypesName);
 
 # You need to define the available types on the following page.
@@ -16,7 +34,7 @@ $PageTypesName = 'PageTypes';
 # have page clustering enabled (see the manual), then the page type
 # will automatically act as a cluster.
 
-$ModulesDescription .= '<p>$Id: page-type.pl,v 1.1 2004/01/30 11:53:18 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: page-type.pl,v 1.2 2004/01/30 21:35:44 as Exp $</p>';
 
 *OldPageTypeDoPost = *DoPost;
 *DoPost = *NewPageTypeDoPost;
