57bce147d83bfba79b6d9ec34f83011ca0e9a553
Alex Schroeder <alex@gnu.org>
2004-02-02 23:50:47 +0000

    new

---
+++ forms.pl
@@ -0,0 +1,35 @@
+# Copyright (C) 2004  Alex Schroeder <alex@emacswiki.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
+
+$ModulesDescription .= '<p>$Id: forms.pl,v 1.1 2004/02/02 23:50:47 as Exp $</p>';
+
+push(@MyRules, \&FormsRule);
+
+sub FormsRule {
+  if ((-f GetLockedPageFile($OpenPageName))
+      and (/\G(\<form.*?\<\/form\>)/sgc)) {
+    my $form = $1;
+    my $oldpos = pos;
+    Dirty($form);
+    $form =~ s/\$([a-z]+)\$/$q->span({-class=>'param'}, GetParam($1))
+      . $q->input({-type=>'hidden', -name=>$1, -value=>GetParam($1)})/ge;
+    print UnquoteHtml($form);
+    pos = $oldpos;
+  }
+  return '';
+}
