dddb9f11cddb02b277409491132119d7c59b5dc7
Alex Schroeder <alex@gnu.org>
2004-03-21 00:32:43 +0000

    new

---
+++ orphaned.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+use CGI qw/:standard/;
+use CGI::Carp qw(fatalsToBrowser);
+use LWP::UserAgent;
+
+if (not param('url')) {
+  print header(),
+    start_html('Orphaned Pages'),
+    h1('Orphaned Pages'),
+    p('$Id: orphaned.pl,v 1.1 2004/03/21 00:32:43 as Exp $'),
+    p('Returns a list of orphaned pages based on a dot-file.'),
+    start_form(-method=>'GET'),
+    p('URL for dot-file: ', textfield('url'), br(),
+      'Example: http://www.emacswiki.org/cgi-bin/alex?action=links;raw=1'),
+    p('URL for list of nodes: ', textfield('nodes'), br(),
+      'Example: http://www.emacswiki.org/cgi-bin/alex?action=index;raw=1'),
+    p(submit()),
+    end_form(),
+    end_html();
+  exit;
+}
+
+print header(-type=>'text/plain; charset=UTF-8');
+$ua = LWP::UserAgent->new;
+$request = HTTP::Request->new('GET', param('url'));
+$response = $ua->request($request);
+$data = $response->content;
+
+while ($data =~ m/"(.*?)" -> "(.*?)"/g) {
+  $page{$1} = 1;
+  $link{$2} = 1;
+}
+
+$request = HTTP::Request->new('GET', param('nodes'));
+$response = $ua->request($request);
+$data = $response->content;
+
+foreach $pg (split(/\n/, $data)) {
+  $pg =~ s/_/ /g;
+  $page{$pg} = 1 if $pg;
+}
+
+foreach $pg (sort keys %page) {
+  print $pg, "\n" unless $link{$pg};
+}
