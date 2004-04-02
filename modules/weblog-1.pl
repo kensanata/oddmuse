70331a7f1ff90347d8f33959ed13e9abca79288b
Alex Schroeder <alex@gnu.org>
2004-04-02 01:16:53 +0000

    (NewWeblog1InitVariables): Switch from gmtime to localtime.

--- modules/weblog-1.pl
+++ modules/weblog-1.pl
@@ -21,7 +21,7 @@ use vars qw($WeblogTextLogo $WeblogXmlLogo);
 $WeblogXmlLogo = '/images/rss.png';
 $WeblogTextLogo = '/images/txt.png';
 
-$ModulesDescription .= '<p>$Id: weblog-1.pl,v 1.2 2004/01/30 21:35:44 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: weblog-1.pl,v 1.3 2004/04/02 01:16:53 as Exp $</p>';
 
 $RefererTracking = 1;
 $CommentsPrefix = 'Comments_on_';
@@ -33,9 +33,9 @@ $EditAllowed = 2;
 sub NewWeblog1InitVariables {
   OldWeblog1InitVariables();
   if (GetParam('blog', 1)) { # language independent!
-    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday) = gmtime(time);
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday) = localtime(time);
     $today = sprintf("%d-%02d-%02d", $year + 1900, $mon + 1, $mday);
-    ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday) = gmtime(time - 60*60*24);
+    ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday) = localtime(time - 60*60*24);
     $yesterday = sprintf("%d-%02d-%02d", $year + 1900, $mon + 1, $mday);
     # this modification is not mod_perl safe!
     my $blog = T('Blog');
