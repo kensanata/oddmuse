6cc6d785eba11bb8ce7eca826136fdb0a89b279e
Alex Schroeder <alex@gnu.org>
2004-06-05 00:35:16 +0000

    new

--- 404handler.pl
+++ 404handler.pl
@@ -1,42 +1,39 @@
 #! /usr/bin/perl
-package OddMuse;
-my $dir = '/var/www/wiki';
-my $script = '/usr/lib/cgi-bin/wiki.pl';
-my $name = '/cgi-bin/wiki.pl';
+# Copyright (C) 2004  Alex Schroeder <alex@emacswiki.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
+
+my $dir = '/var/www/wiki'; # absolute path to the file cache
+my $origname = '/wiki'; # relative url to the file cache, with trailing slash
+my $script = '/usr/lib/cgi-bin/wiki.pl'; # absolute path to the wiki script
+my $name = '/cgi-bin/wiki.pl'; # relative url to the wiki script
 my @path = split(/\//, $ENV{REDIRECT_URL});
 my $file = $path[$#path];
-# we only  care about the timestamp of the following two files.
-# the timestamp of the first file is changed whenever a page is
-# changed.
-my $pageidx = '/tmp/oddmuse/pageidx';
-my $cachepageidx = '/tmp/oddmuse/pageidx-cache';
 
-# cache cleanup: if any page was changed, invalidate the cache
-my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks)
-  = stat($pageidx);
-my $timestamp = $mtime;
-if (not -f $cachepageidx) {
-  open(F, "> $cachepageidx");
-  close(F);
-}
-($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks)
-  = stat($cachepageidx);
-if ($timestamp != $mtime) {
-  unlink(glob("$dir/*"));
-  utime $timestamp, $timestamp, $cachepageidx; # touch index file
-}
-print "<p><strong>$timestamp: ", join(", ", $atime,$mtime,$ctime), "</strong>";
-
-# now call the wiki for the page missing in the cache.
-# first set up CGI environment -- see http://localhost/cgi-bin/printenv.
-# then call the script and read output from the pipe.
+# call the wiki for the page missing in the cache.  first set up CGI
+# environment -- see http://localhost/cgi-bin/printenv.  then call the
+# script and read output from the pipe.
 
 local $/;
-local %ENV;
 $ENV{REQUEST_METHOD}="GET";
 $ENV{QUERY_STRING}=$file;
 $ENV{SCRIPT_FILENAME}=$script;
 $ENV{SCRIPT_NAME}=$name;
+$ENV{REQUEST_URI}=$origname;
 # print "Content-Type: text/plain\r\n\r\n";
 # print "$script $file\n";
 open(F, "$script |") || print STDERR "can't run $script: $!\n";
@@ -58,3 +55,5 @@ if (not $status) { # ie. 200
   print G $data;
   close(G);
 }
+
+# cache cleanup has to hook into the wiki!
