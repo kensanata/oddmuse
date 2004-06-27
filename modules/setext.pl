6fa9df1cb125cedecb049c72b638a6820481d77f
Alex Schroeder <alex@gnu.org>
2004-06-27 23:41:49 +0000

    (SeTextRule): Use the local $bol variable.

--- modules/setext.pl
+++ modules/setext.pl
@@ -16,30 +16,29 @@
 #    59 Temple Place, Suite 330
 #    Boston, MA 02111-1307 USA
 
-$ModulesDescription .= '<p>$Id: setext.pl,v 1.1 2004/06/27 22:04:52 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: setext.pl,v 1.2 2004/06/27 23:41:49 as Exp $</p>';
 
 push(@MyRules, \&SeTextRule);
 
 my $word = '([-A-Za-z\x80-\xff]+)';
 sub SeTextRule {
-  if (($oldpos = pos)
-      and ((m/\G\n(([ \t]*(.+?))[ \t]*\n(-+|=+)[ \t]*\n)/gc
-	    and (length($2) == length($4)))
-	   or ((pos = $oldpos)
-	       and 0))) {
+  my $oldpos = pos;
+  if ($bol && ((m/\G(([ \t]*(.+?))[ \t]*\n(-+|=+)[ \t]*\n)/gc
+		and (length($2) == length($4)))
+	       or ((pos = $oldpos) and 0))) {
     if (substr($4,0,1) eq '=') {
       return CloseHtmlEnvironments() . "<h2>$3</h2>";
     } else {
       return CloseHtmlEnvironments() . "<h3>$3</h3>";
     }
+  } elsif ($bol && m/\G((&gt; .*\n)+)/gc) {
+    return "<pre>$1</pre>";
   } elsif (m/\G\*\*($word( $word)*)\*\*/goc) {
     return "<b>$1</b>";
   } elsif (m/\G~$word~/goc) {
     return "<i>$1</i>";
   } elsif (m/\G\b_($word(_$word)*)_\b/goc) {
     return "<u>" . join(' ', split(/_/, $1)) . "</u>"; # don't clobber pos
-  } elsif (m/\G((\n&gt; .*)+\n)/gc) {
-    return "<pre>$1</pre>";
   } elsif (m/\G`_(.+)_`/gc) {
     return $1;
   }
