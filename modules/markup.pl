7015c8c779c60022a52fb80825867c2165cbe675
Alex Schroeder <alex@gnu.org>
2004-08-18 13:01:48 +0000

    Rewrite to make it customizable.

--- modules/markup.pl
+++ modules/markup.pl
@@ -16,36 +16,58 @@
 #    59 Temple Place, Suite 330
 #    Boston, MA 02111-1307 USA
 
-$ModulesDescription .= '<p>$Id: markup.pl,v 1.9 2004/07/14 14:52:10 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: markup.pl,v 1.10 2004/08/18 13:01:48 as Exp $</p>';
+
+use vars qw(%MarkupPairs %MarkupSingles);
 
 push(@MyRules, \&MarkupRule);
 # The ---- rule in usemod.pl conflicts with the --- rule
 $RuleOrder{\&MarkupRule} = 150;
 
-my $words = '([A-Za-z\x80-\xff][-A-Za-z0-9\x80-\xff ]*?)';
-my $noword = '(?=[^-A-Za-z0-9\x80-\xff]|$)'; # zero-width look-ahead assertion
+%MarkupPairs = ('*' => 'b',
+		'/' => 'i',
+		'_' => 'u',
+		'~' => 'em',
+	       );
+
+# This could be done using macros, however: If we convert to the
+# numbered entity, the next person editing finds it hard to read.  If
+# we convert to a unicode character, it is no longer obvious how to
+# achieve it.
+%MarkupSingles = ('...' => '&#x2026;', # HORIZONTAL ELLIPSIS
+		  '---' => '&#x2014;', # EM DASH
+		  '-- ' => '&#x2013; ', # EN DASH
+		  '-&gt; ' => '&#x2192; ', # RIGHTWARDS ARROW
+		 );
+
+my $words = '([A-Za-z\x80-\xff][-%.,:;\'"!?0-9 A-Za-z\x80-\xff]*?)';
+# zero-width look-ahead assertion to prevent km/h from counting
+my $noword = '(?=[^-0-9A-Za-z\x80-\xff]|$)';
+
+my $markup_pairs_re = '';
+my $markup_singles_re = '';
+
+*OldMarkupInitVariables = *InitVariables;
+*InitVariables = *NewMarkupInitVariables;
+
+sub NewMarkupInitVariables {
+  OldMarkupInitVariables();
+  $markup_pairs_re = '\G([' . join('', (map { quotemeta($_) } keys(%MarkupPairs))) . '])';
+  # die($markup_pairs_re);
+  $markup_pairs_re = qr/\G${markup_pairs_re}${words}\1${noword}/;
+  $markup_singles_re = '\G(' . join('|', (map { quotemeta($_) } keys(%MarkupSingles))) . ')';
+  $markup_singles_re = qr/$markup_singles_re/;
+}
 
 sub MarkupRule {
-  if (m/\G\*$words\*$noword/goc) {
-    return "<b>$1</b>";
-  } elsif (m/\G\/$words\/$noword/goc) {
-    return "<i>$1</i>";
-  } elsif (m/\G_${words}_$noword/goc) {
-    return "<u>$1</u>";
-  } elsif (m/\G~$words~$noword/goc) {
-    return "<em>$1</em>";
-  } elsif (m/\G-\&gt; /gc) {
-    return '&#x2192; '; # RIGHTWARDS ARROW
-  } elsif (m/\G---/gc) {
-    return '&#x2014;'; # EM DASH
-  } elsif (m/\G-- /gc) { # note that leading space is eaten by wiki.pl!
-    return '&#x2013; '; # EN DASH
-  } elsif (m/\G\.\.\./gc) {
-    return '&#x2026;'; # HORIZONTAL ELLIPSIS
-  } elsif (m|\G~/|gc) { # fix ~/elisp/ example
-    return '~/';
-  } elsif (m|\G(/[-A-Za-z0-9\x80-\xff/]+/$words/)|gc) { # fix /usr/share/lib/! example
-    return $1;
+  if (m/$markup_pairs_re/gc) {
+    return '<' . $MarkupPairs{$1} . '>' . $2 . '</' . $MarkupPairs{$1} . '>';
+  } elsif (m/$markup_singles_re/gc) {
+    return $MarkupSingles{$1};
+  } elsif ($MarkupPairs{'/'} and m|\G~/|gc) {
+    return '~/'; # fix ~/elisp/ example
+  } elsif ($MarkupPairs{'/'} and m|\G(/[-A-Za-z0-9\x80-\xff/]+/$words/)|gc) {
+    return $1; # fix /usr/share/lib/! example
   }
   return undef;
 }
