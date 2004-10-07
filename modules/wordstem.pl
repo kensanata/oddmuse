d475b85b9ae9013a82c1f1dddf4c59c48022dc81
Alex Schroeder <alex@gnu.org>
2004-10-07 21:33:44 +0000

    Added GPL Header

--- modules/wordstem.pl
+++ modules/wordstem.pl
@@ -1,7 +1,23 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
+#
 # Porter stemming algorithm code copied verbatim from http://www.tartarus.org/~martin/PorterStemmer/
 
 
-$ModulesDescription .= '<p>$Id: wordstem.pl,v 1.1 2004/10/07 21:06:09 tolchz Exp $</p>';
+$ModulesDescription .= '<p>$Id: wordstem.pl,v 1.2 2004/10/07 21:33:44 tolchz Exp $</p>';
 
 *OldStemmingResolveId = *ResolveId;
 *ResolveId = *NewStemmingResolveId;
