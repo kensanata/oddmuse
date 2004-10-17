435e0d0bfa01fa338bd13849cf3f5d2ee2f9918a
Alex Schroeder <alex@gnu.org>
2004-10-17 02:20:17 +0000

    copyright year

--- test-markup.pl
+++ test-markup.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-# Copyright (C) 2003  Alex Schroeder <alex@emacswiki.org>
+# Copyright (C) 2004  Alex Schroeder <alex@emacswiki.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
