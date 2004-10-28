67b6836cb89c281c9d6e86cd8104f5a74de0764e
Alex Schroeder <alex@gnu.org>
2004-10-28 01:31:05 +0000

    License.

--- modules/despam.pl
+++ modules/despam.pl
@@ -1,3 +1,23 @@
+# Copyright (C) 2004  Alex Schroeder <alex@emacswiki.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
+
+$ModulesDescription .= '<p>$Id: despam.pl,v 1.2 2004/10/28 01:31:05 as Exp $</p>';
+
 $Action{despam} = \&DoDespam;
 
 my @DespamRules = ();
@@ -38,8 +58,7 @@ sub DespamBannedContent {
 }
 
 sub DespamPages {
-  my @pages = AllPagesList(); # only check recently changed pages?
-  return @pages;
+  return AllPagesList(); # only check recently changed pages?
 }
 
 sub DespamPage {
