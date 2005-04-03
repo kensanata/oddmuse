14ca0f16c6a703977ae7b71180e50309b6be81b0
Alex Schroeder <alex@gnu.org>
2005-04-03 02:27:37 +0000

    (GetCurrentRequest): Deleted. (GetCurrentPageName): Deleted. (GetId): New, returns the id as set in the request, like GetCurrentRequest used to do, but don't return a "dirty_id" and "error".  Split info_path and set all parameters with these names to 1.  This is more generic than specialcasing "raw". (DoBrowseRequest): Call GetId.

--- wiki.pl
+++ wiki.pl
@@ -357,7 +357,7 @@ sub InitVariables {    # Init global session variables for mod_perl!
   unshift(@MyRules, \&MyRules) if defined(&MyRules) && (not @MyRules or $MyRules[0] != \&MyRules);
   @MyRules = sort {$RuleOrder{$a} <=> $RuleOrder{$b}} @MyRules; # default is 0
   $WikiDescription = $q->p($q->a({-href=>'http://www.oddmuse.org/'}, 'Oddmuse'))
-    . $q->p(q{$Id: wiki.pl,v 1.539 2005/03/27 01:24:55 as Exp $});
+    . $q->p(q{$Id: wiki.pl,v 1.540 2005/04/03 02:27:37 as Exp $});
   $WikiDescription .= $ModulesDescription if $ModulesDescription;
   foreach my $sub (@MyInitVariables) {
     my $result = &$sub;
@@ -1211,39 +1211,25 @@ sub Tss {
 
 # == Choosing action
 
-sub GetCurrentRequest { # Adapted from DoBrowseRequest
-  return ($HomePage, '', $q->cgi_error)
-    if (!$q->param && !($UsePathInfo && $q->path_info));
-
-  my ($id, $dirty_id, $error);
-
-  $id = join('_', $q->keywords); # script?p+q -> p_q
-  $id = $q->path_info if $UsePathInfo and not $id; # script/p/q -> p/q
-
-  # Save id so far, useful to get info embedded in the path
-  $dirty_id = $id; # Uncleaned id, useful to get info embedded in the path
-
-  $id =~ s|.*/||; # script/ignore/id (ie. we can embed info in the path!)
-  $id = GetParam('id', $id); # id=x overrides
-
-  return ($id, $dirty_id, $error);
-}
-
-sub GetCurrentPageName {
-  return $OpenPageName if (defined($OpenPageName) && length($OpenPageName));
-  return (GetCurrentRequest())[0];
+sub GetId {
+  return $HomePage if (!$q->param && !($UsePathInfo && $q->path_info));
+  my $id = join('_', $q->keywords); # script?p+q -> p_q
+  if ($UsePathInfo) {
+    my @path = split(/\//, $q->path_info);
+    $id = pop(@path) unless $id; # script/p/q -> q
+    foreach my $p (@path) {
+      SetParam($p, 1); # script/p/q -> p=1
+    }
+  }
+  return GetParam('id', $id); # id=x overrides
 }
 
 sub DoBrowseRequest {
-  my ($id, $dirty_id, $error) = GetCurrentRequest();
   # We can use the error message as the HTTP error code
-  ReportError(Ts('CGI Internal error: %s',$error), $error) if $error;
-
-  SetParam('raw', 1) if ($dirty_id =~ s|raw/||); # script/raw/id
-
+  ReportError(Ts('CGI Internal error: %s',$q->cgi_error), $q->cgi_error) if $q->cgi_error;
+  my $id = GetId();
   my $action = lc(GetParam('action', '')); # script?action=foo;id=bar
-  $action = 'download' if ($dirty_id =~ s|download/|| and not $action); # script/download/id
-
+  $action = 'download' if GetParam('download', '') and not $action; # script/download/id
   my $search = GetParam('search', '');
   if ($Action{$action}) {
     &{$Action{$action}}($id);
