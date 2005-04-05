d3c4002a5112c54f3f1405baee851d39396050d4
Fletcher T. Penney <fletcherpenney>
2005-04-05 21:24:39 +0000

    WrapperDiv Module added.

---
+++ modules/aawrapperdiv.pl
@@ -0,0 +1,42 @@
+# Copyright (C) 2004  Fletcher T. Penney <fletcher@freeshell.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
+
+$ModulesDescription .= '<p>$Id: aawrapperdiv.pl,v 1.1 2005/04/05 21:24:39 fletcherpenney Exp $</p>';
+
+
+*OldGetHeader = *GetHeader;
+*GetHeader = *WrapperGetHeader;
+
+sub WrapperGetHeader {
+	my ($id, $title, $oldId, $nocache, $status) = @_;
+	my $result = OldGetHeader ($id, $title, $oldId, $nocache, $status);
+	$result .= $q->start_div({-class=>'wrapper'});
+}
+
+*OldPrintFooter = *PrintFooter;
+*PrintFooter = *WrapperPrintFooter;
+
+sub WrapperPrintFooter {
+	my ($id, $rev, $comment) = @_;
+	print $q->start_div({-class=>'wrapper close'});
+	print $q->end_div(), $q->end_div();
+	OldPrintFooter($id, $rev, $comment);
+}
+
+
+
