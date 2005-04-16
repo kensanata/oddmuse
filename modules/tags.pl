b0e9c24bcc050ffbf377fa853f7d36397d8f44a5
Alex Schroeder <alex@gnu.org>
2005-04-16 09:01:23 +0000

    new

---
+++ modules/tags.pl
@@ -0,0 +1,39 @@
+# Copyright (C) 2005  Alex Schroeder <alex@emacswiki.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
+
+$ModulesDescription .= '<p>$Id: tags.pl,v 1.1 2005/04/16 09:01:23 as Exp $</p>';
+
+push(@MyRules, \&TagsRule);
+
+use vars qw($TagUrl);
+
+$TagUrl = 'http://technorati.com/tag/';
+
+sub TagsRule {
+  if (m/\G(\[\[tag:$FreeLinkPattern\]\])/cog
+      or m/\G(\[\[tag:$FreeLinkPattern\|([^]|]+)\]\])/cog) {
+    # [[tag:Free Link]], [[tag:Free Link|alt text]]
+    my ($tag, $text) = ($2, $3);
+    return Clean($q->a({-href=>$TagUrl . UrlEncode($tag),
+			-class=>'outside tag',
+			-title=>T('Tag'),
+			-rel=>'tag'
+		       }, $text || $tag));
+  }
+  return undef;
+}
