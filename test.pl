8b207d8014efc5a16d0f4246257f9b12c3a79fcd
Alex Schroeder <alex@gnu.org>
2005-04-23 21:40:37 +0000

    added more moin tests

--- test.pl
+++ test.pl
@@ -205,7 +205,6 @@ sub xpath_run_tests {
   # Note that the order of tests is not specified!
   my $output;
   foreach my $input (keys %New) {
-    print '.';
     my $output = apply_rules($input);
     xpath_test("<div>$output</div>", $New{$input});
   }
@@ -2431,6 +2430,8 @@ clear_pages();
 add_module('moin.pl');
 
 %Test = split('\n',<<'EOT');
+foo[[BR]]bar
+//text()[string()="foo"]/following-sibling::br/following-sibling::text()[string()="bar"]
 ''foo''
 //em[text()="foo"]
 '''bar'''
@@ -2443,12 +2444,22 @@ add_module('moin.pl');
 //ul/li[text()="one"]/following-sibling::li/text()[string()="two"]/following-sibling::ul/li[text()="two.one"]
  * one\n * two\n  * two.one
 //ul/li[text()="one"]/following-sibling::li/text()[string()="two"]/following-sibling::ul/li[text()="two.one"]
+  * one\n    * one.one\n  * two
+//ul/li/text()[string()="one"]/following-sibling::ul/li[text()="one.one"]/../../following-sibling::li[text()="two"]
+  * one\n    * one.one\n * two
+//ul/li/text()[string()="one"]/following-sibling::ul/li[text()="one.one"]/../../following-sibling::li[text()="two"]
+ 1. one\n 1. two\n  1. two.one
+//ol/li[text()="one"]/following-sibling::li/text()[string()="two"]/following-sibling::ol/li[text()="two.one"]
+   one\n     one.one\n  two
+//dl[@class="quote"]/dd/text()[normalize-space(string())="one"]/following-sibling::dl/dd[normalize-space(text())="one.one"]/../../following-sibling::dd[text()="two"]
+ * one\n more\n * two\n more
+//ul/li[normalize-space(text())="one more"]/following-sibling::li[normalize-space(text())="two more"]
+ * one\n more\n  * two\n  more
+//ul/li/text()[normalize-space(string())="one more"]/following-sibling::ul/li[normalize-space(text())="two more"]
+  one\n  more\n    two\n    more
+//dl[@class="quote"]/dd/text()[normalize-space(string())="one more"]/following-sibling::dl/dd[normalize-space(text())="two more"]
 {{{\n[[foo bar]]\n}}}
 //pre[@class="real"][text()="[[foo bar]]\n"]
-foo\n\nbar\n* one\n * two\n  * two.one
-//p[normalize-space(text())="bar"]/following-sibling::ul/li[text()="one"]/following-sibling::li/text()[string()="two"]/following-sibling::ul/li[text()="two.one"]
-foo[[BR]]bar
-//text()[string()="foo"]/following-sibling::br/following-sibling::text()[string()="bar"]
 EOT
 
 xpath_run_tests();
