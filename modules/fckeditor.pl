99ae02b91151a8ecb66bad34b1b99d227fcde1d0
Alex Schroeder <alex@gnu.org>
2005-07-05 22:26:59 +0000

    new

---
+++ modules/fckeditor.pl
@@ -0,0 +1,58 @@
+# Copyright (C) 2005  Alex Schroeder <alex@emacswiki.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
+
+$ModulesDescription .= '<p>$Id: fckeditor.pl,v 1.1 2005/07/05 22:26:59 as Exp $</p>';
+
+push (@MyRules, \&WysiwygRule);
+
+sub WysiwygRule {
+  if (m/\G(&lt;.*?&gt;)/gc) {
+    return UnquoteHtml($1);
+  }
+  return undef;
+}
+
+push (@MyInitVariables, \&WysiwygScript);
+
+sub WysiwygScript {
+  # cookie is not initialized yet so we cannot use GetParam
+  if ($q->param('action') eq 'edit') {
+    $HtmlHeaders = qq{
+<script type="text/javascript" src="/FCKeditor/fckeditor.js"></script>
+<script type="text/javascript">
+  window.onload = function()
+  {
+    var oFCKeditor = new FCKeditor( 'text' ) ;
+    oFCKeditor.Config['CustomConfigurationsPath'] = '/myconfig.js';
+    oFCKeditor.ToolbarSet = "EmacsWiki" ;
+    oFCKeditor.ReplaceTextarea() ;
+  }
+</script>
+};
+  }
+}
+
+# add id attribute to textarea
+sub GetTextArea {
+  my ($name, $text, $rows) = @_;
+  return $q->textarea(-id=>$name, -name=>$name,
+		      -default=>$text,
+		      -rows=>$rows||25,
+		      -columns=>78,
+		      -override=>1);
+}
