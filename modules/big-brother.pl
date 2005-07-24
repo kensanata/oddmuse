b31de9c52d41a42edc48c997a3842df19a4b048e
Alex Schroeder <alex@gnu.org>
2005-07-24 15:28:41 +0000

    new

---
+++ modules/big-brother.pl
@@ -0,0 +1,116 @@
+# Copyright (C) 2005  Alex Schroeder <alex@emacswiki.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
+
+package OddMuse;
+
+$ModulesDescription .= '<p>$Id: big-brother.pl,v 1.1 2005/07/24 15:28:41 as Exp $</p>';
+
+my %BigBrotherData;
+
+# replace the subs that DoSurgeProtection calls:
+#       ReadRecentVisitors();
+#       AddRecentVisitor($name);
+# 	WriteRecentVisitors();
+# 	if ($SurgeProtection and DelayRequired($name))
+
+sub IsDownload {
+}
+
+sub AddRecentVisitor {
+  my ($name) = shift;
+  my $value = $BigBrotherData{$name};
+  my %entries = $value ? %{$value} : ();
+  my $action = GetParam('action', 'browse');
+  my $id = join('_', $q->keywords) # script?p+q -> p_q
+    || $UsePathInfo && (split /\//, $q->path_info)[-1]
+    || !$q->param && $HomePage; # script/p/q -> q
+  my $download = GetParam('action', 'browse') eq 'download'
+    || GetParam('download', 0)
+    || $q->path_info() =~ m/\/download\//;
+  if ($download) {
+    # do nothing
+  } elsif ($id) {
+    $entries{$Now} = $id;
+  } elsif ($action eq 'rss' or $action eq 'rc') {
+    $entries{$Now} = $RCName;
+  } else {
+    $entries{$Now} = $q->url(-path_info=>1,-query=>1);
+  }
+  $BigBrotherData{$name} = \%entries;
+}
+
+sub DelayRequired {
+  my $name = shift;
+  my %entries = %{$BigBrotherData{$name}};
+  my @times = sort keys %entries;
+  my $ts = $times[0]; # oldest
+  return 0 if not $ts;
+  return 0 if ($Now - $ts) > $SurgeProtectionTime;
+  return 1;
+}
+
+sub ReadRecentVisitors {
+  my ($status, $data) = ReadFile($VisitorFile);
+  %BigBrotherData = ();
+  return  unless $status;
+  foreach (split(/\n/,$data)) {
+    my ($name, %entries) = split /$FS/;
+    $BigBrotherData{$name} = \%entries if $name and %entries;
+  }
+}
+
+sub WriteRecentVisitors {
+  my $data = '';
+  my $limit = $Now - $VisitorTime; # don't save visits older than this
+  foreach my $name (keys %BigBrotherData) {
+    my %entries = %{$BigBrotherData{$name}};
+    my @times = sort keys %entries;
+    if (not $times[$SurgeProtectionViews - 1]
+	or $times[$SurgeProtectionViews - 1] >= $limit) { # newest is recent enough
+      $data .=  join($FS, $name, map { $_, $entries{$_}} @times[-$SurgeProtectionViews .. -1]) . "\n";
+    }
+  }
+  WriteStringToFile($VisitorFile, $data);
+}
+
+$Action{visitors} = \&DoBigBrother;
+
+sub DoBigBrother { # no caching of this page!
+  print GetHeader('', T('Recent Visitors'), '', 1), $q->start_div({-class=>'content visitors'});
+  ReadRecentVisitors();
+  print '<p><ul>';
+  my %latest = ();
+  foreach (keys %BigBrotherData) {
+    my %entries = %{$BigBrotherData{$_}};
+    my @times = sort keys %entries;
+    $latest{$_} = $times[-1];
+  }
+  foreach my $name (sort {$latest{$a} <=> $latest{$b}} keys %latest) {
+    my $when = CalcTimeSince($Now - $latest{$name});
+    my $error = ValidId($name);
+    my $who = $name && !$error ? GetPageLink($name) : T('Anonymous');
+    my %entries = %{$BigBrotherData{$name}};
+    my $what = join(', ', map { $entries{$_} =~ /^$FullUrlPattern$/
+				? $q->a({-href=>$entries{$_}}, T('some action'))
+				: GetPageLink($entries{$_}) }
+		    sort keys %entries);
+    print $q->li($who, T('was here'), $when, T('and read'), $what);
+  }
+  print '</ul>' . $q->end_div();
+  PrintFooter();
+}
