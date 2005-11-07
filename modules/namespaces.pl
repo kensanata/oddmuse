f03ea299c0c1f02f0583753bd24ea255b5346cc5
Alex Schroeder <alex@gnu.org>
2005-11-07 00:46:15 +0000

    (NewNamespaceScriptLink): Do not mangle actions matching $FullUrlPattern, as that breaks localnames.pl. Since $action is already URL-encoded, however, we do the same test as in ScriptLink: $UrlProtocols followed by the URL-encoded colon: %3a.

--- modules/namespaces.pl
+++ modules/namespaces.pl
@@ -16,7 +16,7 @@
 #    59 Temple Place, Suite 330
 #    Boston, MA 02111-1307 USA
 
-$ModulesDescription .= '<p>$Id: namespaces.pl,v 1.22 2005/10/28 14:46:30 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: namespaces.pl,v 1.23 2005/11/07 00:46:15 as Exp $</p>';
 
 use vars qw($NamespacesMain $NamespacesSelf $NamespaceCurrent $NamespaceRoot);
 
@@ -192,7 +192,7 @@ sub NamespaceRcLines {
 sub NewNamespaceScriptLink {
   my ($action, @rest) = @_;
   local $ScriptName = $ScriptName;
-  if ($action !~ /^$FullUrlPattern$/) {
+  if ($action =~ /^($UrlProtocols)\%3a/) { # URL-encoded URL
     # do nothing
   } elsif ($action !~ /=/ and $action =~ /(.*?)%2f(.*)/) {
     $ScriptName .= "/$1";
