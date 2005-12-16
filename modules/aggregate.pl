c8cb66c6456feda4af21011028fcf495ab3ab7ff
Alex Schroeder <alex@gnu.org>
2005-12-16 12:53:48 +0000

    fix learn more links

--- modules/aggregate.pl
+++ modules/aggregate.pl
@@ -16,7 +16,7 @@
 #    59 Temple Place, Suite 330
 #    Boston, MA 02111-1307 USA
 
-$ModulesDescription .= '<p>$Id: aggregate.pl,v 1.2 2005/12/16 12:48:27 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: aggregate.pl,v 1.3 2005/12/16 12:53:48 as Exp $</p>';
 
 push(@MyRules, \&AggregateRule);
 
@@ -30,14 +30,17 @@ sub AggregateRule {
       my $title = $1;
       local $OpenPageName = FreeToNormal($1);
       my $page = GetPageContent($OpenPageName);
+      my $size = length($page);
       my $i = index($page, "\n=");
       my $j = index($page, "\n----");
-      $page = substr($page, 0, $i) if $i;
-      $page = substr($page, 0, $j) if $j;
+      $page = substr($page, 0, $i) if $i >= 0;
+      $page = substr($page, 0, $j) if $j >= 0;
       $page =~ s/^=.*\n//; # if it starts with a header
       print $q->start_div({class=>"page"}),
 	$q->h2(GetPageLink($OpenPageName, $title));
       ApplyRules(QuoteHtml($page), 1, 0, undef, 'p');
+      print $q->p(GetPageLink($OpenPageName, T('Learn more...')))
+	if length($page) < $size;
       print $q->end_div();
     }
     print $q->end_div();
