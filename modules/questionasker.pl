70d9d21f7baf0ff6b07524129b61c19221284091
Alex Schroeder <alex@gnu.org>
2005-12-18 00:07:36 +0000

    new version awwaiid

--- modules/questionasker.pl
+++ modules/questionasker.pl
@@ -17,25 +17,44 @@
 #    59 Temple Place, Suite 330
 #    Boston, MA 02111-1307 USA
 
-$ModulesDescription .= '<p>questionasker.pl (v0.2) - Answer a question to save the page.</p>';
+$ModulesDescription .= '<p>questionasker.pl (v1.1) - Answer a question to save the page.</p>';
+
+use vars qw( @QuestionaskerQuestions $QuestionaskerBypass);
+
+# Yes, this does what it sounds like it does... bypasses this whole thing
+$QuestionaskerBypass = 'awwaiid';
+
+@QuestionaskerQuestions = (
+  #['What is the first letter of this question?' => sub { shift =~ /W/i }],
+
+  #['How many letters are in the word "four"?' => sub { shift =~ /4|four/i }],
+
+  #['Tell me any number between 1 and 10' => sub { $a=shift; ($a > 0 && $a < 11) }],
+
+  ["Name a popular programming language<br>
+    whose name is four letters long and starts with<br>
+    a 'P' and ends in an 'L'" =>
+    sub { shift =~ /perl/i }],
+
+  ["How many lives does a cat have?" => sub { shift =~ /9|nine/i }],
+
+  ["This old man came _____ home" => sub { shift =~ /rolling/i }],
+
+  ["Why is a raven like a writing desk?<br>
+    Just kidding. What is 2 + 4?" => sub { shift =~ /6|six/i }],
+
+);
 
 *OldQuestionaskerDoPost = *DoPost;
 *DoPost = *NewQuestionaskerDoPost;
-
-use vars qw( $QuestionaskerQuestions );
-
-@QuestionaskerQuestions = (
-  ['What is the first letter of this question?' => sub { shift =~ /W/i }],
-  ['How many letters are in the word "four"?' => sub { shift =~ /4|four/i }],
-  ['Tell me any number between 1 and 10' => sub { $a=shift; ($a > 0 && $a < 11) }]
-);
 
 sub NewQuestionaskerDoPost {
   my(@params) = @_;
   my $question_num = GetParam('question_num', undef);
   my $answer = GetParam('answer', undef);
+  my $bypass = GetParam('bypass', undef);
 
-  unless($QuestionaskerQuestions[$question_num][1]($answer)) {
+  unless($bypass eq $QuestionaskerBypass || $QuestionaskerQuestions[$question_num][1]($answer)) {
       print GetHeader('', T('Edit Denied'), undef, undef, '403 FORBIDDEN');
       print $q->p(T('You did not answer correctly.'));
       print $q->p(T('Contact the wiki administrator for more information.'));
@@ -45,22 +64,36 @@ sub NewQuestionaskerDoPost {
   return (OldQuestionaskerDoPost(@params));
 }
 
+*OldQuestionaskerGetCommentForm = *GetCommentForm;
+*GetCommentForm = *NewQuestionaskerGetCommentForm;
+
 *OldQuestionaskerDoEdit = *DoEdit;
 *DoEdit = *NewQuestionaskerDoEdit;
 $Action{edit} = \&DoEdit;
 
 sub NewQuestionaskerDoEdit {
-  my (@params) = @_;
   *OldQuestionaskerGetFormStart = *GetFormStart;
   *GetFormStart = *NewQuestionaskerGetFormStart;
-  OldQuestionaskerDoEdit(@params);
+  OldQuestionaskerDoEdit(@_);
 }
 
+sub NewQuestionaskerGetCommentForm {
+  *OldQuestionaskerGetFormStart = *GetFormStart;
+  *GetFormStart = *NewQuestionaskerGetFormStart;
+  my $retval = OldQuestionaskerGetCommentForm(@_);
+  *GetFormStart = *OldQuestionaskerGetFormStart;
+  return $retval;
+}
 
 sub NewQuestionaskerGetFormStart {
+  *GetFormStart = *OldQuestionaskerGetFormStart;
   my $retval = OldQuestionaskerGetFormStart(@_);
-  my $question_number = int(rand(scalar(@QuestionaskerQuestions)));
+  $retval .= QuestionaskerGetQuestion();
+  return $retval;
+}
 
+sub QuestionaskerGetQuestion {
+  my $question_number = int(rand(scalar(@QuestionaskerQuestions)));
   $retval .= "<p>To save this page you must answer this question:";
   $retval .= "<blockquote>";
   $retval .= $QuestionaskerQuestions[$question_number][0];
@@ -68,9 +101,6 @@ sub NewQuestionaskerGetFormStart {
   $retval .= "<input type=text name=answer />";
   $retval .= "<input type=hidden name=question_num value=$question_number />";
   $retval .= "</blockquote></p>";
-
-  # From now on use the original GetFormStart
-  *GetFormStart = *OldQuestionaskerGetFormStart;
   return $retval;
 }
 
