184483ff696c252c825fdb81386a908dcf2f28b9
Alex Schroeder <alex@gnu.org>
2006-03-23 19:59:43 +0000

    update include tests

--- test.pl
+++ test.pl
@@ -2862,6 +2862,28 @@ test_page_negative(get_page('search=AAA'), 'alex_pic');
 
 # --------------------
 
+include:
+print '[include]';
+
+clear_pages();
+
+update_page('foo_moo', 'foo_bar');
+test_page(update_page('yadda', '<include "foo moo">'),
+	  qq{<div class="include foo_moo"><p>foo_bar</p></div>});
+test_page(update_page('yadda', '<include text "foo moo">'),
+	  qq{<pre class="include foo_moo">foo_bar\n</pre>});
+test_page(update_page('yadda', '<include "yadda">'),
+	  qq{<strong>Recursive include of yadda!</strong>});
+update_page('yadda', '<include "foo moo">');
+test_page(update_page('dada', '<include "yadda">'),
+	  qq{<div class="include yadda"><div class="include foo_moo"><p>foo_bar</p></div></div>});
+test_page(update_page('foo_moo', '<include "dada">'),
+	  qq{<strong>Recursive include of foo_moo!</strong>});
+test_page(update_page('bar', '<include "foo_moo">'),
+	  qq{<strong>Recursive include of foo_moo!</strong>});
+
+# --------------------
+
 end:
 
 ### END OF TESTS
