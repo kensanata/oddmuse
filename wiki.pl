ec050db15af255a3b79c4cabbd15303cf594abde
Alex Schroeder <alex@gnu.org>
2006-05-20 23:17:06 +0000

    (ApplyRules): Make it so that <search "foo"> only works at the beginning of the line.

--- wiki.pl
+++ wiki.pl
@@ -269,7 +269,7 @@ sub InitRequest {
 
 sub InitVariables {    # Init global session variables for mod_perl!
   $WikiDescription = $q->p($q->a({-href=>'http://www.oddmuse.org/'}, 'Oddmuse'))
-    . $q->p(q{$Id: wiki.pl,v 1.660 2006/05/20 16:20:10 as Exp $});
+    . $q->p(q{$Id: wiki.pl,v 1.661 2006/05/20 23:17:06 as Exp $});
   $WikiDescription .= $ModulesDescription if $ModulesDescription;
   $PrintedHeader = 0;  # Error messages don't print headers unless necessary
   $ReplaceForm = 0;    # Only admins may search and replace
@@ -467,7 +467,7 @@ sub ApplyRules {
 	eval { local $SIG{__DIE__}; binmode(STDOUT, ":raw"); };
 	Clean(AddHtmlEnvironment('p')); # if dirty block is looked at later, this will disappear
 	pos = $oldpos; # restore \G after call to RSS which uses the LWP module
-      } elsif (/\G(&lt;search "(.*?)"&gt;)/cgis) {
+      } elsif ($bol && /\G(&lt;search "(.*?)"&gt;)/cgis) {
 	# <search "regexp">
 	Clean(CloseHtmlEnvironments());
 	Dirty($1);
