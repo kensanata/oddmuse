b596a629198aa8b3c7ef2eaeab9391e72ff05cfc
Alex Schroeder <alex@gnu.org>
2006-06-04 21:31:27 +0000

    update copyright years

--- modules/toc.pl
+++ modules/toc.pl
@@ -1,4 +1,4 @@
-# Copyright (C) 2004  Alex Schroeder <alex@emacswiki.org>
+# Copyright (C) 2004, 2005, 2006  Alex Schroeder <alex@emacswiki.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
 #    59 Temple Place, Suite 330
 #    Boston, MA 02111-1307 USA
 
-$ModulesDescription .= '<p>$Id: toc.pl,v 1.32 2006/05/26 07:48:33 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: toc.pl,v 1.33 2006/06/04 21:31:27 as Exp $</p>';
 
 push(@MyRules, \&TocRule);
 
