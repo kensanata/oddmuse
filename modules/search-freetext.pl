3b3416f5d69efb8c0cefaccd572af4ddeec94b10
Alex Schroeder <alex@gnu.org>
2006-06-12 16:45:01 +0000

    (NewSearchFreeTextTitleAndBody): Do not apply limit when building a journal using search.

--- modules/search-freetext.pl
+++ modules/search-freetext.pl
@@ -40,7 +40,7 @@ sub process {
 
 package OddMuse;
 
-$ModulesDescription .= '<p>$Id: search-freetext.pl,v 1.41 2006/06/12 16:40:37 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: search-freetext.pl,v 1.42 2006/06/12 16:45:01 as Exp $</p>';
 
 push(@MyRules, \&SearchFreeTextTagsRule);
 
@@ -203,8 +203,9 @@ sub NewSearchFreeTextTitleAndBody {
   # limit to the result page requested
   $max = @result - 1 if @result -1 < $max;
   my $count = ($page - 1) * $limit;
-  my @items = @result[($page - 1) * $limit  .. $max]
-    unless $CollectingJournal; # when building a journal using search, no limit!
+  my @items = @result;
+  # no limit when building a journal using search!
+  @item = @item[($page - 1) * $limit  .. $max] unless $CollectingJournal;
   # print links, if this is is really a search
   my $raw = GetParam('raw','');
   my @links = ();
