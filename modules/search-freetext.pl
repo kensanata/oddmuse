1285a8aa4a0c7bc37036177e1d6822ba7f7938f8
Alex Schroeder <alex@gnu.org>
2006-06-12 20:01:09 +0000

    (NewSearchFreeTextTitleAndBody): Do not apply limit when building rc or rss feed.

--- modules/search-freetext.pl
+++ modules/search-freetext.pl
@@ -40,7 +40,7 @@ sub process {
 
 package OddMuse;
 
-$ModulesDescription .= '<p>$Id: search-freetext.pl,v 1.43 2006/06/12 18:53:37 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: search-freetext.pl,v 1.44 2006/06/12 20:01:09 as Exp $</p>';
 
 push(@MyRules, \&SearchFreeTextTagsRule);
 
@@ -204,7 +204,7 @@ sub NewSearchFreeTextTitleAndBody {
   $max = @result - 1 if @result -1 < $max;
   my $count = ($page - 1) * $limit;
   my @items = @result;
-  @item = @item[($page - 1) * $limit  .. $max]
+  @items = @items[($page - 1) * $limit  .. $max]
     unless $CollectingJournal                      # no limit when building a journal,
       or GetParam('action', 'browse') eq 'rc'      # filtering recent changes or the
       or GetParam('action', 'browse') eq 'rss';    # rss feed.
