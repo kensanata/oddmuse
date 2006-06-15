1b10502227a45d4a6e77c331fd2a68dc2b34951f
Alex Schroeder <alex@gnu.org>
2006-06-15 19:22:14 +0000

    (PrintAllPages): Bugfix resulting in up to $JournalLimit page entries with empty id.

--- wiki.pl
+++ wiki.pl
@@ -271,7 +271,7 @@ sub InitRequest {
 
 sub InitVariables {    # Init global session variables for mod_perl!
   $WikiDescription = $q->p($q->a({-href=>'http://www.oddmuse.org/'}, 'Oddmuse'))
-    . $q->p(q{$Id: wiki.pl,v 1.679 2006/06/15 12:11:20 as Exp $});
+    . $q->p(q{$Id: wiki.pl,v 1.680 2006/06/15 19:22:14 as Exp $});
   $WikiDescription .= $ModulesDescription if $ModulesDescription;
   $PrintedHeader = 0;  # Error messages don't print headers unless necessary
   $ReplaceForm = 0;    # Only admins may search and replace
@@ -811,7 +811,7 @@ sub PrintAllPages {
   my $comments = shift;
   my $lang = GetParam('lang', 0);
   my @pages = @_;
-  @pages = @pages[0 .. $JournalLimit - 1] unless UserIsAdmin();
+  @pages = @pages[0 .. $JournalLimit - 1] if $#pages >= $JournalLimit and not UserIsAdmin();
   for my $id (@pages) {
     OpenPage($id);
     my @languages = split(/,/, $Page{languages});
