9ba3e109889490af7d2f104ddedeee8890d15047
Alex Schroeder <alex@gnu.org>
2006-06-23 06:38:14 +0000

    (PermanentAnchorsInit): Initialize %PermanentAnchors, too.

--- wiki.pl
+++ wiki.pl
@@ -271,7 +271,7 @@ sub InitRequest {
 
 sub InitVariables {    # Init global session variables for mod_perl!
   $WikiDescription = $q->p($q->a({-href=>'http://www.oddmuse.org/'}, 'Oddmuse'))
-    . $q->p(q{$Id: wiki.pl,v 1.680 2006/06/15 19:22:14 as Exp $});
+    . $q->p(q{$Id: wiki.pl,v 1.681 2006/06/23 06:38:14 as Exp $});
   $WikiDescription .= $ModulesDescription if $ModulesDescription;
   $PrintedHeader = 0;  # Error messages don't print headers unless necessary
   $ReplaceForm = 0;    # Only admins may search and replace
@@ -3908,7 +3908,7 @@ sub WriteRecentVisitors {
 
 sub PermanentAnchorsInit {
   return if $PermanentAnchorsInit;
-  %PagePermanentAnchors = ();
+  %PagePermanentAnchors = %PermanentAnchors = ();
   $PermanentAnchorsInit = 1; # set to 0 when $PermanentAnchorsFile is saved
   my ($status, $data) = ReadFile($PermanentAnchorsFile);
   return unless $status; # not fatal
