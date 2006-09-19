abd6167cf787ac97e22d180272e5a535a0597676
Alex Schroeder <alex@gnu.org>
2006-09-19 23:49:22 +0000

    *** empty log message ***

---
+++ modules/smiles.pl
@@ -0,0 +1,40 @@
+# Copyright (C) 2004  Alex Schroeder <alex@emacswiki.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
+
+$ModulesDescription .= '<p>$Id: smiles.pl,v 1.1 2006/09/19 23:49:22 as Exp $</p>';
+
+%Smilies = (
+     ':-?D'      => 'http://www.emacswiki.org/pics/grin.png',
+     ':-?\)'     => 'http://www.emacswiki.org/pics/smile.png',
+     ':-?\}='    => 'http://www.emacswiki.org/pics/smile-cool-beard.png',
+     ';-?\)'     => 'http://www.emacswiki.org/pics/blink.png',
+     ':-?]'      => 'http://www.emacswiki.org/pics/forced.png',
+     '=-?\)'     => 'http://www.emacswiki.org/pics/bigeye-smile.png',
+     '8-\)'      => 'http://www.emacswiki.org/pics/braindamaged.png',
+     '8-?D'      => 'http://www.emacswiki.org/pics/goggle.png',
+     ':-\|'      => 'http://www.emacswiki.org/pics/indifferent.png',
+     ':-?[/\\\]' => 'http://www.emacswiki.org/pics/wry.png',
+     ':-?\('     => 'http://www.emacswiki.org/pics/sad.png',
+     ':-?\{'     => 'http://www.emacswiki.org/pics/frown.png',
+     ':\'\('     => 'http://www.emacswiki.org/pics/cry.png',
+     'X-?[(|]'   => 'http://www.emacswiki.org/pics/dead.png',
+     '\&gt;-?\{' => 'http://www.emacswiki.org/pics/evil.png',
+     ':-[Ppb]'  => 'http://www.emacswiki.org/pics/poke.png',
+     '\(-?:'     => 'http://www.emacswiki.org/pics/reverse-smile.png',
+     '&lt;3'     => 'http://www.emacswiki.org/pics/round-heart.png',
+           );
