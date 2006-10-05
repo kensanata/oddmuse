003a93401e4d5e0fd27a5e61b5be5a1ae666a708
Alex Schroeder <alex@gnu.org>
2006-10-05 23:23:43 +0000

    (PrintJournal): Use action=more.

--- wiki.pl
+++ wiki.pl
@@ -272,7 +272,7 @@ sub InitRequest {
 sub InitVariables {    # Init global session variables for mod_perl!
   $WikiDescription = $q->p($q->a({-href=>'http://www.oddmuse.org/'}, 'Oddmuse'),
 			   $Counter++ > 0 ? Ts('%s calls', $Counter) : '')
-    . $q->p(q{$Id: wiki.pl,v 1.746 2006/10/05 23:16:10 as Exp $});
+    . $q->p(q{$Id: wiki.pl,v 1.747 2006/10/05 23:23:43 as Exp $});
   $WikiDescription .= $ModulesDescription if $ModulesDescription;
   $PrintedHeader = 0;  # Error messages don't print headers unless necessary
   $ReplaceForm = 0;    # Only admins may search and replace
@@ -812,7 +812,7 @@ sub PrintJournal {
     print $q->start_div({-class=>'journal'}) . $q->comment("$FullUrl $num $regexp $mode $offset");
     PrintAllPages(1, 1, @pages);
     print $q->end_div();
-    print ScriptLink("action=journal;num=$num;regexp=$regexp;mode=$mode;offset=" . ($offset + $num),
+    print ScriptLink("action=more;num=$num;regexp=$regexp;mode=$mode;offset=" . ($offset + $num),
 		     T('More...'), 'more') if $more;
   }
 }
