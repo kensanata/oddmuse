9d7c79d611c5cad3a766f09921f5721aedb8e0f2
Alex Schroeder <alex@gnu.org>
2006-10-31 15:18:54 +0000

    (SabiFooInit): Only append ellipsis if the text is longer than $SummaryDefaultLength

--- modules/sabifoo.pl
+++ modules/sabifoo.pl
@@ -16,7 +16,7 @@
 #    59 Temple Place, Suite 330
 #    Boston, MA 02111-1307 USA
 
-$ModulesDescription .= '<p>$Id: sabifoo.pl,v 1.5 2006/10/31 15:16:56 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: sabifoo.pl,v 1.6 2006/10/31 15:18:54 as Exp $</p>';
 
 push(@MyInitVariables, \&SabiFooInit);
 
@@ -32,8 +32,10 @@ sub SabiFooInit {
     }
     # base summary on the text added this time only, without title
     my $summary = $text;
-    $summary = substr($summary, 0, $SummaryDefaultLength);
-    $summary =~ s/\s*\S*$/ . . ./;
+    íf (length($summary) > $SummaryDefaultLength) {
+      $summary = substr($summary, 0, $SummaryDefaultLength);
+      $summary =~ s/\s*\S*$/ . . ./;
+    }
     # append to existing page!
     $title = FreeToNormal($today . ' ' . $title);
     if ($IndexHash{$title}) {
