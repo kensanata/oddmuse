d983cd0b97f6e7d651cc487065e5d9f5a458031b
Alex Schroeder <alex@gnu.org>
2007-02-19 18:49:55 +0000

    (BannedContent): Ignore malformed regular expressions in on the page.

--- wiki.pl
+++ wiki.pl
@@ -272,7 +272,7 @@ sub InitRequest {
 sub InitVariables {    # Init global session variables for mod_perl!
   $WikiDescription = $q->p($q->a({-href=>'http://www.oddmuse.org/'}, 'Oddmuse'),
 			   $Counter++ > 0 ? Ts('%s calls', $Counter) : '')
-    . $q->p(q{$Id: wiki.pl,v 1.771 2007/01/31 08:16:06 as Exp $});
+    . $q->p(q{$Id: wiki.pl,v 1.772 2007/02/19 18:49:55 as Exp $});
   $WikiDescription .= $ModulesDescription if $ModulesDescription;
   $PrintedHeader = 0;  # Error messages don't print headers unless necessary
   $ReplaceForm = 0;    # Only admins may search and replace
@@ -3121,7 +3121,9 @@ sub BannedContent {
     next unless m/^\s*([^#]+?)\s*(#\s*(\d\d\d\d-\d\d-\d\d\s*)?(.*))?$/;
     my ($regexp, $comment) = ($1, $4);
     foreach my $url (@urls) {
-      if ($url =~ /($regexp)/i) {
+      my $re;
+      eval { $re = qr/$regexp/i; };
+      if (defined($re) && $url =~ $re) {
 	return Tss('Rule "%1" matched "%2" on this page.', $regexp, $url) . ' '
 	  . ($comment ? Ts('Reason: %s.', $comment) : T('Reason unknown.')) . ' '
 	  . Ts('See %s for more information.', GetPageLink($BannedContent));
