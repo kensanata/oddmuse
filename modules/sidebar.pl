c21cecd23416e9050c0a212c6e8d62cf96afd380
Alex Schroeder <alex@gnu.org>
2007-08-15 15:08:51 +0000

    Remove declaration of $SideBarOpenPageName; change license to GPLv3

--- modules/sidebar.pl
+++ modules/sidebar.pl
@@ -1,9 +1,9 @@
 # Copyright (C) 2004  Tilmann Holst
-# Copyright (C) 2004, 2005  Alex Schroeder <alex@emacswiki.org>
+# Copyright (C) 2004, 2005, 2007  Alex Schroeder <alex@emacswiki.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -12,14 +12,11 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-#    Free Software Foundation, Inc.
-#    59 Temple Place, Suite 330
-#    Boston, MA 02111-1307 USA
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-$ModulesDescription .= '<p>$Id: sidebar.pl,v 1.17 2007/02/09 21:51:09 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: sidebar.pl,v 1.18 2007/08/15 15:08:51 as Exp $</p>';
 
-use vars qw($SidebarName $SideBarOpenPageName);
+use vars qw($SidebarName);
 
 # Include this page on every page:
 
