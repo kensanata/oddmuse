cce733196c42ae9a83be4b0f34e70ab7dd5388f2
Alex Schroeder <alex@gnu.org>
2007-08-17 00:28:10 +0000

    Switch to GPLv3

--- modules/toc.pl
+++ modules/toc.pl
@@ -2,7 +2,7 @@
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -12,11 +12,11 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the
-#    Free Software Foundation, Inc.
-#    59 Temple Place, Suite 330
-#    Boston, MA 02111-1307 USA
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-$ModulesDescription .= '<p>XXX $Id: toc.pl,v 1.43 2007/08/17 00:24:14 as Exp $</p>';
+$ModulesDescription .= '<p>XXX $Id: toc.pl,v 1.44 2007/08/17 00:28:10 as Exp $</p>';
 
 push(@MyRules, \&TocRule);
 
