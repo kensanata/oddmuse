837af28c91f73072c34390941fbbb735cb07e084
Alex Schroeder <alex@gnu.org>
2007-09-10 04:15:23 +0000

    Add the ability to preserve the Comments link in a plainsite

--- modules/plainsite.pl
+++ modules/plainsite.pl
@@ -1,4 +1,4 @@
-# Copyright (C) 2005  Fletcher T. Penney <fletcher@freeshell.org>
+# Copyright (C) 2005-2007  Fletcher T. Penney <fletcher@freeshell.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,9 @@
 #    59 Temple Place, Suite 330
 #    Boston, MA 02111-1307 USA
 
-$ModulesDescription .= '<p>$Id: plainsite.pl,v 1.1 2005/11/11 02:15:32 fletcherpenney Exp $</p>';
+$ModulesDescription .= '<p>$Id: plainsite.pl,v 1.2 2007/09/10 04:15:23 fletcherpenney Exp $</p>';
+
+use vars qw($PlainSiteAllowCommentLink);
 
 *OldGetFooterLinks = *GetFooterLinks;
 *GetFooterLinks = *PlainSiteGetFooterLinks;
@@ -26,8 +28,27 @@ sub PlainSiteGetFooterLinks {
 	if (UserIsAdmin() or UserIsEditor()) {
 		return OldGetFooterLinks(@_);
 	} else {
-		return;
+		if ($PlainSiteAllowCommentLink) {
+			return CommentFooterLink(@_);
+		} else {
+			return;
+		}
 	}
+}
+
+sub CommentFooterLink {
+  my ($id, $rev) = @_;
+  my @elements;
+  if ($id and $rev ne 'history' and $rev ne 'edit') {
+    if ($CommentsPrefix) {
+      if ($OpenPageName =~ /^$CommentsPrefix(.*)/o) {
+	push(@elements, GetPageLink($1, undef, 'original'));
+      } else {
+	push(@elements, GetPageLink($CommentsPrefix . $OpenPageName, undef, 'comment'));
+      }
+    }
+  }
+  return @elements ? $q->span({-class=>'edit bar'}, $q->br(), @elements) : '';
 }
 
 *OldGetFooterTimestamp = *GetFooterTimestamp;
@@ -52,15 +73,35 @@ sub PlainSiteGetRcRss {
 	OldGetRcRss(@_);
 }
 
-*DoRc = *PlainSiteDoRc;
-
-sub PlainSiteDoRc {
-	# must load before clustermap module if used
-	return;
-}
-
 *GetNearLinksUsed = *PlainSiteGetNearLinksUsed;
 
 sub PlainSiteGetNearLinksUsed {
 	return;
+}
+
+
+# Disable the Recent Change function on cluster pages
+# Must load before clustermap module if that module is used
+
+
+*OldPrintRc = *PrintRc;
+*PrintRc = *PlainSitePrintRc;
+
+sub PlainSitePrintRc{
+	my ($id, $standalone) = @_;
+	if (!(UserIsAdmin() or UserIsEditor())) {
+		DoRc(\&PlainSiteRcHtml);
+	} else {
+		return OldPrintRc($id, $standalone);
+	}
+}
+
+sub PlainSiteRcHtml {
+	my ($html, $inlist) = ('', 0);
+	if (!(UserIsAdmin() or UserIsEditor())) {
+		return;
+	} else {
+		*GetRcHtml = *OldGetRcHtml;
+		return OldGetRcHtml();
+	}
 }
