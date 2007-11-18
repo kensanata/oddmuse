b8cb6818b04413b7f56941b3bb4af9dcc02d0343
Alex Schroeder <alex@gnu.org>
2007-11-18 11:17:34 +0000

    Introduce options.

--- raw.pl
+++ raw.pl
@@ -1,4 +1,4 @@
-#! /usr/bin/perl
+#! /usr/bin/perl -w
 
 # Copyright (C) 2005, 2007  Alex Schroeder <alex@emacswiki.org>
 #
@@ -26,30 +26,60 @@ sub ParseData {
   return %result;
 }
 
-my $PageDir = 'page';
-my $RawDir  = 'raw';
-local $/ = undef;   # Read complete files
-
-my $regexp = $ARGV[0];
-
-# include dotfiles!
-foreach my $file (glob("$PageDir/*/*.pg $PageDir/*/.*.pg")) {
-  next unless $file =~ m|/.*/(.+)\.pg$|;
-  my $page = $1;
-  next if $regexp && $page !~ m|$regexp|o;
-  mkdir($RawDir) or die "Cannot create $RawDir directory: $!" unless -d $RawDir;
-  open(F, $file) or die "Cannot read $page file: $!";
-  my $data = <F>;
-  close(F);
-  my $ts = (stat("$RawDir/$page"))[9];
-  my %result = ParseData($data);
-  if ($ts == $result{ts}) {
-    print "skipping $page because it is up to date\n" if $verbose;
-  } else {
-    print "writing $page because $ts != $result{ts}\n" if $verbose;
-    open(F,"> $RawDir/$page") or die "Cannot write $page raw file: $!";
-    print F $result{text};
+sub main {
+  my ($regexp, $PageDir, $RawDir) = @_;
+  # include dotfiles!
+  local $/ = undef;   # Read complete files
+  foreach my $file (glob("$PageDir/*/*.pg $PageDir/*/.*.pg")) {
+    next unless $file =~ m|/.*/(.+)\.pg$|;
+    my $page = $1;
+    next if $regexp && $page !~ m|$regexp|o;
+    mkdir($RawDir) or die "Cannot create $RawDir directory: $!"
+      unless -d $RawDir;
+    open(F, $file) or die "Cannot read $page file: $!";
+    my $data = <F>;
     close(F);
-    utime $result{ts}, $result{ts}, "$RawDir/$page"; # touch file
+    my $ts = (stat("$RawDir/$page"))[9];
+    my %result = ParseData($data);
+    if ($ts && $ts == $result{ts}) {
+      print "skipping $page because it is up to date\n" if $verbose;
+    } else {
+      print "writing $page because $ts != $result{ts}\n" if $verbose;
+      open(F,"> $RawDir/$page") or die "Cannot write $page raw file: $!";
+      print F $result{text};
+      close(F);
+      utime $result{ts}, $result{ts}, "$RawDir/$page"; # touch file
+    }
   }
-};
+}
+
+use Getopt::Long;
+my $regexp = undef;
+my $page = 'page';
+my $dir = 'raw';
+GetOptions ("regexp=s" => \$regexp,
+	    "page=s"   => \$page,
+	    "dir=s"    => \$dir,
+	    "help"     => \$help);
+
+if ($help) {
+  print qq{
+Usage: $0 [--regexp REGEXP] [--page DIR] [--dir DIR]
+
+Writes the raw wiki text into plain text files.
+
+--regexp selects a subsets of pages whose names match the regular
+  expression. Note that spaces have been translated to underscores.
+
+--page designates the page directory. By default is is 'page' in the
+  current directory. If you run this script in your data directory,
+  the default should be fine.
+
+--dir designates an output directory. By default this is 'raw' in the
+  current directory.
+
+Example: $0 --regexp '\\.el\$' --dir elisp
+}
+} else {
+  main ($regexp, $page, $dir);
+}
