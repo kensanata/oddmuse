6612a1d738ae64ef01ed756b1121d380f1486fba
Weakish Jiang <weakish@gmail.com>
2008-02-21 14:55:25 +0000

    * (CreoleHeadingRule):  Change the match pattern for preformatted block   in order to conform Creole 1.0's standard. The actual RegExp change is   provided from Alex Schröder.

--- modules/creole.pl
+++ modules/creole.pl
@@ -16,7 +16,7 @@
 #    59 Temple Place, Suite 330
 #    Boston, MA 02111-1307 USA
 
-$ModulesDescription .= '<p>$Id: creole.pl,v 1.35 2007/10/23 09:33:38 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: creole.pl,v 1.36 2008/02/21 14:55:25 weakish Exp $</p>';
 
 use vars qw($CreoleLineBreaks);
 
@@ -126,7 +126,7 @@ sub CreoleRule {
   # {{{
   # preformatted
   # }}}
-  elsif ($bol && m/\G\{\{\{[ \t]*\n(.*?\n)\}\}\}[ \t]*\n?/cgs) {
+  elsif ($bol && m/\G\{\{\{[ \t]*\n(.*?\n)\}\}\}[ \t]*(\n|\z)/cgs) {
     my $str = $1;
     $str =~ s/\n }}}/\n}}}/g;
     return CloseHtmlEnvironments() . $q->pre({-class=>'real'}, $str)
