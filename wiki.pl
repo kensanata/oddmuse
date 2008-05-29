bc8fefdf6733b10facea49c776b20c3c44f1413c
Alex Schroeder <alex@gnu.org>
2008-05-29 08:32:54 +0000

    Fix typo.

--- wiki.pl
+++ wiki.pl
@@ -8,7 +8,7 @@ use CGI::Carp qw(fatalsToBrowser);
 use vars qw($VERSION);
 local $| = 1;  # Do not buffer output (localized for mod_perl)
 
-$VERSION=(split(/ +/, qq($Revision: 1.852 $)))[1]; # for MakeMaker
+$VERSION=(split(/ +/, q($Revision: 1.853 $)))[1]; # for MakeMaker
 
 # Options:
 
@@ -266,7 +266,7 @@ sub InitRequest {
 sub InitVariables {	 # Init global session variables for mod_perl!
   $WikiDescription = $q->p($q->a({-href=>'http://www.oddmuse.org/'}, 'Oddmuse'),
 			   $Counter++ > 0 ? Ts('%s calls', $Counter) : '')
-    . $q->p(q{$Id: wiki.pl,v 1.852 2008/05/29 08:28:47 as Exp $});
+    . $q->p(q{$Id: wiki.pl,v 1.853 2008/05/29 08:32:54 as Exp $});
   $WikiDescription .= $ModulesDescription if $ModulesDescription;
   $PrintedHeader = 0; # Error messages don't print headers unless necessary
   $ReplaceForm = 0;		# Only admins may search and replace
