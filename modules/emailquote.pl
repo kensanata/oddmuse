24f8a50b2e515f84f23d9c87081720faf69cfe83
Weakish Jiang <weakish@gmail.com>
2008-06-24 15:42:22 +0000

    This extension enables you to write quotes in email’s way.

---
+++ modules/emailquote.pl
@@ -0,0 +1,30 @@
+# Copyright (C) 2008  Weakish Jiang <weakish@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as 
+# published by the Free Software Foundation.
+#
+# You can get a copy of GPL version 2 at
+# http://www.gnu.org/licenses/gpl-2.0.html
+#
+# For user doc, see: 
+# http://www.oddmuse.org/cgi-bin/oddmuse/Email_Quote_Extension
+
+$ModulesDescription .= '<p>$Id: emailquote.pl,v 1.1 2008/06/24 15:42:22 weakish Exp $</p>';
+
+push(@MyRules, \&EmailQuoteRule);
+
+sub EmailQuoteRule {
+   # > hi, you mentioned that:
+   # > > I don't like Oddmuse.
+   # > in last letter.
+    if ($bol && m/\G(\s*\n)*((\&gt;[ \t])+)/cog
+	  or InElement('dd') && m/\G(\s*\n)+((\&gt;[ \t])+)/cog) {
+    $leng = length($2)/5;
+    return CloseHtmlEnvironmentUntil('dd') . OpenHtmlEnvironment('dl',$leng, 'quote')
+      . $q->dt() . AddHtmlEnvironment('dd');
+  }
+   return undef;
+}	
+   
+
