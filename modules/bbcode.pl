40944845d9aa205e065f91be9f1712fd5365666d
Alex Schroeder <alex@gnu.org>
2008-06-26 07:57:51 +0000

    Updated to GPLv3.

--- modules/bbcode.pl
+++ modules/bbcode.pl
@@ -1,8 +1,8 @@
-# Copyright (C) 2007, 2008  Alex Schroeder <alex@emacswiki.org>
+# Copyright (C) 2007, 2008  Alex Schroeder <alex@gnu.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -11,12 +11,9 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-#    Free Software Foundation, Inc.
-#    59 Temple Place, Suite 330
-#    Boston, MA 02111-1307 USA
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-$ModulesDescription .= '<p>$Id: bbcode.pl,v 1.7 2008/06/26 07:46:53 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: bbcode.pl,v 1.8 2008/06/26 07:57:51 as Exp $</p>';
 
 push(@MyRules, \&bbCodeRule);
 
