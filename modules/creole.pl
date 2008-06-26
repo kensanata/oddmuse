00b34f582c0525140ced988111a9c01e46593873
Alex Schroeder <alex@gnu.org>
2008-06-26 07:56:47 +0000

    Updated to GPLv3 and added a separate copyright line for Weakish Jiang.

--- modules/creole.pl
+++ modules/creole.pl
@@ -1,7 +1,9 @@
-# Copyright (C) 2006, 2007, 2008  Alex Schroeder <alex@emacswiki.org>
+# Copyright (C) 2006, 2007  Alex Schroeder <alex@gnu.org>
+# Copyright (C) 2008  Weakish Jiang <weakish@gmail.com>
+
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -10,12 +12,9 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-#    Free Software Foundation, Inc.
-#    59 Temple Place, Suite 330
-#    Boston, MA 02111-1307 USA
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-$ModulesDescription .= '<p>$Id: creole.pl,v 1.40 2008/06/25 15:03:28 weakish Exp $</p>';
+$ModulesDescription .= '<p>$Id: creole.pl,v 1.41 2008/06/26 07:56:47 as Exp $</p>';
 
 use vars qw($CreoleLineBreaks $CreoleTildeAlternative);
 
