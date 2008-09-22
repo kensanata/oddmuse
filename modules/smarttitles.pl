8ae87645abd4c962cf5289f2e304f2d30d0dd09e
Alex Schroeder <alex@gnu.org>
2008-09-22 10:08:23 +0000

    modules/smarttitles.pl: Refactored. This is a flashy rewrite; it improves integration with creole.pl, poetry.pl, et al., HTML configurability, CSS styleability, and perldoc-style documentation.

--- modules/smarttitles.pl
+++ modules/smarttitles.pl
@@ -1,72 +1,170 @@
-# Copyright (C) 2006 Charles Mauch <cmauch@gmail.com>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-#    Free Software Foundation, Inc.
-#    59 Temple Place, Suite 330
-#    Boston, MA 02111-1307 USA
+#!/usr/bin/env perl
+# ====================[ smarttitles.pl                     ]====================
 
-$ModulesDescription .= '<p>$Id: smarttitles.pl,v 1.5 2006/10/01 04:26:11 xterminus Exp $</p>';
+=head1 NAME
 
-push(@MyRules, \&StripTitlesRule);
+smarttitles - An Oddmuse module for embedding user-readable titles and subtitles
+              for a page in the content for that page.
 
-sub StripTitlesRule {
-    if ( m/\G#TITLE[ \t]+(.*?)\s*\n+/cg ) {
-            return undef;
-    }
-    return undef;
-}
+=head1 INSTALLATION
 
-push( @MyRules, \&StripSubTitlesRule );
+smarttitles is easily installable: move this file into the B<wiki/modules/>
+directory of your Oddmuse Wiki.
 
-sub StripSubTitlesRule {
-    if ( m/\G#SUBTITLE[ \t]+(.*?)\s*\n+/cg ) {
-      return undef;
-    }
+=cut
+package OddMuse;
+
+$ModulesDescription .= '<p>$Id: smarttitles.pl,v 1.6 2008/09/22 10:08:23 leycec Exp $</p>';
+
+# ....................{ CONFIGURATION                      }....................
+
+=head1 CONFIGURATION
+
+smarttitles is easily configurable; set these variables in the B<wiki/config.pl>
+file for your Oddmuse Wiki.
+
+=cut
+use vars qw($SmartTitlesBrowserTitle
+            $SmartTitlesBrowserTitleWithoutSubtitle);
+
+=head2 $SmartTitlesBrowserTitle
+
+The browser title for pages having a subtitle. (The browser title is the string
+displayed in your browser's titlebar, for each page.)
+
+smarttitles substites the first '%s' in this string with this Wiki's
+C<$SiteName>, the second '%s' with this Wiki page's title, and the third '%s'
+with this Wiki page's subtitle.
+
+=cut
+$SmartTitlesBrowserTitle = '%s: %s (%s)';
+
+=head2 $SmartTitlesBrowserTitleWithoutSubtitle
+
+The browser title for pages lacking a subtitle.
+
+smarttitles substites the first '%s' in this string with this Wiki's
+C<$SiteName> and the second '%s' with this Wiki page's title.
+
+=cut
+$SmartTitlesBrowserTitleWithoutSubtitle = '%s: %s';
+
+# ....................{ RULES                              }....................
+push(@MyRules, \&SmartTitlesRule);
+
+# "#TITLE" and "#SUBTITLE" conflict with Creole-style numbered lists, and
+# "poetry.pl"-style poetry blocks; as such, rules filtering these strings should
+# be applied before rules filtering Creole- and "poetry.pl"-style strings. As it
+# is likely that these rules, also, conflict with other Oddmuse markup modules,
+# this module requests that these rules be applied with high priority -
+# presumably before another module is permitted to muck them up.
+$RuleOrder{\&SmartTitlesRule} = -50;
+
+=head2 SmartTitlesRule
+
+Strips "#TITLE ...\n" and "#SUBTITLE ...\n" text from the current Wiki page.
+Since GetHeaderSmartTitles() already parses this text, it serves little use past
+that point.
+
+=cut
+sub SmartTitlesRule {
+     if (m/\G#TITLE[ \t]+(.*?)\s*\n+/cg   ) { return ''; }
+  elsif (m/\G#SUBTITLE[ \t]+(.*?)\s*\n+/cg) { return ''; }
+
   return undef;
 }
 
-*OldSmartGetHeader = *GetHeader;
-*GetHeader = *NewSmartGetHeader;
+# ....................{ FUNCTIONS                          }....................
+*GetHeaderSmartTitlesOld = *GetHeader;
+*GetHeader =               *GetHeaderSmartTitles;
 
-sub NewSmartGetHeader {
-    my ($id, $title, $oldId, $nocache, $status, $rev) = @_;
-    my $header = OldSmartGetHeader(@_);
-    $title = NormalToFree($title);
+=head2 GetSmartTitles
 
-    return $header unless $id;    
-    OpenPage($id);
- 
-    my ( $smarttitle ) = ( $Page{ text } =~ /\#TITLE[ \t]+(.*?)\s*\n+/ );    
- 
-    if ($smarttitle) {
-        my $OldGetHtmlHeader = '>' . $title . '</a>';
-        my $NewGetHtmlHeader = '>' . $smarttitle . '</a>';
-        $header =~ s/$OldGetHtmlHeader/$NewGetHtmlHeader/g;
+Returns the title and subtitle for this page. (Presumably, this page has been
+opened with an earlier call to C<OpenPage()>.)
 
-        my $OldTitle = '<title>' . $SiteName . ': ' . $title . '</title>';
-        my $NewTitle = '<title>' . $SiteName . ': ' . $smarttitle . '</title>';
-        $header =~ s/$OldTitle/$NewTitle/;
-    }
-    
-    my ( $subtitle ) = ( $Page{ text } =~ m/\#SUBTITLE[ \t]+(.*?)\s*\n+/ );
+This function is provided as a separate subroutine so as to permit other
+extensions (namely, hibernal) to obtain the title and subtitle for pages.
 
-    if ( $subtitle ) {
-        my $OldSubGetHtmlHeader = '</a></h1>';
-        my $NewSubGetHtmlHeader =
-          '</a></h1><p class="subtitle">' . $subtitle . '</p>';
-        $header =~ s/$OldSubGetHtmlHeader/$NewSubGetHtmlHeader/g;
-    }
-
-    return $header;
+=cut
+sub GetSmartTitles {
+  return (
+    ($Page{text} =~    m/\#TITLE[ \t]+(.*?)\s*\n+/c),
+    ($Page{text} =~ m/\#SUBTITLE[ \t]+(.*?)\s*\n+/c)
+  );
 }
+
+=head2 GetHeaderSmartTitles
+
+Changes the passed page's HTML header to reflect any "#TITLE" or "#SUBTITLE"
+within that passed page's Wiki content.
+
+=cut
+sub GetHeaderSmartTitles {
+  my ($page_name, $title, undef, undef, undef, undef, $subtitle) = @_;
+  my ($smart_title, $smart_subtitle);
+  my  $html_header = GetHeaderSmartTitlesOld(@_);
+
+  if ($page_name) {
+    OpenPage($page_name);
+    $title = NormalToFree($title);
+    ($smart_title, $smart_subtitle) = GetSmartTitles();
+  }
+
+  if (!$smart_title    && $title   ) { $smart_title =    $title; }
+  if (!$smart_subtitle && $subtitle) { $smart_subtitle = $subtitle; }
+
+  #          .........{ TITLE                              }.........
+  if ($smart_title and $smart_title ne $title) {
+    $html_header =~ s~\Q>${title}</a>\Q~>${smart_title}</a>~g;
+  }
+
+  #          .........{ SUBTITLE                           }.........
+  if ($smart_subtitle) {
+    $html_header =~ s~\Q</h1>\Q~</h1><p class="subtitle">${smart_subtitle}</p>~;
+  }
+
+  #          .........{ HEADER                             }.........
+  {
+    my $smart_header;
+
+    if ($SiteName eq $smart_title) {
+      $smart_header = $smart_subtitle
+        ? sprintf($SmartTitlesBrowserTitleWithoutSubtitle, $SiteName, $smart_subtitle)
+        : $SiteName;
+    }
+    else {
+      $smart_header = $smart_subtitle
+        ? sprintf($SmartTitlesBrowserTitle,                $SiteName, $smart_title, $smart_subtitle)
+        : sprintf($SmartTitlesBrowserTitleWithoutSubtitle, $SiteName, $smart_title);
+    }
+
+    $html_header =~ s~\<title\>.*?\<\/title\>~<title>${smart_header}</title>~;
+  }
+
+  return $html_header;
+}
+
+=head1 COPYRIGHT AND LICENSE
+
+The information below applies to everything in this distribution,
+except where noted.
+
+Copyleft  2008 by B.w.Curry <http://www.raiazome.com>.
+Copyright 2006 by Charles Mauch <mailto://cmauch@gmail.com>.
+
+This file is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This file is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this file; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+=cut
