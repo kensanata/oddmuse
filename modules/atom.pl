20b0f2372c7e300da7170152fc60b8ddce449949
Alex Schroeder <alex@gnu.org>
2008-10-04 22:31:11 +0000

    Copyright year, GPLv3

--- modules/atom.pl
+++ modules/atom.pl
@@ -1,8 +1,8 @@
-# Copyright (C) 2004, 2006  Alex Schroeder <alex@emacswiki.org>
+# Copyright (C) 2004, 2006, 2008  Alex Schroeder <alex@gnu.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -11,16 +11,13 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-#    Free Software Foundation, Inc.
-#    59 Temple Place, Suite 330
-#    Boston, MA 02111-1307 USA
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 use XML::Atom::Entry;
 use XML::Atom::Link;
 use XML::Atom::Person;
 
-$ModulesDescription .= '<p>$Id: atom.pl,v 1.12 2008/10/04 22:30:20 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: atom.pl,v 1.13 2008/10/04 22:31:11 as Exp $</p>';
 
 push(@MyInitVariables, \&AtomInit);
 
