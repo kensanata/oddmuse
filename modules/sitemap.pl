6edeff99120e756caf56866c956367a2d9e10ca0
Alex Schroeder <alex@gnu.org>
2008-10-12 21:23:31 +0000

    (DoSitemap): Simplified. No longer requires the admin password because it no longer opens all the pages to get at the timestamp. Now uses sitemap 0.9 as documented here: https://www.google.com/webmasters/tools/docs/en/protocol.html

--- modules/sitemap.pl
+++ modules/sitemap.pl
@@ -1,8 +1,8 @@
-# Copyright (C) 2005  Alex Schroeder <alex@emacswiki.org>
+# Copyright (C) 2005, 2008  Alex Schroeder <alex@gnu.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -11,35 +11,22 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-#    Free Software Foundation, Inc.
-#    59 Temple Place, Suite 330
-#    Boston, MA 02111-1307 USA
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-$ModulesDescription .= '<p>$Id: sitemap.pl,v 1.1 2005/06/12 16:31:03 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: sitemap.pl,v 1.2 2008/10/12 21:23:31 as Exp $</p>';
 
 $Action{'sitemap'} = \&DoSitemap;
 
 sub DoSitemap {
-  UserIsAdminOrError();
   print GetHttpHeader('application/rss+xml');
   print qq{<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.google.com/schemas/sitemap/0.84"
-	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	xsi:schemaLocation="http://www.google.com/schemas/sitemap/0.84
-	http://www.google.com/schemas/sitemap/0.84/sitemap.xsd">};
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">};
   foreach my $id (AllPagesList()) {
-    OpenPage($id);
-    $id = UrlEncode($id);
-    my $url = $UsePathInfo ? "$ScriptName/$id" :  "$ScriptName?$id";
-    my $ts = TimeToW3($Page{ts});
+    my $url = ScriptUrl($id);
     print qq{
-  <url>
-    <loc>$url</loc>
-    <changefreq>always</changefreq>
-    <lastmod>$ts</lastmod>
-  </url>};
+<url><loc>$url</loc></url>};
   }
   print qq{
-</urlset>};
+</urlset>
+};
 }
