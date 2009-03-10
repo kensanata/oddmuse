78a94cabb7ce2a241291a64d1decb1b9eb185a58
Alex Schroeder <alex@gnu.org>
2009-03-10 07:20:58 +0000

    Whitespace.

--- wiki.pl
+++ wiki.pl
@@ -1,5 +1,5 @@
 #! /usr/bin/perl
-# Version       $Id: wiki.pl,v 1.895 2009/03/07 16:21:33 as Exp $
+# Version       $Id: wiki.pl,v 1.896 2009/03/10 07:20:58 as Exp $
 # Copyleft      2008 Brian Curry <http://www.raiazome.com>
 # Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008
 #     Alex Schroeder <alex@gnu.org>
@@ -35,7 +35,7 @@ use CGI::Carp qw(fatalsToBrowser);
 use vars qw($VERSION);
 local $| = 1;  # Do not buffer output (localized for mod_perl)
 
-$VERSION=(split(/ +/, q{$Revision: 1.895 $}))[1]; # for MakeMaker
+$VERSION=(split(/ +/, q{$Revision: 1.896 $}))[1]; # for MakeMaker
 
 # Options:
 use vars qw($RssLicense $RssCacheHours @RcDays $TempDir $LockDir $DataDir
@@ -293,7 +293,7 @@ sub InitRequest {
 sub InitVariables {  # Init global session variables for mod_perl!
   $WikiDescription = $q->p($q->a({-href=>'http://www.oddmuse.org/'}, 'Oddmuse'),
          $Counter++ > 0 ? Ts('%s calls', $Counter) : '')
-    . $q->p(q{$Id: wiki.pl,v 1.895 2009/03/07 16:21:33 as Exp $});
+    . $q->p(q{$Id: wiki.pl,v 1.896 2009/03/10 07:20:58 as Exp $});
   $WikiDescription .= $ModulesDescription if $ModulesDescription;
   $PrintedHeader = 0; # Error messages don't print headers unless necessary
   $ReplaceForm = 0;   # Only admins may search and replace
@@ -1562,21 +1562,21 @@ sub StripRollbacks {
       my $target_id = $result[$i][3];
       # strip global rollbacks
       if ($skip_to and $ts <= $skip_to) {
-  splice(@result, $i + 1, $end - $i);
-  $skip_to = 0;
+	splice(@result, $i + 1, $end - $i);
+	$skip_to = 0;
       } elsif ($id eq '[[rollback]]') {
-  if ($target_id) {
-    $rollback{$target_id} = $target_ts; # single page rollback
-    splice(@result, $i, 1);             # strip marker
-  } else {
-    $end = $i unless $skip_to;
-    $skip_to = $target_ts; # cumulative rollbacks!
-  }
+	if ($target_id) {
+	  $rollback{$target_id} = $target_ts; # single page rollback
+	  splice(@result, $i, 1);             # strip marker
+	} else {
+	  $end = $i unless $skip_to;
+	  $skip_to = $target_ts; # cumulative rollbacks!
+	}
       } elsif ($rollback{$id} and $ts > $rollback{$id}) {
-  splice(@result, $i, 1); # strip rolled back single pages
+	splice(@result, $i, 1); # strip rolled back single pages
       }
     }
-  } else {      # just strip the marker left by DoRollback()
+  } else {		  # just strip the marker left by DoRollback()
     for (my $i = $#result; $i >= 0; $i--) {
       splice(@result, $i, 1) if $result[$i][1] eq '[[rollback]]'; # id
     }
