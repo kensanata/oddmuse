cefd23822124ab2c7282b8f5b04675933237474a
Alex Schroeder <alex@gnu.org>
2009-03-13 17:53:53 +0000

    (InitCookie): We need to fall back on %OldCookie if no explicit parameter was provided.

--- wiki.pl
+++ wiki.pl
@@ -1,5 +1,5 @@
 #! /usr/bin/perl
-# Version       $Id: wiki.pl,v 1.903 2009/03/13 17:41:06 as Exp $
+# Version       $Id: wiki.pl,v 1.904 2009/03/13 17:53:53 as Exp $
 # Copyleft      2008 Brian Curry <http://www.raiazome.com>
 # Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009
 #     Alex Schroeder <alex@gnu.org>
@@ -36,7 +36,7 @@ use CGI::Carp qw(fatalsToBrowser);
 use vars qw($VERSION);
 local $| = 1;  # Do not buffer output (localized for mod_perl)
 
-$VERSION=(split(/ +/, q{$Revision: 1.903 $}))[1]; # for MakeMaker
+$VERSION=(split(/ +/, q{$Revision: 1.904 $}))[1]; # for MakeMaker
 
 # Options:
 use vars qw($RssLicense $RssCacheHours @RcDays $TempDir $LockDir $DataDir
@@ -293,7 +293,7 @@ sub InitRequest {
 sub InitVariables {  # Init global session variables for mod_perl!
   $WikiDescription = $q->p($q->a({-href=>'http://www.oddmuse.org/'}, 'Oddmuse'),
          $Counter++ > 0 ? Ts('%s calls', $Counter) : '')
-    . $q->p(q{$Id: wiki.pl,v 1.903 2009/03/13 17:41:06 as Exp $});
+    . $q->p(q{$Id: wiki.pl,v 1.904 2009/03/13 17:53:53 as Exp $});
   $WikiDescription .= $ModulesDescription if $ModulesDescription;
   $PrintedHeader = 0; # Error messages don't print headers unless necessary
   $ReplaceForm = 0;   # Only admins may search and replace
@@ -347,6 +347,10 @@ sub InitCookie {
   } else {
     %OldCookie = ();
   }
+  my %provided = map { $_ => 1 } $q->param;
+  for my $key (keys %OldCookie) {
+    SetParam($key, $OldCookie{$key}) unless $provided{$key};
+  }
   CookieUsernameFix();
   CookieRollbackFix();
 }
@@ -380,7 +384,6 @@ sub CookieRollbackFix {
 sub GetParam {
   my ($name, $default) = @_;
   my $result = $q->param($name);
-  $result = $OldCookie{$name} unless defined($result);
   $result = $default unless defined($result);
   return QuoteHtml($result); # you need to unquote anything that can have <tags>
 }
