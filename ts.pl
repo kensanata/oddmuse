452ea03ef4baab3a762c5767aca6fbe24a458e7a
Alex Schroeder <alex@gnu.org>
2009-05-22 09:26:11 +0000

    Format

--- ts.pl
+++ ts.pl
@@ -35,15 +35,21 @@ sub TimeToText {
   return CalcDay($t) . ' ' . CalcTime($t);
 }
 
-sub TimeToW3 { # Complete date plus hours and minutes: YYYY-MM-DDThh:mmTZD (eg 1997-07-16T19:20+01:00)
-  my ($sec, $min, $hour, $mday, $mon, $year) = gmtime(shift); # use special UTC designator ("Z")
+# Complete date plus hours and minutes: YYYY-MM-DDThh:mmTZD (eg
+# 1997-07-16T19:20+01:00)
+sub TimeToW3 {
+  my ($sec, $min, $hour, $mday, $mon, $year) = gmtime(shift);
+  # use special UTC designator ("Z")
   return sprintf('%4d-%02d-%02dT%02d:%02dZ', $year+1900, $mon+1, $mday, $hour, $min);
 }
 
 sub TimeToRFC822 {
-  my ($sec, $min, $hour, $mday, $mon, $year, $wday) = gmtime(shift); # Sat, 07 Sep 2002 00:00:01 GMT
-  return sprintf("%s, %02d %s %04d %02d:%02d:%02d GMT", qw(Sun Mon Tue Wed Thu Fri Sat)[$wday], $mday,
-     qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec)[$mon], $year+1900, $hour, $min, $sec);
+  my ($sec, $min, $hour, $mday, $mon, $year, $wday) = gmtime(shift);
+  # Sat, 07 Sep 2002 00:00:01 GMT
+  return sprintf("%s, %02d %s %04d %02d:%02d:%02d GMT",
+		 qw(Sun Mon Tue Wed Thu Fri Sat)[$wday], $mday,
+		 qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec)[$mon],
+		 $year+1900, $hour, $min, $sec);
 }
 
 while(<>) {
