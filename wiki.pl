42d32712b1724e2b53569c394ddbf57c544f4b94
Alex Schroeder <alex@gnu.org>
2009-08-15 22:11:14 +0000

    (SearchString, SearchRegexp): Handle invalid regular expressions by compiling the regular expression, and if that fails, use quotemeta on it and use that instead.

--- wiki.pl
+++ wiki.pl
@@ -1,5 +1,5 @@
 #! /usr/bin/perl
-# Version       $Id: wiki.pl,v 1.926 2009/08/03 22:24:24 as Exp $
+# Version       $Id: wiki.pl,v 1.927 2009/08/15 22:11:14 as Exp $
 # Copyleft      2008 Brian Curry <http://www.raiazome.com>
 # Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009
 #     Alex Schroeder <alex@gnu.org>
@@ -36,7 +36,7 @@ use CGI::Carp qw(fatalsToBrowser);
 use vars qw($VERSION);
 local $| = 1;  # Do not buffer output (localized for mod_perl)
 
-$VERSION=(split(/ +/, q{$Revision: 1.926 $}))[1]; # for MakeMaker
+$VERSION=(split(/ +/, q{$Revision: 1.927 $}))[1]; # for MakeMaker
 
 # Options:
 use vars qw($RssLicense $RssCacheHours @RcDays $TempDir $LockDir $DataDir
@@ -293,7 +293,7 @@ sub InitRequest {
 sub InitVariables {  # Init global session variables for mod_perl!
   $WikiDescription = $q->p($q->a({-href=>'http://www.oddmuse.org/'}, 'Oddmuse'),
          $Counter++ > 0 ? Ts('%s calls', $Counter) : '')
-    . $q->p(q{$Id: wiki.pl,v 1.926 2009/08/03 22:24:24 as Exp $});
+    . $q->p(q{$Id: wiki.pl,v 1.927 2009/08/15 22:11:14 as Exp $});
   $WikiDescription .= $ModulesDescription if $ModulesDescription;
   $PrintedHeader = 0; # Error messages don't print headers unless necessary
   $ReplaceForm = 0;   # Only admins may search and replace
@@ -3218,9 +3218,9 @@ sub BannedContent {
     foreach my $url (@urls) {
       eval { $re = qr/$regexp/i; };
       if (defined($re) && $url =~ $re) {
-  return Tss('Rule "%1" matched "%2" on this page.', $regexp, $url) . ' '
-    . ($comment ? Ts('Reason: %s.', $comment) : T('Reason unknown.')) . ' '
-      . Ts('See %s for more information.', GetPageLink($BannedContent));
+	return Tss('Rule "%1" matched "%2" on this page.', $regexp, $url) . ' '
+	  . ($comment ? Ts('Reason: %s.', $comment) : T('Reason unknown.')) . ' '
+	  . Ts('See %s for more information.', GetPageLink($BannedContent));
       }
     }
   }
@@ -3406,6 +3406,8 @@ sub SearchString {
   my ($string, $data) = @_;
   my @strings = grep /./, $string =~ /\"([^\"]+)\"|(\S+)/g; # skip null entries
   foreach my $str (@strings) {
+    eval { $str = qr/$str/i; }; # handle invalid regular expressions
+    $str = quotemeta($str) if $@;
     return 0 unless ($data =~ /$str/i);
   }
   return 1;
@@ -3415,6 +3417,8 @@ sub SearchRegexp {
   my $regexp = join '|', map { index($_,'|') == -1 ? $_ : "($_)" }
     grep /./, shift =~ /\"([^\"]+)\"|(\S+)/g; # this acts as OR
   $regexp =~ s/\\s/[[:space:]]/g;
+  eval { $regexp = qr/$regexp/i; }; # handle invalid regular expressions
+  $regexp = quotemeta($regexp) if $@;
   return $regexp;
 }
 
