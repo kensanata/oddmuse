5776133b2005d78cdafc92c996e66b07eacb7825
Alex Schroeder <alex@gnu.org>
2009-10-02 13:01:53 +0000

    *** empty log message ***

---
+++ smtp_test.pl
@@ -0,0 +1,58 @@
+#! /usr/bin/perl
+# Copyright (C) 2009  Alex Schroeder <alex@gnu.org>
+
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+
+package OddMuse;
+
+use strict;
+use Getopt::Std;
+use Net::SMTP::TLS;
+use File::Temp;
+use MIME::Entity;
+
+# This script can be invoked as follows:
+# perl smtp_test.pl -m "alex:*secret*@mail.epfarms.org" \
+#                   -f "kensanata@gmail.com"
+
+# -m user:password@mailhost for sending email using SMTP Auth. Without this
+#    information, the script will send mail to localhost.
+# -f email address to use as the sender and recipient.
+
+my %opts;
+getopt('mf', \%opts);
+$opts{m} =~ /(.*?):(.*)\@(.*)/;
+my ($user, $password, $host) = ($1, $2, $3);
+die "Cannot parse -m " . $opts{m} . "\n" if $opts{m} && !$host;
+my $from = $opts{f};
+die "Must provide sender using -f\n" if $host && !$from;
+
+my ($fh, $filename) = File::Temp->new(SUFFIX => '.html', UNLINK => 1);
+print $fh qq(<body>This is a <b>test</b>!);
+$fh->close;
+
+my $mail = new MIME::Entity->build(To => $from, # test!
+				   From => $from,
+				   Subject => 'Test',
+				   Path => $fh,
+				   Type => "text/html");
+my $smtp = Net::SMTP::TLS->new($host,
+			       User => $user,
+			       Password => $password,
+			       Debug => 1);
+$smtp->mail($from);
+$smtp->to($from); # test!
+$smtp->data;
+$smtp->datasend($mail->stringify);
+$smtp->dataend;
+$smtp->quit;
