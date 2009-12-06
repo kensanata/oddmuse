79b01cae2eac9127a5c5e8f8223d2733df1a73df
Alex Schroeder <alex@gnu.org>
2009-12-06 19:26:33 +0000

    (AnchorsRule): Use an empty string instead of a single space.

--- modules/anchors.pl
+++ modules/anchors.pl
@@ -1,22 +1,18 @@
-# Copyright (C) 2004, 2005  Alex Schroeder <alex@emacswiki.org>
+# Copyright (C) 2004, 2005, 2009  Alex Schroeder <alex@gnu.org>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-#    Free Software Foundation, Inc.
-#    59 Temple Place, Suite 330
-#    Boston, MA 02111-1307 USA
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
 
-$ModulesDescription .= '<p>$Id: anchors.pl,v 1.16 2009/12/05 15:09:39 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: anchors.pl,v 1.17 2009/12/06 19:26:33 as Exp $</p>';
 
 push(@MyRules, \&AnchorsRule);
 
@@ -46,7 +42,7 @@ sub AnchorsRule {
     $text =~ s/_/ /g;
     return ScriptLink(UrlEncode($id), $text, $class, undef, $title);
   } elsif (m/\G\[\:$FreeLinkPattern\]/gc) {
-    return $q->a({-name=>FreeToNormal($1), -class=>'anchor'}, ' ');
+    return $q->a({-name=>FreeToNormal($1), -class=>'anchor'}, '');
   }
   return undef;
 }
