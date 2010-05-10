e362f7bb675e807adf279ed0a14827b5758c8f70
Alex Schroeder <alex@gnu.org>
2010-05-10 17:31:41 +0000

    Will not edit and save SVG files, but everything else works.

---
+++ modules/svg-edit.pl
@@ -0,0 +1,79 @@
+# Copyright (C) 2010  Alex Schroeder <alex@gnu.org>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+
+use vars qw($SvgMimeType);
+
+$SvgMimeType = 'image/svg+xml';
+
+push (@MyInitVariables, \&SvgInitVariables);
+
+sub SvgInitVariables {
+  $UploadAllowed = 1;
+  push (@UploadTypes, $SvgMimeType)
+    unless grep {$_ eq $SvgMimeType} @UploadTypes;
+}
+
+*OldSvgGetDownloadLink = *GetDownloadLink;
+*GetDownloadLink = *NewSvgGetDownloadLink;
+
+sub NewSvgGetDownloadLink {
+  my ($name, $image, $revision, $alt) = @_;
+  return OldSvgGetDownloadLink(@_) if $name ne $OpenPageName;
+  return OldSvgGetDownloadLink(@_) if $image != 1;
+  my $data = $Page{text};
+  return OldSvgGetDownloadLink(@_) unless SvgItIs($data);
+  my ($width, $height) = SvgDimensions($data);
+  # add 20 to compensate for scrollbars?
+  return $q->iframe({-width => $width + 20, -height => $height + 20,
+		     -src => OldSvgGetDownloadLink($name, 2)}, "");
+}
+
+sub SvgItIs {
+  my ($type) = TextIsFile(shift);
+  return $type eq $SvgMimeType;
+}
+
+sub SvgDimensions {
+  my $data = shift;
+  $data =~ s/.*\n//; # strip first line
+  require MIME::Base64;
+  $data = MIME::Base64::decode($data);
+  # crude hack to avoid parsing the SVG XML
+  my ($x) = $data =~ /width="(.*?)"/;
+  my ($y) = $data =~ /height="(.*?)"/;
+  return $x, $y;
+}
+
+*OldSvgGetEditForm = *GetEditForm;
+*GetEditForm = *NewSvgGetEditForm;
+
+sub NewSvgGetEditForm {
+  my $html = OldSvgGetEditForm(@_);
+  return $html unless SvgItIs($Page{text});
+  my $link = ScriptLink('action=svg;id=' . UrlEncode($OpenPageName),
+			T('Edit image in the browser'),
+			'svg');
+  my $text = T('Replace this file with text');
+  $html =~ s!$text</a>!$text</a> $link!;
+  return $html;
+}
+
+$Action{svg} = \&DoSvg;
+
+sub DoSvg {
+  my $id = shift;
+  print GetHeader('', Ts('Editing %s', $id));
+  print qq{<iframe src="http://svg-edit.googlecode.com/svn/tags/stable/editor/svg-editor.html" width="750" height="600"></iframe>};
+  PrintFooter($id, 'edit');
+}
