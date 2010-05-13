6b452edfd550522cf142631068dfd4dcfea81684
Alex Schroeder <alex@gnu.org>
2010-05-13 21:58:14 +0000

    (DoSvg): Add Javascript to override the save functionality. It includes an encode64 function, a new save handler, and some init code, and initialize the image.

--- modules/svg-edit.pl
+++ modules/svg-edit.pl
@@ -74,8 +74,72 @@ $Action{svg} = \&DoSvg;
 
 sub DoSvg {
   my $id = shift;
+  $HtmlHeaders .= qq{
+<script type="text/javascript">
+
+var keyStr =
+    "ABCDEFGHIJKLMNOP" +
+    "QRSTUVWXYZabcdef" +
+    "ghijklmnopqrstuv" +
+    "wxyz0123456789+/" +
+    "=";
+
+function encode64(input) {
+    var output = "";
+    var chr1, chr2, chr3 = "";
+    var enc1, enc2, enc3, enc4 = "";
+    var i = 0;
+
+    do {
+        chr1 = input.charCodeAt(i++);
+        chr2 = input.charCodeAt(i++);
+        chr3 = input.charCodeAt(i++);
+
+        enc1 = chr1 >> 2;
+        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+        enc4 = chr3 & 63;
+
+        if (isNaN(chr2)) {
+	    enc3 = enc4 = 64;
+        } else if (isNaN(chr3)) {
+	    enc4 = 64;
+        }
+
+        output = output +
+	    keyStr.charAt(enc1) +
+	    keyStr.charAt(enc2) +
+	    keyStr.charAt(enc3) +
+	    keyStr.charAt(enc4);
+        chr1 = chr2 = chr3 = "";
+        enc1 = enc2 = enc3 = enc4 = "";
+    } while (i < input.length);
+
+    return output;
+}
+
+function oddmuseSaveHandler (window, svg) {
+    frames['svgeditor'].jQuery.post('$FullUrl', { title: '$id', raw: 1,
+						  text: '#FILE $SvgMimeType\\n'
+						  + encode64(svg) } );
+}
+
+function oddmuseInit () {
+    if (frames['svgeditor'].svgCanvas != null) {
+	frames['svgeditor'].svgCanvas.bind("saved", oddmuseSaveHandler);
+    } else {
+	window.setTimeout("oddmuseInit()", 1000);
+    }
+}
+
+window.setTimeout("oddmuseInit()", 1000);
+</script>
+};
   print GetHeader('', Ts('Editing %s', $id));
-  print $q->iframe({-src => "$SvgEditorUrl?url=" . OldSvgGetDownloadLink($id, 2),
-		    -width => "100%", -height => "500"}, "");
+  # This only works if editor and file are on the same site, I think.
+  print $q->iframe({-src => $SvgEditorUrl . '?url=' . GetDownloadLink($id, 2),
+		    -name => 'svgeditor',
+		    -width => "100%",
+		    -height => "500"}, "");
   PrintFooter($id, 'edit');
 }
