a1029d7b4563c1449e706460cb20f3684d9ae91f
Alex Schroeder <alex@gnu.org>
2011-06-25 13:19:28 +0000

    (InitOffline): Disable surge protection when browsing the offline pages. Add offline path_info to separate the offline app from the ordinary stuff. (DoManifest): Add the offline path_info, remove the missing pages from @UserGotoBarPages because these result in 404 errors. (DoOffline): Fix text.

--- modules/offline.pl
+++ modules/offline.pl
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 
-$ModulesDescription .= '<p>$Id: offline.pl,v 1.2 2011/06/23 00:35:56 as Exp $</p>';
+$ModulesDescription .= '<p>$Id: offline.pl,v 1.3 2011/06/25 13:19:28 as Exp $</p>';
 
 # Based on http://diveintohtml5.org/offline.html
 
@@ -29,18 +29,28 @@ sub OfflineMenu {
 push(@MyInitVariables, \&InitOffline);
 
 sub InitOffline {
-  # Switch to HTML5:
+  # Make sure we parse path_info and parameters
+  GetId();
+  # Switch to HTML5 if the offline parameter is set
   if (GetParam('offline', 0)) {
+    # add link to the manifest listing all the pages
     my $manifest = ScriptUrl('action=manifest');
     $DocumentHeader = qq{<!DOCTYPE HTML>
 <html manifest="$manifest">
 };
-  }
-  if ($HtmlHeaders !~ /apple-mobile-web-app-capable/) {
-    $HtmlHeaders .= qq{
+    # HACK ALERT: In order to allow the browser to cache all the pages
+    # listed in the manifest, we need to disable surge protection for
+    # the offline pages.
+    $SurgeProtection = 0;
+    # every offline page will link to other offline pages
+    $ScriptName .= '/offline' unless $ScriptName =~ m!/offline$!;
+    # add some links for Apple devices (boo!)
+    if ($HtmlHeaders !~ /apple-mobile-web-app-capable/) {
+      $HtmlHeaders .= qq{
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 };
+    }
   }
 }
 
@@ -49,20 +59,25 @@ $Action{manifest} = \&DoManifest;
 # List all the pages necessary for the offline application.
 sub DoManifest {
   print GetHttpHeader('text/cache-manifest');
-  my $offline = ScriptUrl('action=offline');
   print "CACHE MANIFEST\n";
+  # make sure to list the URLs for the offline version
+  local $ScriptName = $ScriptName . '/offline';
+  # don't forget to URL encode
   foreach my $id (AllPagesList()) {
-    print ScriptUrl($id) . "\n";
+    print ScriptUrl(UrlEncode($id)) . "\n";
   }
   # Missing pages that should show the default text such as
-  # RecentChanges
-  foreach my $id (@UserGotoBarPages) {
-    print ScriptUrl($id) . "\n" unless $IndexHash{$id};
-  }
+  # RecentChanges cannot be added because fetching them results in a
+  # 404 error.
+  # foreach my $id (@UserGotoBarPages) {
+  #   print ScriptUrl($id) . "\n" unless $IndexHash{$id};
+  # }
   # External CSS
   print $StyleSheet . "\n" if $StyleSheet;
   # FIXME: $StyleSheetPage
   # FIXME: external images, stuff in $HtmlHeaders
+  # Error message all the stuff that's not available offline.
+  my $offline = ScriptUrl('action=offline');
   print qq{
 FALLBACK:
 / $offline
@@ -77,5 +92,5 @@ $Action{offline} = \&DoOffline;
 sub DoOffline {
   ReportError(T('Offline'),
 	      '503 SERVICE UNAVAILABLE',
-	      0, $q->p(T('You are currently offline and the page you requested has not been added to the cache. Reconnect to the network and visit the page in order to add it to the cache.')));
+	      0, $q->p(T('You are currently offline and what you requested is not part of the offline application. You need to be online to do this.')));
 }
