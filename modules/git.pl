ac9a76b5be78b1e8c50d4d6afeb2d0e5fefbefa8
Alex Schroeder <alex@gnu.org>
2011-11-04 22:42:26 +0000

    *** empty log message ***

--- modules/git.pl
+++ modules/git.pl
@@ -21,12 +21,6 @@ git - An Oddmuse module to save all changes made into a git repository.
 This module is easily installable; move this file into the B<modules>
 subdirectory for your data directory (C<$DataDir>).
 
-You should make sure that the script can execute git. Make sure to add
-git's directory to C<$ENV{PATH}> in your B<config> file, if necessary
-(the default value is pretty restricted):
-
-    $ENV{PATH} .= ":/usr/local/bin"; # git
-
 =cut
 
 package OddMuse;
@@ -34,6 +28,12 @@ package OddMuse;
 =head1 CONFIGURATION
 
 Set these variables in the B<config> file within your data directory.
+
+=head2 $GitBinary
+
+Default: C</usr/bin/git>
+
+The fully qualified name for the binary to run. Your PATH will not be searched.
 
 =head2 $GitDir
 
@@ -50,11 +50,30 @@ The email address used to identify users in git.
 
 =cut
 
-use vars qw($GitDir $GitMail);
+use vars qw($GitBinary $GitDir $GitMail);
 
+$GitBinary = '/usr/bin/git';
 $GitMail = 'unknown@oddmuse.org';
 
 push(@MyInitVariables, \&GitInitVariables);
+
+sub GitRun {
+  my $result = '';
+  local *STDOUT;
+  open(STDOUT, '>', \$result) or die "Can't open memory file: $!";
+  system($GitBinary, @_) == 0
+    or ReportError("git failed: $!",
+		   "500 INTERNAL SERVER ERROR",
+		   undef,
+		   $q->p($q->tt(join(' ', $GitBinary, map {
+		     if (index($_, ' ') == -1) {
+		      $_;
+		     } else {
+		       "'$_'";
+		     }
+		   } @_))),
+		   $q->pre($result));
+}
 
 sub GitInitVariables {
   $GitDir = $DataDir . '/git';
@@ -64,17 +83,14 @@ sub GitInitRepository {
   if (not -d "$GitDir/.git") {
     CreateDir($GitDir);
     chdir($GitDir); # important for all the git commands that follow!
-    system(qw(git init --quiet))  == 0
-      or ReportError("git init failed: $!");
+    GitRun('init', '--quiet');
     foreach my $id (AllPagesList()) {
       OpenPage($id);
       WriteStringToFile("$GitDir/$id", $Page{text});
-      system(qw(git add), $id)  == 0
-	or ReportError("git add $id failed: $!");
+      GitRun('add', $id);
     }
-    system(qw(git commit -m), "initial import",
-	   "--author=Oddmuse <$GitMail>") == 0
-	     or ReportError("git initial commit failed: $!");
+    GitRun('commit', '-m', 'initial import',
+	   "--author=Oddmuse <$GitMail>");
   } else {
     chdir($GitDir); # important for all the git commands that follow!
   }
@@ -89,14 +105,14 @@ sub GitNewSave {
   my ($id) = @_;
   WriteStringToFile("$GitDir/$id", $Page{text});
   if ($Page{revision} == 1) {
-    system(qw(git add), $id) == 0
-      or ReportError("git add $id failed: $!");
+    GitRun('add', $id);
   }
-  my $message = $Page{summary} || T('no summary available');
+  my $message = $Page{summary};
+  $message =~ s/^\s+$//;
+  $message ||= T('no summary available');
   my $author = $Page{username} || T('Anonymous');
-  system(qw(git commit --quiet -m), $message,
-	 "--author=$author <$GitMail>",
-	 $id) == 0 or ReportError("git commit $id failed: $!");
+  GitRun('commit', '--quiet', '-m', $message,
+	 "--author=$author <$GitMail>", $id);
 }
 
 *GitOldDeletePage = *DeletePage;
@@ -107,13 +123,11 @@ sub GitNewDeletePage {
   return $error if $error;
   GitInitRepository();
   my ($id) = @_;
-  system(qw(git rm --quiet --ignore-unmatch), $id) == 0
-    or ReportError("git rm $id failed: $!");
+  GitRun('rm', '--quiet', '--ignore-unmatch', $id);
   my $message = T('page marked was for deletion');
   my $author = T('Oddmuse');
-  system(qw(git commit --quiet -m), $message,
-	 "--author=$author <$GitMail>",
-	 $id) == 0 or ReportError("git commit $id failed: $!");
+  GitRun('commit', '--quiet', '-m', $message,
+	 "--author=$author <$GitMail>", $id);
   return ''; # no error
 }
 
@@ -135,10 +149,8 @@ sub GitCleanup {
     }
     # commit the new state
     chdir($GitDir); # important for all the git commands that follow!
-    system(qw(git add --all)) == 0
-      or ReportError("git add -A failed: $!");
-    system(qw(git commit --quiet -m), 'maintenance job',
-	   "--author=Oddmuse <$GitMail>",
-	   $id) == 0 or ReportError("git maintenance commit failed: $!");
+    GitRun('add', '--all');
+    GitRun('commit', '--quiet', '-m', 'maintenance job',
+	   "--author=Oddmuse <$GitMail>", $id);
   }
 }
