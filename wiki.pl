5769928c8998efc8a8c29415cb26770396b7a188
Alex Schroeder <alex@gnu.org>
2011-11-19 15:28:26 +0000

    (PrintAllPages): If the number of pages to print is not provided, do not test.

--- wiki.pl
+++ wiki.pl
@@ -1,5 +1,5 @@
 #! /usr/bin/perl
-# Version       $Id: wiki.pl,v 1.954 2011/11/19 15:19:34 as Exp $
+# Version       $Id: wiki.pl,v 1.955 2011/11/19 15:28:26 as Exp $
 # Copyleft      2008 Brian Curry <http://www.raiazome.com>
 # Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010
 #     Alex Schroeder <alex@gnu.org>
@@ -36,7 +36,7 @@ use CGI::Carp qw(fatalsToBrowser);
 use vars qw($VERSION);
 local $| = 1;  # Do not buffer output (localized for mod_perl)
 
-$VERSION=(split(/ +/, q{$Revision: 1.954 $}))[1]; # for MakeMaker
+$VERSION=(split(/ +/, q{$Revision: 1.955 $}))[1]; # for MakeMaker
 
 # Options:
 use vars qw($RssLicense $RssCacheHours @RcDays $TempDir $LockDir $DataDir
@@ -290,7 +290,7 @@ sub InitRequest {
 sub InitVariables {  # Init global session variables for mod_perl!
   $WikiDescription = $q->p($q->a({-href=>'http://www.oddmuse.org/'}, 'Oddmuse'),
          $Counter++ > 0 ? Ts('%s calls', $Counter) : '')
-    . $q->p(q{$Id: wiki.pl,v 1.954 2011/11/19 15:19:34 as Exp $});
+    . $q->p(q{$Id: wiki.pl,v 1.955 2011/11/19 15:28:26 as Exp $});
   $WikiDescription .= $ModulesDescription if $ModulesDescription;
   $PrintedHeader = 0; # Error messages don't print headers unless necessary
   $ReplaceForm = 0;   # Only admins may search and replace
@@ -885,7 +885,7 @@ sub PrintAllPages {
   my ($i, $n) = 0;
   for my $id (@pages) {
     $i++; # pages looked at
-    last if $n >= $JournalLimit and not UserIsAdmin() or $n >= $num;
+    last if $n >= $JournalLimit and not UserIsAdmin() or $num and $n >= $num;
     local ($OpenPageName, %Page); # this is local!
     OpenPage($id);
     my @languages = split(/,/, $Page{languages});
