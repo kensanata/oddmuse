a1c5e8cfe360871d0631e4ab0ea44989dd925d23
Alex Schroeder <alex@gnu.org>
2012-10-26 16:20:30 +0200

    Also handles other parameter per feed, eg. ignore_in_feed = updated

--- contrib/campaignwiki/submit.pl
+++ contrib/campaignwiki/submit.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# Copyright (C) 2010  Alex Schroeder <alex@gnu.org>
+# Copyright (C) 2010, 2012  Alex Schroeder <alex@gnu.org>
 
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -17,6 +17,7 @@
 package OddMuse;
 use URI;
 use LWP::UserAgent;
+use utf8;
 
 # load Oddmuse core
 $RunCGI = 0;
@@ -61,16 +62,16 @@ sub parse_blogs {
   return %cached_blogs if %cached_blogs;
   my @data = split(/\n/, GetRaw($src));
   my $url;
-  my $name;
+  my $paramref;
   foreach $_ (@data) {
     if (/^\[(.+)\]/) {
       $url = $1;
-      $name = undef;
-    } elsif (/^name *= *(.+)/) {
-      $name = $1;
+      $paramref = {};
+    } elsif (/^([a-z_]+) *= *(.+)/) {
+      $paramref->{$1} = $2;
     }
-    if ($url && $name) {
-      $cached_blogs{$url} = $name;
+    if ($url && $paramref->{name}) {
+      $cached_blogs{$url} = $paramref;
     }
   }
   return %cached_blogs;
@@ -84,8 +85,16 @@ sub host_exists {
   }
 }
 
+sub debug_url {
+  my $url = $q->url(-path_info=>1) . "?debug=1;";
+  $url .= join(";", map { $_ . "=" . GetParam($_) }
+	       qw(username confirmed candidate url));
+  return $url;
+}
+
 sub check_url {
   my $url = shift;
+  print $q->p("Debug: url=$url") if GetParam("debug");
   my $frown = $q->img({-src=>"http://emacswiki.org/pics/smiles/sad.png",
 		       -alt=>":("});
   my $smile = $q->img({-src=>"http://emacswiki.org/pics/smiles/smile.png",
@@ -97,26 +106,24 @@ sub check_url {
     $u = URI->new($url);
     eval {$u->host };
   }
+
+  # - not an url
+  # - it's campaign wiki site
+  # - no username
+  # or read Feeds page and
+  #   - it's a duplicate
+  #   - it's a partial match: continue with confirmed=1
+  # or read the list of alternatives from the url
+  #     - one of the feeds listed is known: continue with confirmed=2
+  #     - no feeds were listed: url is a feed or report it
+  #     - one feed was listed: try it
+  #     - some feeds were listed: pick one
+
   if ($@) {
     # the prefixing of http:// above should make it really hard to reach this code
     print $q->p($q->a({-href=>$url}, $url) . qq{
 seems to be <strong>invalid</strong>. $frown Make sure you use something
 like the following: <tt>http://grognardia.blogspot.com/</tt>});
-  } elsif (not GetParam('username', '')) {
-    print $q->p(qq{As an anti-spam measure I'd really like you to <strong>provide a name</strong> for the log file. Sorry about that. $frown});
-  } else {
-    my %blogs = parse_blogs();
-    my $duplicate = host_exists($u->host, %blogs);
-    if ($duplicate
-	&& !$blogs{$url}
-	&& !GetParam('confirmed')) {
-      print $q->p("We have a partial match: ",
-		  $q->a({-href=>$duplicate}, $duplicate));
-      print GetFormStart();
-      print $q->hidden('confirmed', 1);
-      print $q->hidden('url', $url);
-      print $q->submit('go', 'Proceed anyway!');
-      print $q->end_form();
     } elsif ($url =~ /campaignwiki\.org/i) {
       print $q->p(qq{
 This looks <strong>familiar</strong>!
@@ -124,17 +131,35 @@ I do not think that adding any of the wikis on this site is the right
 thing to do, though.});
       print $q->p(qq{Thanks for testing it. }
 		  . $q->img({-src=>"http://www.emacswiki.org/pics/grin.png"}));
+  } elsif (not GetParam('username', '')) {
+    print $q->p(qq{As an anti-spam measure I'd really like you to
+<strong>provide a name</strong> for the log file. Sorry about that. $frown});
+  } else {
+    my %blogs = parse_blogs();
+    my $duplicate = host_exists($u->host, %blogs);
+    if ($blogs{$url}) {
+      print $q->p("We already list ",
+		  $q->a({-href=>$duplicate}, $duplicate));
+    } elsif ($duplicate	&& !GetParam('confirmed')) {
+      print $q->p("We have a partial match: ",
+		  $q->a({-href=>$duplicate}, $duplicate));
+      print GetFormStart();
+      print $q->hidden('confirmed', 1);
+      print $q->hidden('url', $url);
+      print $q->submit('go', 'Proceed anyway!');
+      print $q->end_form();
     } else {
-      my @alternatives = get_feeds($url, keys %blogs);
-      if ($#alternatives > 0 && !GetParam('candidate')) {
+      my ($status, @alternatives) = get_feeds($url, %blogs);
+      if ($status eq 'known' && GetParam('confirmed') < 2) {
+	print $q->p($q->a({-href=>$url},
+			  "The page you submitted")
+		    . " lists "
+		    . $q->a({-href=>$alternatives[0]},
+			    "a known feed") . ".");
 	print GetFormStart();
-	print $q->hidden('confirmed', 1);
+	print $q->hidden('confirmed', 2);
 	print $q->hidden('url', $url);
-	print $q->p("You need to pick one of the candidates:");
-	print $q->p(join($q->br(), map {
-	  $q->input({-type=>"radio", -name=>"candidate", -value=>$_},
-		    $q->a({-href=>$_}, QuoteHtml($_))) } @alternatives));
-	print $q->submit('go', 'Submit');
+	print $q->submit('go', 'Proceed anyway!');
 	print $q->end_form();
       } elsif ($#alternatives < 0) {
 	if (is_feed($url)) {
@@ -143,20 +168,31 @@ thing to do, though.});
 	  print $q->p("Apparently " . $q->a({-href=>$url}, QuoteHtml($url))
 		      . " is not a feed and doesn't link to any feed. "
 		      . "There is nothing for me to add. " . $frown);
+	  print $q->p("If you feel like it, you could try to "
+		      . $q->a({-href=>debug_url()}, "debug")
+		      . " this.");
 	}
+      } elsif ($#alternatives == 0) {
+	print $q->p($q->a({-href=>$url}, "The page you submitted")
+		    . " lists "
+		    . $q->a({-href=>$alternatives[0]},
+			    "one new feed")
+		    .  ".");
+	print GetFormStart();
+	print $q->hidden('url', $alternatives[0]);
+	print $q->submit('go', 'Take it!');
+	print $q->end_form();
+	print $q->p("If you feel like it, you could try to "
+		    . $q->a({-href=>debug_url()}, "debug")
+		    . " this.");
       } else {
-	my $candidate = GetParam('candidate');
-	$candidate = $alternatives[0] unless $candidate;
-	if (is_feed($candidate)) {
-	  post_addition($candidate);
-	} else {
-	  print $q->p($q->a({-href=>$candidate}, "The page you submitted")
-		      . " listed "
-		      . $q->a({-href=>$candidate}, QuoteHtml($candidate))
-		      . " as one of its feeds. "
-		      . "But it turns out that this is not a valid feed! "
-		      . "I can't add an invalid feed. " . $frown);
-	}
+	print GetFormStart();
+	print $q->p("You need to pick one of the candidates:");
+	print $q->p(join($q->br(), map {
+	  $q->input({-type=>"radio", -name=>"url", -value=>$_},
+		    $q->a({-href=>$_}, QuoteHtml($_))) } @alternatives));
+	print $q->submit('go', 'Submit');
+	print $q->end_form();
       }
     }
   }
@@ -172,23 +208,73 @@ sub is_feed {
 
 sub get_feeds {
   my $url = shift;
-  my %others = map { $_ => 1 } @_;
-  my @links = GetRaw($url) =~ /<link\b *(.*?)>/g;
+  my %others = @_;
+  my $html = GetRaw($url);
+  my @links = $html =~ /<link\b *(.*?)>/g;
+  print $q->p("Debug: " . scalar(@links) . " links found") if GetParam("debug");
+  print $q->pre($html) unless scalar(@links);
+  print $q->p("Debug: no content returned") if GetParam("debug") and not $html;
   my @feeds;
   foreach my $link (@links) {
-    my %link;
-    foreach (split(/ /, lc($link))) {
-      my ($attr, $val) = split(/=/, $_, 2);
-      # strip quotes and garbage: "foo"/ -> foo
-      my $to = index($val, substr($val, 0, 1), 1);
-      $val = substr($val, 1, $to -1) if $to >= 0;
-      $link{$attr} = $val;
+
+    print $q->p("Debug: $link")
+      if GetParam("debug");
+
+    if ($link !~ /\brel=(['"])alternate\1/i) {
+      print $q->p("Debug: missing rel='alternate'")
+	if GetParam("debug");
+      next;
     }
-    next unless $link{rel} eq 'alternate';
-    next unless $valid_content_type{$link{type}};
-    push(@feeds, $link{href}) unless $others{$link{href}};
+
+    $link =~ /\btype=(['"])(.*?)\1/i;
+    my $type = $2;
+    if (not $valid_content_type{$type}) {
+      print $q->p("Debug: type parameter is invalid ($type)")
+	if GetParam("debug");
+      next;
+    }
+
+    $link =~ /\bhref=(['"])(.*?)\1/i;
+    my $href = $2;
+    # clean up blogspot urls and prefer atom format
+    $href =~ s/\?alt=rss$//i if $href =~ /blogspot/i;
+    if (not $href) {
+      print $q->p("Debug: href missing")
+	if GetParam("debug");
+      next;
+    }
+    if ($others{$href}) {
+      print $q->p("Debug: feed already known ($href)")
+	if GetParam("debug");
+      if ($q->param('confirmed') >= 2) {
+	next;
+      } else {
+	# don't look for other alternatives!
+	return 'known', $href;
+      }
+    }
+
+    push(@feeds, $href);
   }
-  return @feeds;
+  print $q->p("Debug: returning " . scalar(@feeds) . " links found")
+    if GetParam("debug");
+  return 'ok', @feeds;
+}
+
+sub config {
+  my %blogs = @_;
+  my $result = qq{#! config file for the RPG Planet
+# format:
+# Feed URL in square brackets, followed by name = and the name of the feed
+};
+  foreach my $url (sort {lc($blogs{$a}->{name}) cmp lc($blogs{$b}->{name})} keys %blogs) {
+    $result .= "[$url]\n";
+    $paramref = $blogs{$url};
+    foreach my $key (sort keys %{$paramref}) {
+      $result .= $key . " = " . $paramref->{$key} . "\n";
+    }
+  }
+  return $result;
 }
 
 sub post_addition {
@@ -206,14 +292,9 @@ sub post_addition {
     $title = $final_url unless $title;
     print $q->p("Adding ",
 		$q->a({-href=>$final_url}, $title));
-    $blogs{$url} = $title;
-    my $result = qq{#! config file for the RPG Planet
-# format:
-# Feed URL in square brackets, followed by name = and the name of the feed
-};
-    foreach $url (sort {lc($blogs{$a}) cmp lc($blogs{$b})} keys %blogs) {
-      $result .= "[$url]\nname = " . $blogs{$url} . "\n";
-    }
+    my %param = (name => $title);
+    $blogs{$url} = \%param;
+    my $result = config(%blogs);
     my $ua = LWP::UserAgent->new;
     my %params = (text => $result,
 		  title => $page,
@@ -250,16 +331,18 @@ sub main {
   if ($q->path_info eq '/source') {
     seek DATA, 0, 0;
     print "Content-type: text/plain; charset=UTF-8\r\n\r\n", <DATA>;
+  } elsif ($q->path_info eq '/test') {
+    print "Content-type: text/plain; charset=UTF-8\r\n\r\n";
+    print config(parse_blogs());
   } else {
     $UserGotoBar .= $q->a({-href=>$q->url . '/source'}, 'Source');
     print GetHeader('', 'Submit a new blog');
     print $q->start_div({-class=>'content index'});
-    if (not GetParam('url')
-	or not GetParam($HoneyPotOk)) {
+    if (not GetParam('url')) {
+      print $q->p("Debug: no url parameter provided.") if GetParam("debug");
       default();
     } else {
       SetParam('title', 'Feeds'); # required to trigger HoneyPotInspection()
-      HoneyPotInspection();
       check_url(GetParam('url'));
     }
     print $q->p('Questions? Send mail to Alex Schröder <'
