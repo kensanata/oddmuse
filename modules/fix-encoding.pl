bcb97214993f374016f2724e34ed752d9b815b40
Alex Schroeder <alex@gnu.org>
2012-10-27 13:28:13 +0200

    New extension.

---
+++ modules/fix-encoding.pl
@@ -0,0 +1,40 @@
+# Copyright (C) 2012  Alex Schroeder <alex@gnu.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+$ModulesDescription .= '<p><a href="http://git.savannah.gnu.org/cgit/oddmuse.git/tree/modules/fix-encoding.pl">fix-encoding.pl</a></p>';
+
+$Action{'fix-encoding'} = \&FixEncoding;
+
+sub FixEncoding {
+  my $id = shift;
+  ValidIdOrDie($id);
+  RequestLockOrError();
+  OpenPage($id);
+  my $text = $Page{text};
+  utf8::decode($text);
+  Save($id, $text, 'fix encoding', 1);
+  ReleaseLock();
+  ReBrowsePage($id);
+}
+
+push(@MyAdminCode, \&FixEncodingMenu);
+
+sub FixEncodingMenu {
+  my ($id, $menuref, $restref) = @_;
+  if ($id) {
+    push(@$menuref,
+	 ScriptLink('action=fix-encoding;id=' . UrlEncode($id), T('Fix page encoding')));
+  }
+}
