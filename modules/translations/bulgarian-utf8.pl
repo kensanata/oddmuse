5c41aa3fd0efd42f0247af52228f64722c6e7a5f
Alex Schroeder <alex@gnu.org>
2012-11-20 15:47:03 +0100

    Add header back to bulgarian-utf8.pl and chinese-utf8.pl

--- modules/translations/bulgarian-utf8.pl
+++ modules/translations/bulgarian-utf8.pl
@@ -1,3 +1,25 @@
+# UTF-8 encoded Bulgarian translation file for use with Oddmuse
+#
+# Copyright (c) 2004 Stanislav Traykov <stanislav@tortoises.org>
+# Copyright (c) 2003, 2004  Alex Schröder <alex@emacswiki.org>
+#
+# Permission is granted to copy, distribute and/or modify this
+# document under the terms of the GNU Free Documentation License,
+# Version 1.2 or any later version published by the Free Software
+# Foundation; with no Invariant Sections, no Front-Cover Texts, and no
+# Back-Cover Texts.  A copy of the license could be found at:
+# http://www.gnu.org/licenses/fdl.txt .
+#
+# Installation:
+# =============
+#
+# Create a modules subdirectory in your data directory, and put the
+# file in there. It will be loaded automatically.
+#
+# This translation was updated for Oddmuse 1.354.
+#
+use utf8;
+$ModulesDescription .= '<p><a href="http://git.savannah.gnu.org/cgit/oddmuse.git/tree/modules/translations/bulgarian-utf8.pl">bulgarian-utf8.pl</a>, see <a href="http://www.oddmuse.org/cgi-bin/oddmuse/Bulgarian">Bulgarian</a></p>';
 %Translate = split(/\n/,<<END_OF_TRANSLATION);
 Include normal pages
 
