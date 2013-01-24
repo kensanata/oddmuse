270e0f4932032037fa55975ba54c809726bd676f
Alex Schroeder <alex@gnu.org>
2013-01-24 23:04:35 +0100

    Invert the loop when scanning for banned content.

--- modules/list-banned-content.pl
+++ modules/list-banned-content.pl
@@ -19,12 +19,41 @@ $Action{'list-banned-content'} = \&DoListBannedContent;
 sub DoListBannedContent {
   print GetHeader('', T('Banned Content'), '');
   my @pages = AllPagesList();
+  my %url_regexps;
+  my %text_regexps;
+  foreach (split(/\n/, GetPageContent($BannedContent))) {
+    next unless m/^\s*([^#]+?)\s*(#\s*(\d\d\d\d-\d\d-\d\d\s*)?(.*))?$/;
+    $url_regexps{qr($1)} = $4;
+  }
+  foreach (split(/\n/, GetPageContent($BannedRegexps))) {
+    next unless m/^\s*([^#]+?)\s*(#\s*(\d\d\d\d-\d\d-\d\d\s*)?(.*))?$/;
+    $text_regexps{qr($1)} = $4;
+  }
   print '<div class="content banned"><p>';
-  foreach my $id (@pages) {
+ PAGE: foreach my $id (@pages) {
     OpenPage($id);
-    my $rule = BannedContent($Page{text});
-    if ($rule) {
-      print GetPageLink($id) . ': ' . $rule . $q->br();
+    my @urls = $str =~ /$FullUrlPattern/go;
+    foreach my $url (@urls) {
+      foreach my $re (keys %url_regexps) {
+	if ($url =~ $re) {
+	  print GetPageLink($id) . ': '
+	    . Tss('Rule "%1" matched "%2" on this page.', $re, $url) . ' '
+	      . ($url_regexps{$re}
+		 ? Ts('Reason: %s.', $url_regexps{$re})
+		 : T('Reason unknown.')) . $q->br() . "\n";
+	  next PAGE;
+	}
+      }
+    }
+    foreach my $re (keys %text_regexps) {
+      if ($Page{text} =~ $re) {
+	print GetPageLink($id) . ': '
+	  . Tss('Rule "%1" matched on this page.', $re) . ' '
+	    . ($text_regexps{$re}
+	       ? Ts('Reason: %s.', $text_regexps{$re})
+	       : T('Reason unknown.')) . $q->br() . "\n";
+	next PAGE;
+      }
     }
   }
   print '</p></div>';
