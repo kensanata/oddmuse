a4bd6383a20a6397175f09e4e27b3fc36ac6dbe3
Alex Schroeder <alex@gnu.org>
2013-08-16 00:30:58 +0200

    Quote the unquoted string "commenthidden".

--- modules/dynamic-comments.pl
+++ modules/dynamic-comments.pl
@@ -1,20 +1,16 @@
-# Copyright (C) 2005  Alex Schroeder <alex@emacswiki.org>
+# Copyright (C) 2005-2013  Alex Schroeder <alex@gnu.org>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-#    Free Software Foundation, Inc.
-#    59 Temple Place, Suite 330
-#    Boston, MA 02111-1307 USA
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
 
 $ModulesDescription .= '<p><a href="http://git.savannah.gnu.org/cgit/oddmuse.git/tree/modules/dynamic-comments.pl">dynamic-comments.pl</a>, see <a href="http://www.oddmuse.org/cgi-bin/oddmuse/Dynamic_Comments_Extension">Dynamic Comments Extension</a></p>';
 
@@ -51,7 +47,7 @@ sub DynamicCommentsNewGetPageLink {
       my $anchor = "id" . $num++;
       return qq{<a href="javascript:togglecomments('$anchor')">$title</a>}
         . '</p>' # close p before opening div
-        . $q->div({-class=>commenthidden, -id=>$anchor},
+        . $q->div({-class=>'commenthidden', -id=>$anchor},
                   $page,
                   $q->p(DynamicCommentsOldGetPageLink($id, T('Add Comment'))))
         . '<p>'; # open an empty p that will be closed in PrintAllPages
