5b0578bf94c0949b2e0fb1db5938a476ad0a4491
Alex Schroeder <alex@gnu.org>
2013-08-15 23:46:31 +0200

    New module: don't allow quick editing by an IP or username.

---
+++ modules/ban-quick-editors.pl
@@ -0,0 +1,37 @@
+# Copyright (C) 2013  Alex Schroeder <alex@gnu.org>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# This file must load before logbannedcontent.pl such that quick
+# editors will be logged.
+
+$ModulesDescription .= '<p><a href="http://git.savannah.gnu.org/cgit/oddmuse.git/tree/modules/ban-quick-editors.pl">ban-quick-editors.pl</a>, see <a href="http://www.oddmuse.org/cgi-bin/oddmuse/Banning_Quick_Editors">Banning Quick Editors</a></p>';
+
+*BanQuickOldUserIsBanned = *UserIsBanned;
+*UserIsBanned = *BanQuickNewUserIsBanned;
+
+sub BanQuickNewUserIsBanned {
+  my $rule = BanQuickOldUserIsBanned(@_);
+  if (not $rule
+      and $SurgeProtection # need surge protection
+      and GetParam('title')) {
+    my $name = GetParam('username', $ENV{'REMOTE_ADDR'});
+    my @entries = @{$RecentVisitors{$name}};
+    # $entry[0] is $Now after AddRecentVisitor
+    my $ts = $entries[1];
+    if ($Now - $ts < 5) {
+      return "fast editing spam bot";
+    }
+  }
+  return $rule;
+}
---
+++ modules/ban-yourself.pl
@@ -0,0 +1,7 @@
+# This extension keeps tracks of the IP numbers that try to POST
+# banned stuff. Three strikes and they're out: The IP number gets
+# added to BannedHosts.
+#
+# We're not considering bots that POST without following the
+# redirect... An idea for later!
+
