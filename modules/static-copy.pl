bf1991cdc8f6dcd54f6b3444fea0847d76346630
Alex Schroeder <alex@gnu.org>
2013-10-25 08:49:13 +0200

    Static action now takes $StaticAlways into account.

--- modules/static-copy.pl
+++ modules/static-copy.pl
@@ -1,20 +1,16 @@
-# Copyright (C) 2004, 2005, 2006  Alex Schroeder <alex@emacswiki.org>
+# Copyright (C) 2004-2013  Alex Schroeder <alex@gnu.org>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-#    Free Software Foundation, Inc.
-#    59 Temple Place, Suite 330
-#    Boston, MA 02111-1307 USA
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
 
 $ModulesDescription .= '<p><a href="http://git.savannah.gnu.org/cgit/oddmuse.git/tree/modules/static-copy.pl">static-copy.pl</a>, see <a href="http://www.oddmuse.org/cgi-bin/oddmuse/Static_Copy_Extension">Static Copy Extension</a></p>';
 
@@ -63,7 +59,11 @@ sub StaticWriteFiles {
   local *ScriptLink = *StaticScriptLink;
   local *GetDownloadLink = *StaticGetDownloadLink;
   foreach my $id (AllPagesList()) {
-    StaticWriteFile($id);
+    if ($StaticAlways > 1
+	or GetParam('html', 0)
+	or PageIsUploadedFile($id)) {
+      StaticWriteFile($id);
+    }
   }
 }
 
@@ -122,16 +122,16 @@ sub StaticFileName {
 sub StaticWriteFile {
   my $id = shift;
   my $raw = GetParam('raw', 0);
-  my $html = GetParam('html', 1);
   my $filename = StaticFileName($id);
   OpenPage($id);
   my ($mimetype, $encoding, $data) = $Page{text} =~ /^\#FILE ([^ \n]+) ?([^ \n]*)\n(.*)/s;
-  return unless $html or $data;
   open(F,"> $StaticDir/$filename") or ReportError(Ts('Cannot write %s', $filename));
   if ($data) {
     StaticFile($id, $mimetype, $data);
   } elsif ($html) {
     StaticHtml($id);
+  } else {
+    print "no data for ";
   }
   close(F);
   chmod 0644,"$StaticDir/$filename";
