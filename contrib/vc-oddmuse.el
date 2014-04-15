5cc7d55152be0249e08b999b7f1a8b10123499fd
Alex Schroeder <alex@gnu.org>
2014-04-15 07:39:01 +0200

    License, dependencies.

--- contrib/vc-oddmuse.el
+++ contrib/vc-oddmuse.el
@@ -1,12 +1,35 @@
-(require 'oddmuse)
-(require 'diff)
+;;; vc-oddmuse.el -- add VC support to oddmuse-curl
+;; 
+;; Copyright (C) 2014  Alex Schroeder <alex@gnu.org>
+;; 
+;; Latest version:
+;;   http://git.savannah.gnu.org/cgit/oddmuse.git/plain/contrib/vc-oddmuse.el
+;; Discussion, feedback:
+;;   http://www.emacswiki.org/cgi-bin/wiki/OddmuseCurl
+;; 
+;; This program is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+;; 
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+;; more details.
+;; 
+;; You should have received a copy of the GNU General Public License along
+;; with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
-;;; Commentary
-
+;;; Commentary:
+;;
 ;; Add the following to your init file:
+;;
 ;; (add-to-list 'vc-handled-backends 'oddmuse)
 
 (add-to-list 'vc-handled-backends 'oddmuse)
+
+(require 'oddmuse)
+(require 'diff)
 
 (defun vc-oddmuse-revision-granularity () 'file)
 
