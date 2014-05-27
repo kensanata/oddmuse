5f582565433daef55574e478ddbb2cf6bfd389bf
Aki Goto <tyatsumi@gmail.com>
2014-05-27 14:45:50 +0200

    Updated Japanese translation.

--- modules/translations/japanese-utf8.pl
+++ modules/translations/japanese-utf8.pl
@@ -1,12 +1,12 @@
 # UTF-8 encoded Japanese translation file for use with Oddmuse
 #
-# Copyright (c) 2003  ...
-#
-# Permission is granted to copy, distribute and/or modify this
-# document under the terms of the GNU Free Documentation License,
-# Version 1.2 or any later version published by the Free Software
-# Foundation; with no Invariant Sections, no Front-Cover Texts, and no
-# Back-Cover Texts.  A copy of the license could be found at:
+# Copyright (c) 2014  Aki Goto <tyatsumi@gmail.com>
+# 
+# Permission is granted to copy, distribute and/or modify this document
+# under the terms of the GNU Free Documentation License, Version 1.3
+# or any later version published by the Free Software Foundation;
+# with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
+# A copy of the license could be found at:
 # http://www.gnu.org/licenses/fdl.txt .
 #
 # Installation:
@@ -15,1059 +15,497 @@
 # Create a modules subdirectory in your data directory, and put the
 # file in there. It will be loaded automatically.
 #
-# This translation was last checked for Oddmuse version 1.215.
-#
 use utf8;
-$ModulesDescription .= '<p><a href="http://git.savannah.gnu.org/cgit/oddmuse.git/tree/modules/translations/japanese-utf8.pl">japanese-utf8.pl</a>, see <a href="http://www.oddmuse.org/cgi-bin/oddmuse/Japanese">Japanese</a></p>';
+$ModulesDescription .= '<p>japanese-utf8.pl</p>';
 %Translate = split(/\n/,<<END_OF_TRANSLATION);
-Include normal pages
-
+This page is empty.
+このページは空です。
+Add your comment here.
+ここにコメントを追加してください。
 Reading not allowed: user, ip, or network is blocked.
-
+閲覧は許可されません: ユーザ、IP、またはネットワークがブロックされています。
 Login
-
+ログイン
 Error
-
+エラー
 %s calls
-
-Could not create %s
-
+%s回呼び出し
+Cannot create %s
+%s を作れません
+Include normal pages
+通常ページを含める
 Invalid UserName %s: not saved.
-
+不正なユーザ名 %s: 保存されませんでした。
 UserName must be 50 characters or less: not saved
-
+ユーザ名は50文字以下でなければなりません: 保存されませんでした
 This page contains an uploaded file:
-
+このページはアップロードされたファイルを含んでいます:
 Recursive include of %s!
-
+%s の再帰的取り込み！
 Clear Cache
-
+キャッシュを消去
 Main lock obtained.
-
+メインロックを取得しました。
 Main lock released.
-
+メインロックを開放しました。
 Journal
-
+ジャーナル
 More...
-
+もっと...
 Comments on this page
-
+このページのコメント
 XML::RSS is not available on this system.
-
+このシステムではXML::RSSは利用できません。
 diff
-
+差分
 history
-
+履歴
 %s returned no data, or LWP::UserAgent is not available.
-
+%s がデータを返さなかったか、LWP::UserAgentが利用できません。
 RSS parsing failed for %s
-
+%s のRSS解析に失敗しました
 No items found in %s.
-
+%s に項目がありません。
  . . . . 
-
+ . . . . 
 Click to edit this page
-
+このページを編集するにはクリックしてください
 CGI Internal error: %s
-
+CGI内部エラー: %s
 Invalid action parameter %s
-
+不正なアクションパラメタ %s
 Page name is missing
-
+ページ名がありません
 Page name is too long: %s
-
+ページ名が長すぎます: %s
 Invalid Page %s (must not end with .db)
-
+不正なページ %s (.db で終わってはいけない)
 Invalid Page %s (must not end with .lck)
-
+不正なページ %s (.lck で終わってはいけない)
 Invalid Page %s
-
+不正なページ %s
 Too many redirections
-
+リダイレクトが多すぎます
 No redirection for old revisions
-
+古いリビジョンはリダイレクトしません
 Invalid link pattern for #REDIRECT
-
+#REDIRECT への不正なリンク形式
 Please go on to %s.
 
 Updates since %s
-
+%sからの更新
+up to %s
+%sまで
 Updates in the last %s days
-
+過去%s日間の更新
 Updates in the last %s day
-
+過去%s日間の更新
 for %s only
 
 List latest change per page only
-
+ページあたりの最新の変更のみを一覧
 List all changes
-
+すべての変更を一覧
 Skip rollbacks
-
+ロールバックを飛ばす
 Include rollbacks
-
+ロールバックを含める
 List only major changes
-
+メジャーな変更のみ一覧
 Include minor changes
-
+マイナーな変更を含める
 %s days
-
+%s日間
 List later changes
-
+最近の変更を一覧
 RSS
-
+RSS
 RSS with pages
-
+ページ付きRSS
 RSS with pages and diff
-
+ページと差分付きRSS
 Filters
-
+フィルタ
 Title:
-
+タイトル:
 Title and Body:
-
+タイトルと本文:
 Username:
-
+ユーザ名:
 Host:
-
+ホスト:
 Follow up to:
 
 Language:
-
+言語:
 Go!
-
+実行！
 (minor)
-
+(マイナー)
 rollback
-
+ロールバック
 new
-
+new
 All changes for %s
-
-from %s
-
+%s へのすべての変更
 This page is too big to send over RSS.
-
+このページはRSSで送るには大きすぎます。
 History of %s
-
+%s の履歴
 Compare
-
+比較
 Deleted
-
+削除指定
 Mark this page for deletion
-
+このページを削除指定する
 No other revisions available
-
+他のリビジョンはありません
 current
-
+現在
 Revision %s
-
+リビジョン %s
 Contributors to %s
-
+%s への投稿者
 Missing target for rollback.
-
+ロールバックする対象がありません。
 Target for rollback is too far back.
-
+ロールバックする対象が古すぎます。
 A username is required for ordinary users.
-
+通常ユーザにはユーザ名が必要です。
 Rolling back changes
-
+変更をロールバック
 The two revisions are the same.
-
+2つのリビジョンは同じです。
 Editing not allowed for %s.
-
+%s の編集は許可されません。
+Rollback of %s would restore banned content.
+%s のロールバックは禁止された内容を復活させることになります。
 Rollback to %s
-
+%s にロールバックする
 %s rolled back
-
+%s はロールバックされました
 to %s
-
+%s に
 Index of all pages
-
+すべてのページの目録
 Wiki Version
-
+ウィキバージョン
 Unlock Wiki
-
+ウィキのロックを解除
 Password
-
+パスワード
 Run maintenance
-
+保守作業を実行
 Unlock site
-
+サイトのロックを解除
 Lock site
-
-Install CSS
-
+サイトをロック
 Unlock %s
-
+%s のロックを解除
 Lock %s
-
+%s をロック
 Administration
-
+管理
 Actions:
-
+アクション:
 Important pages:
-
+重要なページ:
 To mark a page for deletion, put <strong>%s</strong> on the first line.
-
+ページを削除指定するには、最初の行に <strong>%s</strong> と記してください。
+from %s
+%s より
 [Home]
-
+[ホーム]
 redirected from %s
-
+%s からリダイレクト
 %s: 
-
+%s: 
 Click to search for references to this page
-
+クリックするとこのページへの参照を検索する
 Cookie: 
-
+クッキー: 
 Edit this page
-
+このページを編集
 Preview:
-
+プレビュー:
 Preview only, not yet saved
-
+プレビューにすぎません、まだ保存されていません
 Warning
-
+警告
 Database is stored in temporary directory %s
-
+データベースが一時ディレクトリ %s に保存されています
 %s seconds
-
+%s秒
 Last edited
-
+最終編集
 Edited
-
+編集
 by %s
-
+%s による
 (diff)
-
+(差分)
+a
+a
+c
+c
 Edit revision %s of this page
-
+このページのリビジョン %s を編集
 e
-
+e
 This page is read-only
-
+このページは閲覧のみ
 View other revisions
-
+他のリビジョンを見る
 View current revision
-
+現在のリビジョンを見る
 View all changes
-
+すべての変更を見る
 View contributors
-
+投稿者を見る
 Homepage URL:
-
+ホームページURL:
 s
-
+s
 Save
-
+保存
 p
-
+p
 Preview
-
+プレビュー
 Search:
-
+検索:
 f
-
+f
 Replace:
-
+置換:
 Delete
-
+削除
 Validate HTML
-
+HTMLを確認
 Validate CSS
-
+CSSを確認
 Last edit
-
+最終編集
+Summary:
+要約:
 Difference between revision %1 and %2
-
+リビジョン %1 と %2 の違い
 revision %s
-
+リビジョン %s
 current revision
-
+現在のリビジョン
 Last major edit (%s)
-
+最終のメジャーな編集 (%s)
 later minor edits
-
+その後のマイナーな編集
 No diff available.
-
+差分はありません。
 Old revision:
-
+古いリビジョン:
 Changed:
-
+変更:
 Deleted:
-
+削除:
 Added:
-
+追加:
 to
 
 Revision %s not available
-
+リビジョン %s はありません
 showing current revision instead
-
+代わりに現在のリビジョンを表示しています
 Showing revision %s
-
+リビジョン %s を表示しています
 Cannot save a nameless page.
-
+名前のないページは保存できません。
 Cannot save a page without revision.
-
+リビジョンのないページは保存できません。
 Cannot open %s
-
+%s を開けません
 Cannot write %s
-
-Cannot create %s
-
+%s を書き込めません
 Could not get %s lock
-
+%s ロックを取得できません
 The lock was created %s.
-
+ロックは %s に作られました。
+Maybe the user running this script is no longer allowed to remove the lock directory?
+このスクリプトを実行しているユーザはもはやロックディレクトリを除去することが許可されていないかもしれません？
 This operation may take several seconds...
-
+この処理は何秒かかかります...
 Forced unlock of %s lock.
-
+%s ロックを強制的に解除しました。
 No unlock required.
-
+ロック解除は必要ありません。
 %s hours ago
-
+%s 時間前
 1 hour ago
-
+1 時間前
 %s minutes ago
-
+%s 分前
 1 minute ago
-
+1 分前
 %s seconds ago
-
+%s 秒前
 1 second ago
-
+1 秒前
 just now
-
+たった今
 Edit Denied
-
+編集拒否
 Editing not allowed: user, ip, or network is blocked.
-
+編集は許可されません: ユーザ、IP、またはネットワークがブロックされています。
 Contact the wiki administrator for more information.
-
+詳しくはウィキ管理者に問い合わせてください。
 The rule %s matched for you.
-
+規則 %s があなたに当てはまりました。
 See %s for more information.
-
+詳しくは %s を見てください。
 Editing not allowed: %s is read-only.
-
+編集は許可されません: %s は閲覧のみです。
 Only administrators can upload files.
-
+管理者だけがファイルをアップロードできます。
 Editing revision %s of
-
+編集中: リビジョン %s の
 Editing %s
-
+%s を編集中
 Editing old revision %s.
-
+古いリビジョン %s を編集中。
 Saving this page will replace the latest revision with this text.
-
-Summary:
-
+このページを保存すると最新のリビジョンをこのテキストに入れ替えます。
 This change is a minor edit.
-
+この変更はマイナーな編集です。
 Cancel
-
+中止
 Replace this file with text
-
+このファイルをテキストと入れ替える
 Replace this text with a file
-
+このテキストをファイルと入れ替える
 File to upload: 
-
+アップロードするファイル: 
 Files of type %s are not allowed.
-
+形式 %s のファイルは許可されません。
 Your password is saved in a cookie, if you have cookies enabled. Cookies may get lost if you connect from another machine, from another account, or using another software.
-
+クッキーを有効にしていれば、パスワードはクッキーに保存されます。別のマシンから接続したり、別アカウントで接続したり、別のソフトウェアを使って接続すると、クッキーは失われることがあります。
 You are currently an administrator on this site.
-
+あなたは現在このサイトの管理者です。
 You are currently an editor on this site.
-
+あなたは現在このサイトの編集者です。
 You are a normal user on this site.
-
+あなたはこのサイトの通常ユーザです。
 Your password does not match any of the  administrator or editor passwords.
-
+あなたのパスワードは管理者パスワードにも編集者パスワードにも一致しません。
 Password:
-
+パスワード:
 This site does not use admin or editor passwords.
-
+このサイトは管理者パスワードも編集者パスワードも使っていません。
 This operation is restricted to site editors only...
-
+この操作はサイト編集者のみに制限されています...
 This operation is restricted to administrators only...
-
+この操作は管理者のみに制限されています...
 SampleUndefinedPage
 
 Sample_Undefined_Page
 
 Rule "%1" matched "%2" on this page.
-
+規則 "%1" がこのページの "%2" に当てはまりました。
 Reason: %s.
-
+理由: %s。
 Reason unknown.
-
+理由は不明です。
 Filter:
-
+フィルタ:
 (for %s)
-
+(%s に対して)
 %s pages found.
-
+%s ページ見付かりました。
+Malformed regular expression in %s
+%s に不正な正規表現があります
 Replaced: %s
-
+%s に入れ替えられました
 Search for: %s
-
+%s についての検索
 View changes for these pages
-
+これらのページの変更を見る
 last updated
-
+最終更新
 by
-
+投稿者
 Transfer Error: %s
-
+転送エラー: %s
 Browser reports no file info.
-
+ブラウザがファイル情報を報告しません。
 Browser reports no file type.
-
+ブラウザがファイル形式を報告しません。
 The page contains banned text.
-
+ページが禁止されたテキストを含んでいます。
 No changes to be saved.
-
+保存すべき変更がありません。
 This page was changed by somebody else %s.
-
+このページは %s 別な誰かによって変更されました。
 The changes conflict.  Please check the page again.
-
+変更が衝突します。ページをもう一度確認してください。
 Please check whether you overwrote those changes.
-
+これらの変更を上書きするかどうか確認してください。
 Anonymous
-
+匿名
 Cannot delete the index file %s.
-
+目録ファイル %s を削除できません。
 Please check the directory permissions.
-
+ディレクトリ許可属性を確認してください。
 Your changes were not saved.
-
+変更は保存されませんでした。
 Could not get a lock to merge!
-
+統合のためのロックを取得できませんでした！
 you
-
+あなた
 ancestor
-
+先祖
 other
-
+他者
 Run Maintenance
-
+保守作業の実行
 Maintenance not done.
-
+保守作業は行なわれません。
 (Maintenance can only be done once every 12 hours.)
-
+(保守作業は12時間に一度だけ行なわれます。)
 Remove the "maintain" file or wait.
-
+"maintain" ファイルを削除するか、待ってください。
 Expiring keep files and deleting pages marked for deletion
-
+期限切れの保持ファイルを破棄し、削除指定されたファイルを削除しています
 not deleted: 
-
+削除されませんでした: 
 deleted
-
+削除されました
 Moving part of the %s log file.
-
+%s の一部をログファイルに移動しています。
 Could not open %s log file
-
+%s ログファイルが開けません
 Error was
-
+エラーは次のようでした
 Note: This error is normal if no changes have been made.
-
+注意: 何も変更されていなければ、このエラーは普通です。
 Moving %s log entries.
-
+%s 個のログ項目を移動しています。
 Set or Remove global edit lock
-
+全体の編集ロックを設定または除去
 Edit lock created.
-
+編集ロックが作られました。
 Edit lock removed.
-
+編集ロックが除去されました。
 Set or Remove page edit lock
-
+ページ編集ロックを設定または除去
 Lock for %s created.
-
+%s のロックが作られました。
 Lock for %s removed.
-
+%s のロックが除去されました。
 Displaying Wiki Version
-
+ウィキバージョンの表示
 Debugging Information
-
-Inter links:
-
+デバッグ情報
 Too many connections by %s
-
+%s による多すぎる接続
 Please do not fetch more than %1 pages in %2 seconds.
-
+%2 秒間に %1 ページより多く取得しないでください。
 Check whether the web server can create the directory %s and whether it can create files in it.
-
-Copy one of the following stylesheets to %s:
-
-Deleting %s
-
-Deleted %s
-
-Renaming %1 to %2.
-
-The page %s does not exist
-
-The page %s already exists
-
-Cannot rename %1 to %2
-
-Renamed to %s
-
-Renamed from %s
-
-Renamed %1 to %2.
-
-Immediately delete %s
-
-Rename %s to:
-
-Learn more...
-
-Complete Content
-
-The main page is %s.
-
-Archive:
-
-Rebuild BackLink database
-
-Internal Page: 
-
-Pages that link to this page
-
-The search parameter is missing.
-
-Pages link to %s
-
-Cannot highlight the language %s.
-
-Recent Visitors
-
-some action
-
-was here
-
-and read
-
-Illegal year value: Use 0001-9999
-
-The match parameter is missing.
-
-Page Collection for %s
-
-Previous
-
-Next
-
-Calendar %s
-
-Su
-
-Mo
-
-Tu
-
-We
-
-Th
-
-Fr
-
-Sa
-
-January
-
-February
-
-March
-
-April
-
-May
-
-June
-
-July
-
-August
-
-September
-
-October
-
-November
-
-December
-
-set %s
-
-unset %s
-
-Clustermap
-
-Pages without a Cluster
-
-Comments on 
-
-Comment on 
-
-Compilation for %s
-
-Compilation tag is missing a regular expression.
-
-List spammed pages
-
-Despamming pages
-
-Spammed pages
-
-Cannot find revision %s.
-
-Revert to revision %1: %2
-
-Marked as %s.
-
-Cannot find unspammed revision.
-
-Recover Draft
-
-No text to save
-
-Draft saved
-
-Draft recovered
-
-No draft available to recover
-
-Save Draft
-
-Draft Cleanup
-
-%1 was last modified %2 and was kept
-
-%1 was last modified %2 and was deleted
-
-Unable to delete draft %s
-
-Add Comment
-
-ordinary changes
-
-Matching page names:
-
-Could not find %1.html template in %2
-
-Only Editors are allowed to see this hidden page.
-
-Only Admins are allowed to see this hidden page.
-
-Index
-
-Languages:
-
-Show!
-
-Define
-
-Full Link List
-
-List of locked pages
-
-Pages tagged with %s
-
-Template without parameters
-
-The template %s is either empty or does not exist.
-
- -- defined on %s
-
-Local names defined on %1: %2
-
-Locked Pages
-
-Register for %s
-
-Please choose a username of the form "FirstLast" using your real name.
-
-The passwords do not match.
-
-The password must be at least %s characters.
-
-That email address is invalid.
-
-The username %s has already been registered.
-
-Your registration for %s has been submitted.
-
-Please allow time for the webmaster to approve your request.
-
-An email has been sent to "%s" with further instructions.
-
-There was an error saving your registration.
-
-An account was created for %s.
-
-Login to %s
-
-Username and/or password are incorrect.
-
-Logged in as %s.
-
-Logout of %s
-
-Logout of %s?
-
-Logged out of %s
-
-You are now logged out.
-
-Register a new account
-
-Logout
-
-Who am I?
-
-Forgot your password?
-
-Change your password
-
-Approve pending registrations
-
-Confirm Registration for %s
-
-%s, your registration has been approved. You can now use your password to login and edit this wiki.
-
-Confirmation failed.  Please email %s for help.
-
-Who Am I?
-
-You are logged in as %s.
-
-You are not logged in.
-
-Reset Password
-
-The password for %s was reset.  It has been emailed to the address on file.
-
-There was an error resetting the password for %s.
-
-The username "%s" does not exist.
-
-Reset Password for %s
-
-Reset Password?
-
-Change Password for %s
-
-Change Password?
-
-Your current password is incorrect.
-
-Your password has been changed.
-
-Approve Pending Registrations for %s
-
-%s has been approved.
-
-There was an error approving %s.
-
-<ul>
-
-<li>%1 - %2</li>
-
-</ul>
-
-There are no pending registrations.
-
-Invalid Mail %s: not saved.
-
-unsubscribe
-
-subscribe
-
-Email: 
-
-%s appears to be an invalid mail address
-
-Your mail subscriptions
-
-All mail subscriptions
-
-Subscriptions
-
-Show
-
-Subscriptions for %s:
-
-Unsubscribe
-
-There are no subscriptions for %s.
-
-Change email address
-
-Mail addresses are linked to unsubscription links.
-
-Subscribe to %s.
-
-Subscribe
-
-Subscribed %s to the following pages:
-
-The remaining pages do not exist.
-
-Unsubscribed %s from the following pages:
-
-You linked more than %s times to the same domain. It would seem that only a spammer would do this. Your edit is refused.
-
-%s is not a legal name for a namespace
-
-Namespaces
-
-Getting page index file for %s.
-
-Near links:
-
-Search sites on the %s as well
-
-Fetching results from %s:
-
-Near pages:
-
-Include near pages
-
-EditNearLinks
-
-The same page on other sites:
-
- (create locally)
-
-image
-
-download
-
-Backlinks
-
-Clearing Cache
-
-Done.
-
-Generating Link Database
-
-The 404 handler extension requires the link data extension (links.pl).
-
-LocalMap
-
-No page id for action localmap
-
-Requested page %s does not exist
-
-Local Map for %s
-
-view
-
-Self-ban by %s
-
-You have banned your own IP.
-
-OpenID Login
-
-Your identity is saved in a cookie, if you have cookies enabled. Cookies may get lost if you connect from another machine, from another account, or using another software.
-
-Your homepage is set to %s.
-
-You have no homepage set.
-
-Homepage:
-
-Homepage is missing
-
-OpenID error %s
-
-Orphan List
-
-Trail: 
-
-None
-
-Type
-
-Permalink to "%s"
-
-anchor first defined here: %s
-
-the page %s also exists
-
-There was an error generating the pdf for %s.  Please report this to webmaster, but do not try to download again as it will not work.
-
-Someone else is generating a pdf for %s.  Please wait a minute and then try again.
-
-Download this page as PDF
-
-Click to search for references to this permanent anchor
-
-Include permanent anchors
-
-Portrait
-
-Publish %s
-
-No target wiki was specified in the config file.
-
-The target wiki was misconfigured.
-
-You did not answer correctly.
-
-To save this page you must answer this question:
-
-Please type the following two words:
-
-Please answer this captcha:
-
-Referrers
-
-All Referrers
-
-Tag
-
-Rebuild index for searching
-
-Tag Cloud
-
-Search::FreeText is not available on this system.
-
-Rebuilding index not done.
-
-(Rebuilding the index can only be done once every 12 hours.)
-
-New Pages for Indexed Search
-
-List changes since %s
-
- ... 
-
-Search term missing.
-
-Result pages: 
-
-(%s results)
-
-Tags:
-
-Tags: %s.
-
-No tags
-
-Page list for %s
-
-Slideshow:%s
-
-Index of all small pages
-
-Static Copy
-
-Back to %s
-
-Copy to %1 succeeded: %2.
-
-Copy to %1 failed: %2.
-
-Feed for this tag
-
-Rebuild tag index
-
-list tags
-
-tag cloud
-
-Alternatively, use one of the following templates:
-
-Thread: %s
-
-ID parameter is missing.
-
-Thread %s does not exist.
-
-Page %s does not contain a thread.
-
-Add
-
-URL parameter is missing.
-
-Add to %s thread
-
-Below:
-
-URL:
-
-Name:
-
-Too many instances.  Only %s allowed.
-
-Please try again later. Perhaps somebody is running maintenance or doing a long search. Unfortunately the site has limited resources, and so we must ask you for a bit of patience.
-
-Contents
-
-Create a new page for today
-
-Add Translation
-
-Added translation: %1 (%2)
-
-Translate %s
-
-Thank you for writing a translation of %s.
-
-Please indicate what language you will be using.
-
-Language is missing
-
-Suggested languages:
-
-Please indicate a page name for the translation of %s.
-
-More help may be available here: %s.
-
-Translated page: 
-
-This page is a translation of %s. 
-
-The translation is up to date.
-
-The translation is outdated.
-
-The page does not exist.
-
-http://search.barnesandnoble.com/booksearch/isbninquiry.asp?ISBN=%s
-
-http://www.amazon.com/exec/obidos/ISBN=%s
-
-alternate
-
-http://www.pricescan.com/books/BookDetail.asp?isbn=%s
-
-search
-
-Wanted Pages
-
-%s pages
-
-%s, referenced from:
-
-Upload of %s file
-
-Blog
-
-Matching pages:
-
-New
-
-Edit %s.
-
-Title: 
-
-Tags: 
-
+ウェブサーバが %s ディレクトリを作れるかどうかを、またウェブサーバがそのディレクトリの中にファイルを作れるかどうかを、確認してください。
 END_OF_TRANSLATION
