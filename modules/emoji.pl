1c630ad1fbd52389b565979591f77d643382e3c1
Alex Schroeder <alex@gnu.org>
2014-07-04 10:33:33 +0200

    emoji.pl: Added module description.

--- modules/emoji.pl
+++ modules/emoji.pl
@@ -12,6 +12,8 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 
+$ModulesDescription .= '<p><a href="http://git.savannah.gnu.org/cgit/oddmuse.git/tree/modules/emoji.pl">emoji.pl</a>, see <a href="http://www.oddmuse.org/cgi-bin/oddmuse/Logo_And_Icons#Smilies">Logo And Icons</a></p>';
+
 push(@MyRules, \&EmojiRule);
 # this can be last
 $RuleOrder{\&EmojiRule} = 500;
