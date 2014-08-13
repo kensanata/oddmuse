6cfe1ab29adc3c72ac467952fbe54fbeb05308fa
Alex-Daniel Jakimenko <alex.jakimenko@gmail.com>
2014-08-13 12:17:24 +0200

    Links With AccessKeys Extension
    Oddmuse provides several shortcuts, like ‘e’ to edit page or ‘c’ to open
    comments page and several others. In most browsers it means that
    pressing Alt+Shift+e should automatically click (or at least focus) edit
    link. However, by default there is no way to provide accesskey for links
    on your wiki pages. This module is trying to solve this.

---
+++ modules/accesskeys.pl
@@ -0,0 +1,36 @@
+# Copyright (C) 2014  Alex-Daniel Jakimenko <alex.jakimenko@gmail.com>
+# Copyright (C) 2014  Alex Schroeder <alex@gnu.org>
+
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+
+package OddMuse;
+
+$ModulesDescription .= '<p><a href="http://git.savannah.gnu.org/cgit/oddmuse.git/tree/modules/accesskeys.pl">accesskeys.pl</a>, see <a href="http://www.oddmuse.org/cgi-bin/oddmuse/Links_With_AccessKeys_Extension">Links With AccessKeys Extension</a></p>';
+
+push(@MyRules, \&LinksWithAccessKeys);
+sub LinksWithAccessKeys {
+  if (m/\G(\[\[$FreeLinkPattern\{(.)\}\]\])/cog) {
+    my ($id, $key) = ($2, $3);
+    Dirty($1);
+    $id = FreeToNormal($id);
+    my ($class, $resolved, $title, $exists) = ResolveId($id);
+    my $text = NormalToFree($id);
+    if ($resolved) { # anchors don't exist as pages, therefore do not use $exists
+      print ScriptLink(UrlEncode($resolved), $text, $class, undef, $title, $key);
+    } else {
+      print "[[" . QuoteHtml($text) . GetEditLink($id, '?') . "]]";
+    }
+    return ''; # this is a dirty rule that depends the definition of other pages
+  }
+  return undef; # the rule didn't match
+}
