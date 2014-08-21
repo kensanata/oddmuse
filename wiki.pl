04cdc08ee381fb22b0a3fc2e805430993e2df86d
Alex Schroeder <alex@gnu.org>
2014-08-21 04:35:53 +0300

    AddModuleDescripton()

--- wiki.pl
+++ wiki.pl
@@ -3930,5 +3930,10 @@ sub WriteRecentVisitors {
 
 sub TextIsFile { $_[0] =~ /^#FILE (\S+) ?(\S+)?\n/ }
 
+sub AddModuleDescription {
+  my ($filename, $name) = @_;
+  $ModulesDescription .= '<p><a href="http://git.savannah.gnu.org/cgit/oddmuse.git/tree/modules/' . UrlEncode($filename) . '">' . QuoteHtml($filename) . '</a>, see <a href="http://www.oddmuse.org/cgi-bin/oddmuse/' . UrlEncode($name) . '">' . QuoteHtml($name) . '</a></p>';
+}
+
 DoWikiRequest() if $RunCGI and not exists $ENV{MOD_PERL}; # Do everything.
 1; # In case we are loaded from elsewhere
