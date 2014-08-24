466348816649648aa6fd935393f749e470c8080c
Alex Schroeder <alex@gnu.org>
2014-08-24 22:20:50 +0200

    Makefile: more fixing for prepare target
    The prepare target didn't handle wiki.pl itself correctly. This has been
    fixed. The command to add the git tag was changed from sed to perl, like
    for the other targets. As emoji.pl and smiles.pl refer to permanent
    anchors, the addition of the anchor parameter to AddModuleDescription
    was obviously misguided and I reverted it.

--- modules/smiles.pl
+++ modules/smiles.pl
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 
-AddModuleDescription('smiles.pl', 'Logo And Icons', 'Smilies');
+AddModuleDescription('smiles.pl', 'Smilies');
 
 # The smilies are from the Emacs 24 distribution. There, you'll find
 # them in the etc/images/smilies/medium directory.
