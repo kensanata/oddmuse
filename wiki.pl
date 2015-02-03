a19453cb8e62d6cb14dcdc648b55394e8e73da4e
Alex Schroeder <alex@gnu.org>
2015-02-03 10:50:49 +0100

    Submit empty comment no longer wipes comment page.

--- wiki.pl
+++ wiki.pl
@@ -3530,7 +3530,7 @@ sub DoPost {
   $comment =~ s/(\r|$FS)//go;
   if (defined $comment and $comment eq '') {
     ReleaseLock();
-    ReBrowsePage($id);
+    return ReBrowsePage($id);
   }
   if ($filename) {		# upload file
     my $file = $q->upload('file');
