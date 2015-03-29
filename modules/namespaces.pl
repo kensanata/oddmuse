0aa41c8f06331ddfcf5bd1a2ad4f4d3bbb90e472
Alex Schroeder <alex@gnu.org>
2015-03-29 23:15:35 +0200

    namespaces.pl uses strict
    Apparently whatever prevented this from working has been fixed in the
    mean time.

--- modules/namespaces.pl
+++ modules/namespaces.pl
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 
-#use strict; #TODO $Monolithic is 'my'
+use strict;
 
 =head1 Namespaces Extension
 
