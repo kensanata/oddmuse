85e3eaaccb9dd100ed171a4d68598be336335bc5
Alex Schroeder <alex@gnu.org>
2015-03-29 17:50:51 +0200

    strange-spam.pl uses strict

--- modules/strange-spam.pl
+++ modules/strange-spam.pl
@@ -1,22 +1,18 @@
-# Copyright (C) 2006, 2007  Alex Schroeder <alex@emacswiki.org>
+# Copyright (C) 2006–2015  Alex Schroeder <alex@gnu.org>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-#    Free Software Foundation, Inc.
-#    59 Temple Place, Suite 330
-#    Boston, MA 02111-1307 USA
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
 
-# use strict; # TODO again that weird thing
+use strict;
 
 AddModuleDescription('strange-spam.pl', 'StrangeBannedContent');
 
@@ -25,8 +21,15 @@ use vars qw($StrangeBannedContent);
 
 $StrangeBannedContent = 'StrangeBannedContent';
 
+my $StrangeOldBannedContent; # use strict
+
 *StrangeOldBannedContent = *BannedContent;
 *BannedContent = *StrangeNewBannedContent;
+
+# The code above changes both the sub and the variable. $BannedContent now points to $StrangeNewBannedContent (which is
+# undefined) and the name of the Banned Content page is only accessible via $StrangeOldBannedContent. If we copy
+# $StrangeOldBannedContent to $BannedContent, everything else will keep working.
+
 $BannedContent = $StrangeOldBannedContent;
 
 push(@MyInitVariables, \&StrangeBannedContentInit);
