1c32e0d45c0c2024154cd50b21f481304252c786
Alex Schroeder <alex@gnu.org>
2015-03-29 14:56:52 +0200

    latex.pl uses strict
    This should make sure that we do in fact run the cleanup code even if
    an error occured. Also, whitespace changes because it was a pain to
    read.

--- modules/latex.pl
+++ modules/latex.pl
@@ -1,22 +1,18 @@
-# Copyright (C) 2003, 2004  Alex Schroeder <alex@emacswiki.org>
+# Copyright (C) 2003–2015  Alex Schroeder <alex@gnu.org>
 # Copyright (C) 2004  Haixing Hu <huhaixing@msn.com>
 # Copyright (C) 2004, 2005 Todd Neal <tolchz@tolchz.net>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-#    Free Software Foundation, Inc.
-#    59 Temple Place, Suite 330
-#    Boston, MA 02111-1307 USA
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
 #
 # External programs needed
 # LaTeX   - http://www.latex-project.org
@@ -32,7 +28,7 @@
 # img.InlineMath
 # img.DisplayMath
 
-# use strict; # TODO some vars are out of scope, but there is no way to see the scope because of the broken indentation
+use strict;
 
 use File::Glob ':glob';
 
@@ -119,10 +115,10 @@ sub LatexRule {
     my $label = $1;
     $label =~ tr/A-Z/a-z/;
     if ($eqHash{$label}) {
-	return $eqAbbrev . "<a href=\"#$label\">". $eqHash{$label} . "</a>";
+      return $eqAbbrev . "<a href=\"#$label\">". $eqHash{$label} . "</a>";
     }
     else {
-	return "[ Equation $label not found ]";
+      return "[ Equation $label not found ]";
     }
   }
   return;
@@ -135,12 +131,12 @@ sub MakeLaTeX {
   # Select which binary to use for conversion of dvi to images
   my $useConvert = 0;
   if (not -e $dvipngPath) {
-      if (not -e $convertPath) {
-	  return "[Error: dvipng binary and convert binary not found at $dvipngPath or $convertPath ]";
-      }
-      else {
-	  $useConvert = 1; # Fall back on convert if dvipng is missing and convert exists
-      }
+    if (not -e $convertPath) {
+      return "[Error: dvipng binary and convert binary not found at $dvipngPath or $convertPath ]";
+    }
+    else {
+      $useConvert = 1; # Fall back on convert if dvipng is missing and convert exists
+    }
   }
 
   $latex = UnquoteHtml($latex); # Change &lt; back to <, for example
@@ -150,74 +146,81 @@ sub MakeLaTeX {
   my $hasMD5;
   if ($useMD5)  { $hasMD5 = eval "require Digest::MD5;"; }
   if ($useMD5 && $hasMD5) {
-      $hash = Digest::MD5::md5_base64($latex);
-      $hash =~ s/\//a/g;
+    $hash = Digest::MD5::md5_base64($latex);
+    $hash =~ s/\//a/g;
   }  else {
-      $hash = UrlEncode($latex);
-      $hash =~ s/%//g;
+    $hash = UrlEncode($latex);
+    $hash =~ s/%//g;
   }
 
   # check cache
   if (not -f "$LatexDir/$hash.png" or -z "$LatexDir/$hash.png") { #If file doesn't exist or is zero bytes
-      # Then create the image
+    # Then create the image
 
-      # read template and replace <math>
-      mkdir($LatexDir) unless -d $LatexDir;
-      if (not -f $LatexDefaultTemplateName) {
-	  open (F, "> $LatexDefaultTemplateName") or return '[Unable to write template]';
-	  print F $LatexDefaultTemplate;
-	  close (F);
-      }
-      my $template = ReadFileOrDie($LatexDefaultTemplateName);
-      $template =~ s/<math>/$latex/ig;
-      #setup rendering directory
-      my $dir = "$LatexDir/$hash";
-      if (-d $dir) {
-	  unlink (bsd_glob('$dir/*'));
-      } else {
-	  mkdir($dir) or return "[Unable to create $dir]";
-      }
-      chdir ($dir) or return "[Unable to switch to $dir]";
-      WriteStringToFile ("srender.tex", $template);
-      my $errorText = qx(latex srender.tex);
-      return "[Illegal LaTeX markup: <pre>$latex</pre>] <br/> Error: <pre>$errorText</pre>" if ($? && $dispErrors);
-      return "[Illegal LaTeX markup: <pre>$latex</pre>] <br/>" if $?;
+    # read template and replace <math>
+    mkdir($LatexDir) unless -d $LatexDir;
+    if (not -f $LatexDefaultTemplateName) {
+      open (F, "> $LatexDefaultTemplateName") or return '[Unable to write template]';
+      print F $LatexDefaultTemplate;
+      close (F);
+    }
+    my $template = ReadFileOrDie($LatexDefaultTemplateName);
+    $template =~ s/<math>/$latex/ig;
+    #setup rendering directory
+    my $dir = "$LatexDir/$hash";
+    if (-d $dir) {
+      unlink (bsd_glob('$dir/*'));
+    } else {
+      mkdir($dir) or return "[Unable to create $dir]";
+    }
+    chdir ($dir) or return "[Unable to switch to $dir]";
+    WriteStringToFile ("srender.tex", $template);
+    my $errorText = qx(latex srender.tex);
 
+    # We should not return until we're ready to clean up!
+    my $error = '';
+
+    if ($? && $dispErrors) {
+      $error = "[Illegal LaTeX markup: <pre>$latex</pre>] <br/> Error: <pre>$errorText</pre>";
+    } elsif ($?) {
+      $error = "[Illegal LaTeX markup: <pre>$latex</pre>] <br/>";
+    } else {
       my $output;
 
       # Use specified binary to convert dvi to png
       if ($useConvert) {
-	  $output = qx($convertPath -antialias -crop 0x0 -density 120x120 -transparent white srender.dvi srender1.png );
-	  return "[convert error $? ($output)]" if $?;
+	$output = qx($convertPath -antialias -crop 0x0 -density 120x120 -transparent white srender.dvi srender1.png );
+	$error = "[convert error $? ($output)]" if $?;
       } else {
-	  $output = qx($dvipngPath -T tight -bg Transparent srender.dvi);
-	  return "[dvipng error $? ($output)]" if $?;
+	$output = qx($dvipngPath -T tight -bg Transparent srender.dvi);
+	$error = "[dvipng error $? ($output)]" if $?;
       }
+      
+      if (not $error and -f 'srender1.png' and not -z 'srender1.png') {
+	my $png = ReadFileOrDie("srender1.png");
+	WriteStringToFile ("$LatexDir/$hash.png", $png);
+      } else {
+	$error = "[Error retrieving image for $latex]";
+      }
+    }
+    unlink (glob('*'));
+    chdir ($LatexDir);
+    rmdir ($dir);
 
-      my $result;
-      if (-f 'srender1.png' and not -z 'srender1.png') {
-	  my $png = ReadFileOrDie("srender1.png");
-	  WriteStringToFile ("$LatexDir/$hash.png", $png);
-      } else {	  $result = "[Error retrieving image for $latex]"; }
-
+    return $error if $error;
   }
-    # Finally print the html for the image
-    if ($type eq "inline math") { # inline math
-      return ("<img class='InlineMath' "
-              ."src='$LatexLinkDir/$hash.png' alt='$latex'\/>");
-    } elsif ($type eq "display math") { # display math
-	my $ret;
-	if ($label) { $ret = "<a name='$label'>"; }
-	$ret .= "<center><img class='DisplayMath' "
-             ."src='$LatexLinkDir/$hash.png' alt='$latex'> <span class=\'eqCount\'>($eqCounter)</span><\/center>";
-	return($ret);
-    } else {  # latex format
-      return ("<img class='LaTeX' "
-             ."src='$LatexLinkDir/$hash.png' alt='$latex' \/>");
-	   }
-
-  unlink (glob('*'));
-  chdir ($LatexDir);
-  rmdir ($dir);
-  return $result;
+  # Finally print the html for the image
+  if ($type eq "inline math") { # inline math
+    return ("<img class='InlineMath' "
+	    ."src='$LatexLinkDir/$hash.png' alt='$latex'\/>");
+  } elsif ($type eq "display math") { # display math
+    my $ret;
+    if ($label) { $ret = "<a name='$label'>"; }
+    $ret .= "<center><img class='DisplayMath' "
+	."src='$LatexLinkDir/$hash.png' alt='$latex'> <span class=\'eqCount\'>($eqCounter)</span><\/center>";
+    return($ret);
+  } else {  # latex format
+    return ("<img class='LaTeX' "
+	    ."src='$LatexLinkDir/$hash.png' alt='$latex' \/>");
+  }
 }
