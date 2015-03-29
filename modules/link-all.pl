60561397aa426b281d02462d92127231d4731e6f
Alex Schroeder <alex@gnu.org>
2015-03-29 15:07:37 +0200

    link-all uses strict
    Get rid of $IndexInit from UseMod days.

--- modules/link-all.pl
+++ modules/link-all.pl
@@ -1,22 +1,18 @@
-# Copyright (C) 2004  Alex Schroeder <alex@emacswiki.org>
+# Copyright (C) 2004–2015  Alex Schroeder <alex@gnu.org>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-#    Free Software Foundation, Inc.
-#    59 Temple Place, Suite 330
-#    Boston, MA 02111-1307 USA
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
 
-# use strict; #TODO $IndexInit
+use strict;
 
 AddModuleDescription('link-all.pl', 'Link All Words Extension');
 
@@ -40,7 +36,7 @@ sub LinkAllRule {
 
 sub LinkAllGetPageLinkIfItExists {
   my $id = shift;
-  AllPagesList() unless $IndexInit;
+  AllPagesList();
   if ($IndexHash{$id}) {
     return GetPageLink($id);
   } elsif (GetParam('define', 0)) {
