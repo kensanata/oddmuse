432ad81993c2cab95260cfc0b8cbeeb9b295dd9f
Alex Schroeder <alex@gnu.org>
2015-03-29 23:44:34 +0200

    sistersites.pl uses strict
    Like in index.pl, we need to assign $match from the parameter, first.

--- modules/sistersites.pl
+++ modules/sistersites.pl
@@ -1,22 +1,18 @@
-# Copyright (C) 2007  Alex Schroeder <alex@emacswiki.org>
+# Copyright (C) 2007–2014  Alex Schroeder <alex@gnu.org>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-#    Free Software Foundation, Inc.
-#    59 Temple Place, Suite 330
-#    Boston, MA 02111-1307 USA
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
 
-# use strict; #TODO what is $match? (42)
+use strict;
 
 AddModuleDescription('sistersites.pl', 'Sister Pages');
 
@@ -39,6 +35,7 @@ sub SisterPages {
   push(@pages, AllPagesList()) if GetParam('pages', 1);
   push(@pages, keys %PermanentAnchors) if GetParam('permanentanchors', 1);
   push(@pages, keys %NearSource) if GetParam('near', 0);
-  @pages = grep /$match/i, @pages if GetParam('match', '');
+  my $match = GetParam('match', '');
+  @pages = grep /$match/i, @pages if $match;
   return sort @pages;
 }
