848510959773c20bcd756c455dc7fa4e24974aa9
Alex Schroeder <alex@gnu.org>
2015-04-02 10:46:54 +0200

    pygmentize.pl: add our $TempDir

--- modules/pygmentize.pl
+++ modules/pygmentize.pl
@@ -21,7 +21,7 @@ package OddMuse;
 
 AddModuleDescription('pygmentize.pl', 'Pygmentize Extension');
 
-our ($q, $bol, %RuleOrder, @MyRules);
+our ($q, $bol, %RuleOrder, @MyRules, $TempDir);
 
 # You can push other stuff to that list.
 # For example: push @PygmentizeArgs, qw(-F whitespace:spaces=true,tabs=true)
