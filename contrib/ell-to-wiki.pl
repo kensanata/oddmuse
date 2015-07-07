295d64e34f1db773ddb8140c45cabb33b0f8f553
Alex Schroeder <alex@gnu.org>
2015-07-07 12:46:26 +0200

    ell-to-wiki.pl: use GPLv3 and gnu.org email

--- contrib/ell-to-wiki.pl
+++ contrib/ell-to-wiki.pl
@@ -1,26 +1,20 @@
 #!/usr/bin/perl
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2004  Alex Schroeder <alex@emacswiki.org>
+# Copyright (C) 2004  Alex Schroeder <alex@gnu.org>
 # Copyright (C) 2007  Vinicius José Latorre <viniciusjl at ig.com.br>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-#    Free Software Foundation, Inc.
-#    59 Temple Place, Suite 330
-#    Boston, MA 02111-1307 USA
-
-# $Id: ell-to-wiki.pl,v 1.5 2007/02/15 22:00:26 as Exp $
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
 
 use LWP::UserAgent;
 use XML::Parser;
