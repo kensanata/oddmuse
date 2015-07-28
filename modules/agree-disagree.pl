c269d0c303816b3d5657220c0595221ed405df57
Alex Schroeder <alex@gnu.org>
2015-07-28 21:58:15 +0200

    agree-disagree.pl: fix issues

--- modules/agree-disagree.pl
+++ modules/agree-disagree.pl
@@ -16,7 +16,12 @@
 #    59 Temple Place, Suite 330
 #    Boston, MA 02111-1307 USA
 
-$ModulesDescription .= '<p>$Id: agreeDisagree.pl,v 1.23 2005/6/13 $</p>';
+use strict;
+use v5.10;
+
+our ($Now, @MyMacros, @MyRules, $DefaultStyleSheet, $q, $bol);
+
+    AddModuleDescription('agree-disagree.pl', 'AgreeDisagreePlugin');
 
 push(@MyRules, \&AgreeDisagreeSupportRule);
 
@@ -29,36 +34,36 @@ push(@MyMacros, sub{ s/\[\-(:[^]:]+)\]/"[-$1:" . TimeToText($Now) . "]"/ge });
 
 
 $DefaultStyleSheet .= <<'EOT' unless $DefaultStyleSheet =~ /div\.agree/; # mod_perl?
-div.agreeCount {
-        float: left;
-        clear: left;
-        background-color: Green;
-        padding-left: .5em;
-        padding-right: .5em;
-        padding-top: .5em;
-        padding-bottom: .5em;
+    div.agreeCount {
+ float: left;
+ clear: left;
+background-color: Green;
+padding-left: .5em;
+padding-right: .5em;
+padding-top: .5em;
+padding-bottom: .5em;
 }
 div.disagreeCount {
-        float: left;
-        clear: right;
-        background-color: Red;
-        padding-left: .5em;
-        padding-right: .5em;
-        padding-top: .5em;
-        padding-bottom: .5em;
+  float: left;
+ clear: right;
+background-color: Red;
+padding-left: .5em;
+padding-right: .5em;
+padding-top: .5em;
+padding-bottom: .5em;
 }
 
 div.agreeNames {
-        float: left;
-        background-color: Green;
-        font-size: xx-small;
-        display: none;
+  float: left;
+background-color: Green;
+font-size: xx-small;
+ display: none;
 }
 div.disagreeNames {
-        float: left;
-        background-color: Red;
-        font-size: xx-small;
-        display: none;
+  float: left;
+background-color: Red;
+font-size: xx-small;
+ display: none;
 }
 
 
@@ -68,35 +73,35 @@ EOT
 
 
 
-my %AgreePortraits = ();
+    my %AgreePortraits = ();
 
 
 sub AgreeDisagreeSupportRule {
   if ($bol) {
     if ($bol && m/(\G(\s*\[\+(.*?)\]|\s*\[-(.*?)\])+)/gcs) {
 
-	$votes = $1;
-	@ayes = ();
-	@nayes = ();
-	while ($votes =~ m/\G.*?\[\+(.*?)\]/gcs) {
-	    my ($ignore, $name, $time) = split(/:/, $1, 3);
-	    push(@ayes, $name);
-	}
-	$votes2 = $votes;
-	while ($votes2 =~ m/\G.*?\[-(.*?)\]/gcs) {
-	    my ($ignore, $name, $time) = split(/:/, $1, 3);
-	    push(@nayes, $name);
-	}
+      my $votes = $1;
+      my @ayes = ();
+      my @nayes = ();
+      while ($votes =~ m/\G.*?\[\+(.*?)\]/gcs) {
+        my ($ignore, $name, $time) = split(/:/, $1, 3);
+        push(@ayes, $name);
+      }
+      my $votes2 = $votes;
+      while ($votes2 =~ m/\G.*?\[-(.*?)\]/gcs) {
+        my ($ignore, $name, $time) = split(/:/, $1, 3);
+        push(@nayes, $name);
+      }
+      
+      my $html = CloseHtmlEnvironments() ;
+      $html .= $q->div({-class=>'agreeCount'}) . ($#ayes+1) . '  ' . '</div>' ;
 
-	$html = CloseHtmlEnvironments() ;
-	$html .= $q->div({-class=>'agreeCount'}) . ($#ayes+1) . '  ' . '</div>' ;
-
-	$html .= $q->div({-class=>'agreeNames'}) . printNames(@ayes) . '</div>' ;
-	$html .= $q->div({-class=>'disagreeCount'}) . '  ' . ($#nayes+1) . '</div>' ;
-	$html .= $q->div({-class=>'disagreeNames'}) . printNames(@nayes) . '</div>' ;
+      $html .= $q->div({-class=>'agreeNames'}) . printNames(@ayes) . '</div>' ;
+      $html .= $q->div({-class=>'disagreeCount'}) . '  ' . ($#nayes+1) . '</div>' ;
+      $html .= $q->div({-class=>'disagreeNames'}) . printNames(@nayes) . '</div>' ;
 
 
-	return $html;
+      return $html;
     }
   }
   return undef;
@@ -104,11 +109,11 @@ sub AgreeDisagreeSupportRule {
 
 
 sub printNames {
-    @names = @_;
-
-    my $html = '';
-    foreach $name (@names)  {
-	$html .= "$name<br>";
-    }
-    return $html;
+  my @names = @_;
+  
+  my $html = '';
+  foreach my $name (@names)  {
+    $html .= "$name<br>";
+  }
+  return $html;
 }
