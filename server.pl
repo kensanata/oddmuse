ad299b6b1d7c458038f7806d03ec810a2d751799
Alex Schroeder <alex@gnu.org>
2015-10-11 19:59:04 +0200

    server.pl: instructions for the plugin to use
    I've submitted some patches for Mojolicious::Plugin::CGI and the
    comments now point to a fork.

--- server.pl
+++ server.pl
@@ -1,5 +1,14 @@
 #! /usr/bin/env perl
 
+# This script only works with a version of Mojolicious::Plugin::CGI better than
+# the official 0.23. One version would be my fork:
+# https://github.com/kensanata/mojolicious-plugin-cgi
+
+# If you use the fork, you might want to simply add its lib directory to your
+# libraries instead of installing it?
+
+# use lib '/Users/alex/src/mojolicious-plugin-cgi/lib';
+
 use Mojolicious::Lite;
 
 plugin CGI => {
@@ -9,6 +18,12 @@ plugin CGI => {
 plugin CGI => {
   route => '/wiki',
   script => 'wiki.pl',
+  run => \&OddMuse::DoWikiRequest,
+  before => sub {
+    $OddMuse::RunCGI = 0;
+    $OddMuse::DataDir = '/tmp/oddmuse';
+    require 'wiki.pl' unless defined &OddMuse::DoWikiRequest;
+  },
   env => {},
   errlog => 'wiki.log', # path to where STDERR from cgi script goes
 };
