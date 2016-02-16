0effc866205c77cb9c0b78e9e685f9db9f81fd7a
Alex Schroeder <alex@gnu.org>
2016-02-16 16:42:37 +0100

    added delete.sh
    I've been using this to clear out wikis on campaignwiki.org

---
+++ contrib/campaignwiki/delete.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if test -z "$2" -o ! -z "$3"; then
+    echo "Usage: delete.sh USERNAME WIKI"
+    exit 1
+fi
+
+username=$1
+wiki=$2
+
+for p in $(curl "https://campaignwiki.org/wiki/$wiki?action=index;raw=1"); do
+    echo "Deleting: $p"
+    curl -F frodo=1 -F "title=$p" -F text=DeletedPage -F summary=Deleted -F username="$username" "https://campaignwiki.org/wiki/$1"
+    sleep 5
+done
