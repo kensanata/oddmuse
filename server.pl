677d60f691e7d8260d03e939ef2a5915a862aa63
Alex Schroeder <alex@gnu.org>
2016-06-12 21:25:28 +0200

    Simplify Mojolicious server setup
    Make sure the wiki log is written into the data directory.

--- server.pl
+++ server.pl
@@ -14,22 +14,22 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 
+package OddMuse;
+
+our $RunCGI = 0;
+do 'wiki.pl';
+
+my $dir = $DataDir; # used for Mojolicious::Plugin::CGI setup
+
 use Mojolicious::Lite;
 
 plugin CGI => {
   support_semicolon_in_query_string => 1,
   route => '/wiki',
-  script => 'wiki.pl',
   run => \&OddMuse::DoWikiRequest,
-  before => sub {
-    no warnings;
-    $OddMuse::RunCGI = 0;
-    # $OddMuse::DataDir = '/tmp/oddmuse';
-    use warnings;
-    require 'wiki.pl' unless defined &OddMuse::DoWikiRequest;
-  },
   env => {},
-  errlog => 'wiki.log', # path to where STDERR from cgi script goes
+  # path to where STDERR from cgi script goes
+  errlog => "$dir/wiki.log",
 };
 
 get '/' => sub {
