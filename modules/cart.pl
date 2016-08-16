d2db3d849e3e904a4fe7cd8075a2e08985d8c37c
Alex Schroeder <alex@gnu.org>
2016-08-16 16:43:21 +0200

    Fixed erroneous edits from previous commit
    The keyboard macro did not deal kindly with two files... This should
    address comments by @m4z for the incorrect FSF address issue #4 on
    GitHub.

--- modules/cart.pl
+++ modules/cart.pl
@@ -13,6 +13,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+use strict;
+use v5.10;
+use utf8;
+
 AddModuleDescription('cart.pl', 'Cart Extension');
 
 our ($q, %Action, $UserGotoBar, $CookieName);
