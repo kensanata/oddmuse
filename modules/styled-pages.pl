a8cd060bf97ac1ff91d9e2950e2e224256408715
Alex Schroeder <alex@gnu.org>
2017-04-15 03:32:01 +0300

    New module: styled-pages.pl
    A user on #oddmuse asked what would be the easiest way to have
    different css styles on different pages. Turns out the easiest way is
    to write a module.
    
    Not sure if it will break html in some yet unknown edge cases, but for
    most things this should work just fine.
    
    greenfive++

---
+++ modules/styled-pages.pl
@@ -0,0 +1,42 @@
+# Copyright (C) 2014-2017
+#     Aleks-Daniel Jakimenko-Aleksejev <alex.jakimenko@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+
+use strict;
+use v5.10;
+
+AddModuleDescription('styled-pages.pl', 'Styled Pages Extension');
+
+our (@MyRules);
+our ($StyledPagesPrefix);
+our (%RuleOrder);
+
+$StyledPagesPrefix = 'style_';
+
+push(@MyRules, \&StyledPagesRule);
+
+my $StyledPage = '';
+
+sub StyledPagesRule {
+  if (!$StyledPage and m/\G (^|\n)? \#STYLE [ \t]+ ([a-z_-][a-z0-9 _-]+[a-z0-9_-]) \s*(\n+|$) /cgx) {
+    $StyledPage = 1;
+    return CloseHtmlEnvironments()
+        . '<div class="' . join(' ', map {"$StyledPagesPrefix$_"} split /\s+/, $2) . '">'
+        . AddHtmlEnvironment('p');
+  }
+  if ($StyledPage and m/\G $/cgx) {
+      return CloseHtmlEnvironments() . '</div>';
+  }
+  return;
+}
