b42b9f0cc8fb196678ecf09f0696f03a32d5d3c6
Ralph Graulich <maillist@shauny.de>
2017-04-21 00:02:48 +0200

    refactored into single outer if-branch

--- modules/search-list.pl
+++ modules/search-list.pl
@@ -2,7 +2,7 @@
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
+# the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -11,32 +11,44 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
 
 use strict;
 use v5.10;
 
-AddModuleDescription('search-list.pl', 'Search List Extension');
+AddModuleDescription('search-list.pl', 'Search List Extension', undef, '2.3.7');
 
 our ($q, $bol, %Action, %Page, $OpenPageName, @MyRules);
 
 push(@MyRules, \&SearchListRule);
 
 sub SearchListRule {
-  if ($bol && /\G(&lt;list (.*?)&gt;)/cgis) {
-    # <list regexp> (search page titles and pages bodies)
+  if ($bol && /\G(&lt;(list|titlelist) (.*?)&gt;)/cgis) {
+    # <list regexp> (search page titles and page bodies)
+    # <titlelist regexp> (search page titles only)
     Clean(CloseHtmlEnvironments());
     Dirty($1);
     my ($oldpos, $old_) = (pos, $_);
     my $original = $OpenPageName;
-    my $term = $2;
+    my $variation = $2;
+    my $term = $3;
     if ($term eq "") {
         $term = GetId();
     }
     local ($OpenPageName, %Page);
     my %hash = ();
-    foreach my $id (SearchTitleAndBody($term)) {
-      $hash{$id} = 1 unless $id eq $original; # skip the page with the query
+    if ($variation eq 'list') {
+      foreach my $id (SearchTitleAndBody($term)) {
+        $hash{$id} = 1 unless $id eq $original; # skip the page with the query
+      }
+    }
+    if ($variation eq 'titlelist') {
+      foreach my $id (grep(/$term/, AllPagesList())) {
+        $hash{$id} = 1 unless $id eq $original; # skip the page with the query
+        }
     }
     my @found = keys %hash;
     if (defined &PageSort) {
@@ -45,36 +57,13 @@ sub SearchListRule {
       @found = sort(@found);
     }
     @found = map { $q->li(GetPageLink($_)) } @found;
-    print $q->start_div({-class=>'search list'}),
-      $q->ul(@found), $q->end_div;
-    Clean(AddHtmlEnvironment('p')); # if dirty block is looked at later, this will disappear
-    ($_, pos) = ($old_, $oldpos); # restore \G (assignment order matters!)
-    return '';
-  }
-  if ($bol && /\G(&lt;titlelist (.*?)&gt;)/cgis) {
-    # <titlelist regexp> (search page titles)
-    Clean(CloseHtmlEnvironments());
-    Dirty($1);
-    my ($oldpos, $old_) = (pos, $_);
-    my $original = $OpenPageName;
-    my $term = $2;
-    if ($term eq "") {
-        $term = GetId();
+    if ($variation eq 'list') {
+      print $q->start_div({-class=>'search list'});
     }
-    local ($OpenPageName, %Page);
-    my %hash = ();
-    foreach my $id (grep(/$term/, AllPagesList())) {
-      $hash{$id} = 1 unless $id eq $original; # skip the page with the query
+    if ($variation eq 'titlelist') {
+      print $q->start_div({-class=>'search titlelist'});
     }
-    my @found = keys %hash;
-    if (defined &PageSort) {
-      @found = sort PageSort @found;
-    } else {
-      @found = sort(@found);
-    }
-    @found = map { $q->li(GetPageLink($_)) } @found;
-    print $q->start_div({-class=>'search titlelist'}),
-      $q->ul(@found), $q->end_div;
+    print $q->ul(@found), $q->end_div;
     Clean(AddHtmlEnvironment('p')); # if dirty block is looked at later, this will disappear
     ($_, pos) = ($old_, $oldpos); # restore \G (assignment order matters!)
     return '';
@@ -111,3 +100,4 @@ my $search = GetParam('search', '');
       $q->ul(@found), $q->end_div;
   PrintFooter();
 }
+
