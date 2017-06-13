4dd29bd4a7b0104ca06e8b742baeb62c33600ad5
Alex Schroeder <alex@gnu.org>
2017-06-13 22:06:39 +0200

    paste-image: automatic resizing via Javascript

--- modules/paste-image.pl
+++ modules/paste-image.pl
@@ -18,9 +18,17 @@ use v5.10;
 
 AddModuleDescription('paste-image.pl', 'Paste Files to Upload');
 
-our (@MyInitVariables, $ScriptName, $HtmlHeaders);
+our (@MyInitVariables, $ScriptName, $HtmlHeaders, $MaxPost, $CommentsPattern);
+
+our ($PageImageOnBrowse);
+
+$PageImageOnBrowse = 0;
 
 push(@MyInitVariables, \&PasteImageScript);
+
+# Resampling based on the following:
+# https://stackoverflow.com/a/19223362/534893
+# https://github.com/viliusle/Hermite-resize
 
 sub PasteImageScript {
   my $id = GetId();
@@ -28,7 +36,10 @@ sub PasteImageScript {
   OpenPage($id);
   my $username = GetParam('username', '');
   my $template = "Image_{n}_for_$id";
-  if ($HtmlHeaders !~ /PasteImage/) {
+  if ((GetParam('action', 'browse') eq 'edit'
+       or $CommentsPattern and $id =~ /$CommentsPattern/
+       or $PageImageOnBrowse and GetParam('action', 'browse') eq 'browse')
+      and $HtmlHeaders !~ /PasteImage/) {
     $HtmlHeaders .= << "EOT";
 <script type="text/javascript">
 if (!HTMLTextAreaElement.prototype.insertAtCaret) {
@@ -66,7 +77,11 @@ var PasteImage = {
           let blob = items[i].getAsFile();
           let reader = new window.FileReader();
           reader.onloadend = function() {
-            PasteImage.process(reader.result, "$template", 1);
+            let dataUrl = reader.result;
+            let n = 1;
+            while (n++ < 4 && $MaxPost > 0 && dataUrl.length > $MaxPost)
+              dataUrl = PasteImage.shrink(dataUrl);
+            PasteImage.process(dataUrl, "$template", 1);
           }
           reader.readAsDataURL(blob);
         }
@@ -74,9 +89,90 @@ var PasteImage = {
     }
   },
 
+  shrink: function(dataUrl) {
+    let image = new Image;
+    image.src = dataUrl;
+
+    let canvas = document.createElement("canvas");
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(image, 0, 0);
+
+    let width_source = canvas.width;
+    let height_source = canvas.height;
+    let width = Math.round(width_source * 0.8);
+    let height = Math.round(height_source * 0.8);
+
+    let ratio_w = width_source / width;
+    let ratio_h = height_source / height;
+    let ratio_w_half = Math.ceil(ratio_w / 2);
+    let ratio_h_half = Math.ceil(ratio_h / 2);
+
+    let img = ctx.getImageData(0, 0, width_source, height_source);
+    let img2 = ctx.createImageData(width, height);
+    let data = img.data;
+    let data2 = img2.data;
+
+    for (let j = 0; j < height; j++) {
+      for (let i = 0; i < width; i++) {
+        let x2 = (i + j * width) * 4;
+        let weight = 0;
+        let weights = 0;
+        let weights_alpha = 0;
+        let gx_r = 0;
+        let gx_g = 0;
+        let gx_b = 0;
+        let gx_a = 0;
+        let center_y = (j + 0.5) * ratio_h;
+        let yy_start = Math.floor(j * ratio_h);
+        let yy_stop = Math.ceil((j + 1) * ratio_h);
+        for (let yy = yy_start; yy < yy_stop; yy++) {
+          let dy = Math.abs(center_y - (yy + 0.5)) / ratio_h_half;
+          let center_x = (i + 0.5) * ratio_w;
+          let w0 = dy * dy; //pre-calc part of w
+          let xx_start = Math.floor(i * ratio_w);
+          let xx_stop = Math.ceil((i + 1) * ratio_w);
+          for (let xx = xx_start; xx < xx_stop; xx++) {
+            let dx = Math.abs(center_x - (xx + 0.5)) / ratio_w_half;
+            let w = Math.sqrt(w0 + dx * dx);
+            if (w >= 1) {
+              //pixel too far
+              continue;
+            }
+            //hermite filter
+            weight = 2 * w * w * w - 3 * w * w + 1;
+            let pos_x = 4 * (xx + yy * width_source);
+            //alpha
+            gx_a += weight * data[pos_x + 3];
+            weights_alpha += weight;
+            //colors
+            if (data[pos_x + 3] < 255)
+              weight = weight * data[pos_x + 3] / 250;
+            gx_r += weight * data[pos_x];
+            gx_g += weight * data[pos_x + 1];
+            gx_b += weight * data[pos_x + 2];
+            weights += weight;
+          }
+        }
+        data2[x2] = gx_r / weights;
+        data2[x2 + 1] = gx_g / weights;
+        data2[x2 + 2] = gx_b / weights;
+        data2[x2 + 3] = gx_a / weights_alpha;
+      }
+    }
+    canvas.width = width;
+    canvas.height = height;
+    ctx.putImageData(img2, 0, 0);
+    let png = canvas.toDataURL();
+    let jpg = canvas.toDataURL('image/jpeg');
+    return png <= jpg ? png : jpg;
+  },
+  
   process: function(dataUrl, template, n) {
     let name = template.replace('{n}', n);
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open("HEAD", "$ScriptName/" + name, true);
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4) {
@@ -95,7 +191,7 @@ var PasteImage = {
   },
 
   post: function(dataUrl, name) {
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open("POST", "$ScriptName", true);
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4) {
