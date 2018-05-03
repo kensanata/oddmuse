45cadbfedc04d21fc4daa1a282eb0aee5af51053
Alex Schroeder <alex@gnu.org>
2018-05-03 10:29:50 +0200

    wiki.pl: update copyright year

--- wiki.pl
+++ wiki.pl
@@ -1,5 +1,5 @@
 #! /usr/bin/perl
-# Copyright (C) 2001-2015
+# Copyright (C) 2001-2018
 #     Alex Schroeder <alex@gnu.org>
 # Copyright (C) 2014-2015
 #     Alex Jakimenko <alex.jakimenko@gmail.com>
