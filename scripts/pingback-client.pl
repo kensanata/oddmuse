a45e13f74c8bcc97b4d023b08749410a1e01f9df
Alex Schroeder <alex@gnu.org>
2019-02-01 23:28:03 +0100

    pingback-client: add license and intro paragraph

--- scripts/pingback-client.pl
+++ scripts/pingback-client.pl
@@ -1,3 +1,29 @@
+#! /usr/bin/perl -w
+
+# Copyright (C) 2013  Alex Schroeder <alex@gnu.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=head1 Pingback from one site to another
+
+If you link from page A on your site to page B on some other site, you can
+invoke this script with the command-line arguments A and B. In theory, this will
+create a link back from B to A, letting them and all their visitors know that
+you wrote something in response.
+
+=cut
+
 use Modern::Perl;
 use RPC::XML;
 use RPC::XML::Client;
