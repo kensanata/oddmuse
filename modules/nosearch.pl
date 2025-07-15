cc7240dc983e519523e6afbfb27d58c0b4a15754
Alex Schroeder <alex@gnu.org>
2025-07-15 10:50:00 +0200

    [nosearch] no class, no GET
    Don't wrap the title in a span with extra stuff.
    
    Don't use GET requests for the backlink search.

--- modules/nosearch.pl
+++ modules/nosearch.pl
@@ -22,21 +22,18 @@ our ($q, @MyAdminCode);
 *OldGetSearchLink = \&GetSearchLink;
 *GetSearchLink = \&NewGetSearchLink;
 sub NewGetSearchLink {
-  my ($text, $class, $name, $title) = @_;
-  $name = UrlEncode($name);
-  $text =~ s/_/ /g;
-  return $q->span({-class=>$class}, $text);
+  my ($id, $class, $name, $title) = @_;
+  return NormalToFree($id);
 }
 
 push(@MyAdminCode, \&BacklinksMenu);
 sub BacklinksMenu {
   my ($id, $menuref, $restref) = @_;
   if ($id) {
-      my $text = T('Backlinks');
-      my $class = 'backlinks';
-      my $name = 'backlinks';
-      my $title = T('Click to search for references to this page');
-      my $link = ScriptLink('search=' . $id, $text, $class, $name, $title);
-      push(@$menuref, $link);
+    my $form = GetFormStart(undef, 'post', 'search');
+    $form .= $q->input({-type=>'hidden', -name=>'search', -value=>'"'.NormalToFree($id).'"'});
+    $form .= $q->p(T('Click to search for references to this page'));
+    $form .= $q->submit('search', T('Go!')) . $q->end_form;
+    push(@$menuref, $form);
   }
 }
