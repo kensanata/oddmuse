c7a37261d150e23e4c5c1d817b87431894b48aa7
Alex Schroeder <alex@gnu.org>
2025-07-15 11:20:29 +0200

    [post-instead-of-get] Fix checkboxes
    Use -checked correctly. Make label clickable by adding an id to the
    checkboxes. Put the space inside the label.

--- modules/post-instead-of-get.pl
+++ modules/post-instead-of-get.pl
@@ -40,15 +40,19 @@ sub RcHeader {}
 *GetFilterForm=*PostNewGetFilterForm;
 
 sub PostNewGetFilterForm {
+  my $all = GetParam('all', $ShowAll);
+  my $showedit = GetParam('showedit', $ShowEdits);
+  my $rollback = GetParam('rollback', $ShowRollbacks);
+  my $lang = GetParam('lang', '');
   my $form = GetFormStart(undef, 'post', 'filter') . $q->h2(T('Filters'));
   $form .= $q->input({-type=>'hidden', -name=>'action', -value=>'rc'});
   $form .= $q->radio_group(-name=>'days', -values=>\@RcDays, -default=> $RcDefault) . ' ' . T('days') . $q->br();
-  $form .= $q->input({-type=>'checkbox', -name=>'all', -value=>1, -checked=>GetParam('all', $ShowAll)});
-  $form .= ' ' . $q->label({-for=>'all'}, T('List all changes')) . $q->br();
-  $form .= $q->input({-type=>'checkbox', -name=>'showedits', -value=>1, -checked=>GetParam('showedits', $ShowEdits)});
-  $form .= ' ' . $q->label({-for=>'showedits'}, T('Include minor changes')) . $q->br();
-  $form .= $q->input({-type=>'checkbox', -name=>'rollback', -value=>1, -checked=>GetParam('rollback', $ShowRollbacks)});
-  $form .= ' ' . $q->label({-for=>'rollback'}, T('Include rollbacks')) . $q->br();
+  $form .= $q->input({-type=>'checkbox', -id=>'all', -name=>'all', -value=>1, $all && '-checked'});
+  $form .= $q->label({-for=>'all'}, ' ' . T('List all changes')) . $q->br();
+  $form .= $q->input({-type=>'checkbox', -id=>'showedit', -name=>'showedit', -value=>1, $showedit && '-checked'});
+  $form .= $q->label({-for=>'showedit'}, ' ' . T('Include minor changes')) . $q->br();
+  $form .= $q->input({-type=>'checkbox', -id=>'rollback', -name=>'rollback', -value=>1, $rollback && '-checked'});
+  $form .= $q->label({-for=>'rollback'}, ' ' . T('Include rollbacks')) . $q->br();
   foreach my $h (['match' => T('Title:')], ['rcfilteronly' => T('Title and Body:')],
      ['rcuseronly' => T('Username:')], ['rchostonly' => T('Host:')], ['followup' => T('Follow up to:')]) {
     $form .= $q->label({-for=>$h->[0], -style=>'width:20ch; display:inline-block'}, $h->[1]);
@@ -57,16 +61,15 @@ sub PostNewGetFilterForm {
   }
   if (%Languages) {
     $form .= $q->label({-for=>'rclang', -style=>'width:20ch; display:inline-block'}, T('Language:'));
-    $form .= $q->textfield(-name=>'lang', -id=>'rclang', -size=>20,
-                           -default=>GetParam('lang', ''));
+    $form .= $q->textfield(-name=>'lang', -id=>'rclang', -size=>20, -default=>$lang);
   }
   $form .= $q->br() . $q->submit('dofilter', T('Go!')) . $q->end_form;
   $form .= GetFormStart(undef, 'post', 'later');
-  $form .= $q->input({-type=>'hidden', -name=>'all', -value=>1,-checked=>GetParam('all', $ShowAll)});
-  $form .= $q->input({-type=>'hidden', -name=>'showedits', -value=>1, -checked=>GetParam('showedits', $ShowEdits)});
+  $form .= $q->input({-type=>'hidden', -name=>'all', -value=>1, $all && '-checked'});
+  $form .= $q->input({-type=>'hidden', -name=>'showedit', -value=>1, $showedit && '-checked'});
   $form .= $q->p(T('List later changes') . ' ' . $q->submit('dofilter', T('Go!')));
   $form .= $q->end_form;
-  $form .= $q->p({-class => 'documentation'}, T('Using the ｢rollback｣ button on this page will reset the wiki to that particular point in time, undoing any later changes to all of the pages.')) if UserIsAdmin() and GetParam('all', $ShowAll);
+  $form .= $q->p({-class => 'documentation'}, T('Using the ｢rollback｣ button on this page will reset the wiki to that particular point in time, undoing any later changes to all of the pages.')) if UserIsAdmin() and $all;
   return $form;
 }
 
